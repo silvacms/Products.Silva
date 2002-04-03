2edeaa8dc415622b7000777a86d62a53b7a48b92
Martijn Faassen <faassen@startifact.com>
2002-04-03 14:38:48 +0000

    Added very simple user management system used for testing purposes.

---
+++ SimpleUserManagement.py
@@ -0,0 +1,19 @@
+
+class SimpleUserManagement:
+    def __init__(self):
+        pass
+
+    def find_users(self, object, search_string):
+        userids = object.acl_users.getUserNames()
+        for userid in userids:
+            if userid.find(search_string) != -1:
+                result.append(userid)
+        return result
+    
+    def get_user_info(self, object, userid):
+        return {
+            'userid': userid,
+            'name': name
+            }
+    
+user_management = SimpleUserManagement()
