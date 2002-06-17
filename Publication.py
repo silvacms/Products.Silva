699586ce016d7859ce97c74144ff34f46b0111ba
Infrae <info@infrae.com>
2002-06-17 12:40:08 +0000

    Tweaks to publication.

--- Publication.py
+++ Publication.py
@@ -26,16 +26,27 @@ class Publication(Folder):
         """
         # first create new folder
         container = self.get_container()
+        orig_id = self.id
+        convert_id = 'convert__%s' % orig_id
         container.manage_addProduct['Silva'].manage_addFolder(
-            'convert__%s' % self.id, self.get_title(), create_default=0)
+            convert_id, self.get_title(), create_default=0)
+        publication = getattr(container, convert_id)
         # copy all contents into new folder
-        cp = self.manage_copyObjects(self.objectIds())
+        cb = self.manage_copyObjects(self.objectIds())
+        publication.manage_pasteObjects(cb)
         # copy over all properties
         for key, value in self.propertyItems():
-            self.manage_addProperty(key, value, self.getPropertyKey(key))
+            publication.manage_addProperty(
+                key, value, self.getPropertyKey(key))
         # copy over authorization info
         for userid in self.sec_get_userids():
-            pass
+            roles = self.sec_get_roles_for_userid(userid)
+            for role in roles:
+                publication.sec_assign(userid, role)
+        # now remove this object from the container
+        container.manage_delObjects([self.id])
+        # and rename the copy
+        container.manage_renameObject(convert_id, orig_id)
         
     # ACCESSORS
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
