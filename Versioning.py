791b45d5e44dbc96ec2b85fcb63b395b5c703288
Guido Wesdorp <info@infrae.com>
2002-09-09 13:31:41 +0000

    Fixed bug Versioning: _update_publication_status now places the public version (if any) in the previous_versions-list when the next version is published by it.

--- Versioning.py
+++ Versioning.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 from DateTime import DateTime
 import Interfaces
 from AccessControl import ClassSecurityInfo
@@ -195,6 +195,10 @@ class Versioning:
         publication_datetime = self._approved_version[1]
         # if it is time make approved version public
         if publication_datetime and now >= publication_datetime:
+	    if self._public_version != empty_version:
+	        if not self._previous_versions:
+		    self._previous_versions = []
+		self._previous_versions.append(self._public_version)
             self._public_version = self._approved_version
             self._approved_version = empty_version
         # get expiration datetime of public version 
