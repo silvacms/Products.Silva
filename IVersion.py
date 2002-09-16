5555bb54562bcbb70a1cd845e5843cd2888f3976
Guido Wesdorp <info@infrae.com>
2002-09-16 15:36:35 +0000

    Added 'import Interface' as bugfix

--- IVersion.py
+++ IVersion.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.1 $
+# $Revision: 1.2 $
+import Interface
         
 class IVersion(Interface.Base):
     """Version of a versioned object
