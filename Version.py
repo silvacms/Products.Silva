01db8d14fb8362a42569037904a7b1a09405b44e
Guido Wesdorp <info@infrae.com>
2002-09-16 15:26:58 +0000

    Added Version.py

---
+++ Version.py
@@ -0,0 +1,88 @@
+from AccessControl import ClassSecurityInfo
+from OFS.SimpleItem import SimpleItem
+from Globals import InitializeClass
+
+from Products.Silva.IVersion import IVersion
+from Products.Silva import SilvaPermissions
+
+class Version(SimpleItem):
+
+    __implements__ = IVersion
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, id):
+        self.id = id
+        #self.index_object()
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'version_status')
+    def version_status(self):
+        """Returns the status of the current version
+        Can be 'unapproved', 'approved', 'published', 'last_closed' or 'closed'
+        """
+        status = None
+        unapproved_version = self.get_unapproved_version()
+        approved_version = self.get_approved_version()
+        public_version = self.get_public_version()
+        previous_versions = self.get_previous_versions()
+        if unapproved_version and unapproved_version[0] == self.id:
+            status = "unapproved"
+        elif approved_version and approved_version[0] == self.id:
+            status = "approved"
+        elif public_version and public_version[0] == self.id:
+            status = "public"
+        else:
+            if previous_versions and previous_versions[-1] == self.id:
+                status = "last_closed"
+            elif self.id in previous_versions:
+                status = "closed"
+            else:
+                raise ValueError, "Version %s not found in object %s" % (self.id, self.object().id)
+        return status
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'object_path')
+    def object_path(self):
+        """Returns the physical path of the object (for identification-purposes)
+        """
+        return self.aq_inner.aq_parent.getPhysicalPath()
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'version')
+    def version(self):
+        """Returns the version
+        """
+        return (self.id, self.publication_datetime(), self.expiration_datetime())
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'object')
+    def object(self):
+        """Returns the object this version belongs to
+        """
+        return self.aq_inner.aq_parent
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'publication_datetime')
+    def publication_datetime(self):
+        """Returns the publication_datetime of this version (if any)
+        """
+        status = self.version_status()
+        if status == 'closed' or status == 'last_closed':
+            return None
+        else:
+            return getattr(self, 'get_%s_version_publication_datetime' % status)()
+
+    security.declareProtected(SilvaPermissions.AccessContentsInformation,
+                              'expiration_datetime')
+    def expiration_datetime(self):
+        """Returns the expiration_datetime of this version (if any)
+        """
+        status = self.version_status()
+        if status == 'closed' or status == 'last_closed':
+            return None
+        else:
+            return getattr(self, 'get_%s_version_expiration_datetime' % status)()
+
+InitializeClass(Version)
+
