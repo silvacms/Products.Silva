80828ead233acc5e45bbf94982615ea094dce05c
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2002-09-20 09:49:56 +0000

    On second thought: disabled the Silva File object init, since people might not yet have the corresponding ZODB code.

--- __init__.py
+++ __init__.py
@@ -1,24 +1,24 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.29 $
+# $Revision: 1.30 $
 import Document, Folder, Root, ViewRegistry,\
        Publication, Ghost, Image, DemoObject,\
        File
 
 def initialize(context):
-    context.registerClass(
-        File.FilesService,
-        constructors = (
-            File.manage_addFilesServiceForm, File.manage_addFilesService),
-        icon = "www/files_service.gif"
-        )
+    #context.registerClass(
+    #    File.FilesService,
+    #    constructors = (
+    #        File.manage_addFilesServiceForm, File.manage_addFilesService),
+    #    icon = "www/files_service.gif"
+    #    )
 
-    context.registerClass(
-        File.File,
-        constructors = (
-            File.manage_addFileForm, File.manage_addFile),
-        icon="www/silvageneric.gif"
-        )
+    #context.registerClass(
+    #    File.File,
+    #    constructors = (
+    #        File.manage_addFileForm, File.manage_addFile),
+    #    icon="www/silvageneric.gif"
+    #    )
 
     context.registerClass(
         Document.Document,
