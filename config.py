5fee10c9a1e0357065286293b793d5d0b484185d
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2002-09-23 17:21:30 +0000

    Typo/clearification in config option comment

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # Installation-wide configuration options:
 
@@ -10,7 +10,7 @@
 # These options maybe overrided on a per site basis
 # by an FilesService object named 'service_files'.
 #
-FILESYSTEM_STORAGE_ENABLED = 0       # Set to 0 to disable external storage.
+FILESYSTEM_STORAGE_ENABLED = 0       # Set to 1 to enable external storage.
 
 FILESYSTEM_PATH = 'var/repository'   # Path to store files relative to
                                      # Zope's INSTANCE_HOME. For security
