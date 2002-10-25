4dc6a1d148f9239a4dec98c5e31a93056c68f01f
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2002-10-25 13:27:37 +0000

    Proof of concept access restriction parser (thanks to Clemens)

--- helpers.py
+++ helpers.py
@@ -1,11 +1,13 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.6 $
+# $Revision: 1.7 $
+# Zope
+from AccessControl import ModuleSecurityInfo
 # Silva interfaces
 from IVersioning import IVersioning
 from IContainer import IContainer
-# misc
-import urllib
+# python
+import string, re, urllib
 
 def add_and_edit(self, id, REQUEST):
     """Helper function to point to the object's management screen if
@@ -50,3 +52,133 @@ from Products.ParsedXML.PrettyPrinter import _translateCdata, _translateCdataAtt
 translateCdata = _translateCdata
 translateCdataAttr = _translateCdataAttr
 
+#
+# This code is not here to stay. It is an experimental "proof of concept"
+# implementation for resticting view access on Silva objects based on
+# ip addresses. We need a more generic approach integrated in the silva core
+# e.g. based on roles?
+#
+# This code is based on Clemens Klein-Robbenhaar's "CommentParser.py" 
+# Marc Petitmermet's and Wolfgang Korosec's code.
+#
+security = ModuleSecurityInfo('Products.Silva.helpers')
+security.declarePublic('parseAccessRestriction')
+
+__all__ = ('parseAccessRestriction',)
+
+ACCESS_RESTRICTION_PROPERTY = 'access_restriction'
+
+# match any quote _not_ preceeded by a backslash
+quote_split = re.compile(r'(?<![^\\]\\)"', re.M)
+
+# match any escapes of quotes (e.g. \\ or \" )
+drop_escape = re.compile(r'\\(\\|")')
+
+def parseAccessRestriction(item):
+    raw_string = ''
+    #if item.implements_container():
+    # use "getattr" to get aquired access restrictions.
+    raw_string = getattr(item, ACCESS_RESTRICTION_PROPERTY, '')
+    #elif item.implements_versioned_content():
+    #    raw_string = item.getProperty(DOCUMENT_PROPERTY, '')
+    if not raw_string: return {}
+
+    return _parse_raw(raw_string)
+
+#stupid record
+class State:
+    pass
+
+def _parse_raw(raw_string):
+    props = {}
+
+    state = State()
+    
+    # first split due to quotes
+    quoted = quote_split.split(raw_string)
+
+    in_quote = None
+    state.read_props = None
+
+    for item in quoted:
+        if in_quote:
+            _parse_quote(item, state, props)
+        else:
+            _parse_unquote(item, state, props)
+            
+        in_quote = not in_quote
+    
+    return props
+
+
+def _parse_unquote(something, state, props):
+    
+    if not state.read_props:
+        try:
+            name, something = map (string.strip, something.split(':',1) )
+        except ValueError:
+            # no name: quit.
+            return
+        state.name = name
+        props[name] = []
+        state.plist = props[name]
+        state.read_props = 1
+
+    separate = something.split(';',1)
+    if len(separate) > 1:
+        something, rest = map (string.strip, separate)
+    else:
+        rest=None
+
+    for p in map (string.strip, something.split(',') ):
+        if p:
+            state.plist.append(p)
+
+    if rest is not None:
+        state.read_props=None
+        _parse_unquote(rest, state, props)
+
+
+def _parse_quote(something, state, props):
+    """ parse everything enclosed in quotes.
+    this is an easy one: just remove the escapes
+    """
+    if not state.read_props:
+        raise ValueError, "not inside a property definition: <<%s>>" % something
+
+    something = drop_escape.sub(lambda match: match.group(1), something )
+    state.plist.append(something)
+    
+
+def test():
+    """ hacky: test by hand ..."""
+    test1 = r"""
+property: value1, "my quote, comma containing value", 
+"another value with \"quotes\" in it, as well as commas", "something containing \\", even more;
+
+property2: aha, what's that, ";;;" ,,,'ignores commas',"",;
+
+single: only one property  ;
+
+white: "  one property,  whitespace preserving   ";
+
+"""
+
+    props1 = {'property': ['value1','my quote, comma containing value',
+                           'another value with "quotes" in it, as well as commas',
+                           'something containing \\', 'even more'] ,
+              'property2': [ 'aha', "what's that", ';;;', "'ignores commas'",'' ] ,
+              'single' : [ 'only one property' ],
+              'white' : ["  one property,  whitespace preserving   "],
+              }
+                          
+    props = _parse_raw(test1)
+
+    if not props1 == props:
+        print "FAILURE:\n expected %s\n but got %s\n" %  tuple(map(str, (props1, props) ))
+    else:
+        print "simple test ok"
+
+# self testing:
+if __name__ == '__main__':
+    test()
