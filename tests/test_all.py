879f3dc26da5e6efd4eb7e566ee56dd0223c10af
Guido Wesdorp <info@infrae.com>
2002-11-06 13:49:23 +0000

    Commented out the test for CatalogedDemoObject, which is not part of the current distribution of Silva.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.6 $
+# $Revision: 1.7 $
 import unittest
 import Zope
 
@@ -24,7 +24,7 @@ def test_suite():
     suite.addTest(test_Ghost.test_suite())
     suite.addTest(test_Copy.test_suite())
     suite.addTest(test_Security.test_suite()) 
-    suite.addTest(test_CatalogedVersioning.test_suite()) 
+    #suite.addTest(test_CatalogedVersioning.test_suite()) 
     return suite
 
 def main():
