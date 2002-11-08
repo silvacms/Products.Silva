4c664629a0c836be54b48e39744bbe99ce64f747
Infrae <info@infrae.com>
2002-11-08 04:08:38 +0000

    issue22 (request for approval):  added new tests:
      - test_workflow6: check "normal" workflow for request for approval
      - test_illegal_request_approval: check that the proper exceptions are thrown
          if request for approval is illegal
      - _test_workflow7: test if it is possible to reopen closed versions
         without creating a new one
         this test is disabled as it will fail; the functionality is not yet implemented

--- tests/test_Versioning.py
+++ tests/test_Versioning.py
@@ -1,12 +1,20 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 import unittest
 import Zope
 #import ZODB
 #import OFS.Application
 from DateTime import DateTime
 from Products.Silva import Versioning
+from OFS import SimpleItem
+from Testing import makerequest
+from test_SilvaObject import hack_add_user
+
+class ZODBVersioning(Versioning.Versioning,  SimpleItem.SimpleItem):
+    # awful hack a verisoning implementation which also may have a REQUEST associated
+    # interstingly it works w/o having an id, aquisition parent, etc.
+    pass
 
 class VersioningTestCase(unittest.TestCase):
     def setUp(self):
@@ -159,6 +167,146 @@ class VersioningTestCase(unittest.TestCase):
         versioning.close_version()
         self.assertEqual(versioning.get_previous_versions(), ['first', 'second'])
         self.assertEqual(versioning.get_last_closed_version(), 'second')
+
+
+    # XXX assume boolean values are 0 and 1 ... will fail for python2.2
+    def _check_version_state(self, approved=0, approval_requested=0, published=0):
+        # helper method to check consistency of the version state
+        # XXX no good error messages if test fail
+        self.assertEquals(approved, self.versioning.is_version_approved())
+        self.assertEquals(approved and self.versioning.get_next_version() or None,
+                          self.versioning.get_approved_version())
+        self.assertEquals(published, self.versioning.is_version_published())
+        # XXX cannot test this: get_next_version returns None
+        # if there is only a published version (?)
+        #self.assertEquals(published and self.versioning.get_next_version() or None,
+        #                  self.versioning.get_public_version())
+        self.assertEquals(approval_requested and not approved,
+                          self.versioning.is_version_approval_requested())
+        self.assertEquals((not approved) and self.versioning.get_next_version() or None,
+                          self.versioning.get_unapproved_version())
+        
+
+    def test_workflow6(self):
+        true=1
+        # we need a request with an user here
+        self.versioning = makerequest.makerequest(ZODBVersioning())
+        hack_add_user(self.versioning.REQUEST)
+        
+        # test request for approval
+        self._check_version_state()
+        self.versioning.create_version('0', DateTime() + 10, None)
+        self._check_version_state()        
+        self.versioning.set_approval_request_message('foo')
+        self.versioning.request_version_approval()
+        self._check_version_state(approval_requested=true)
+        self.assertEquals('TestUser',self.versioning.get_approval_requester())
+        self.assertEquals('foo',
+                          self.versioning.get_approval_request_message())
+        self.versioning.withdraw_version_approval()
+        self._check_version_state()
+        self.versioning.request_version_approval()
+        self._check_version_state(approval_requested=true)
+        self.versioning.approve_version()
+        self._check_version_state(approved=true)
+
+        
+        # just check, if request for approval could break unaproval
+        # or close later, though this is unreasonable
+        self.versioning.unapprove_version()
+        self._check_version_state()
+        self.versioning.approve_version()
+        self._check_version_state(approved=true)
+        self.versioning.set_approved_version_publication_datetime(DateTime()-1)
+        self.versioning.close_version()
+        self._check_version_state()
+        self.versioning.create_version('1', DateTime() + 1, None)
+        self._check_version_state()
+
+        
+
+    def test_illegal_request_approval(self):
+        # test if all kind of VersioningError are actually raised
+        self.versioning = makerequest.makerequest(ZODBVersioning())
+        hack_add_user(self.versioning.REQUEST)
+
+        try:
+            self.versioning.request_version_approval()
+            self.fail(msg="cannot request for approval without version")
+        except Versioning.VersioningError:
+            pass
+        
+        self.versioning.create_version('first', DateTime() + 10, None)
+        self.versioning.request_version_approval()
+        try:
+            self.versioning.request_version_approval()
+            self.fail(msg="cannot request for approval twice")
+        except Versioning.VersioningError:
+            pass
+
+        self.versioning.withdraw_version_approval()
+        try:
+            self.versioning.withdraw_version_approval()
+            self.fail(msg="cannot withdraw request for approval twice")
+        except Versioning.VersioningError:
+            pass
+
+        self.versioning.approve_version()
+        try:
+            self.versioning.request_version_approval()
+            self.fail(msg="cannot request for approval after approving version")
+        except Versioning.VersioningError:
+            pass
+        try:
+            self.versioning.withdraw_version_approval()
+            self.fail(msg="cannot withdraw request for approval after approving version")
+        except Versioning.VersioningError:
+            pass
+
+        self.versioning.unapprove_version()
+        try:
+            self.versioning.withdraw_version_approval()
+            self.fail(msg="cannot withdraw request for approval after approving and unapproving version")
+        except Versioning.VersioningError:
+            pass
+        self.versioning.request_version_approval()
+        self._check_version_state(approval_requested=1)
+
+        # XXX check publish state here ? shoud be in workflow6?
+        self.versioning.set_unapproved_version_publication_datetime(DateTime()-1)
+        self.versioning.approve_version()
+        self._check_version_state(published=1)
+        self.versioning.close_version()
+        
+        try:
+            self.versioning.request_version_approval()
+            self.fail(msg="cannot request for approval after closing version")
+        except Versioning.VersioningError:
+            pass
+        try:
+            self.versioning.withdraw_version_approval()
+            self.fail(msg="cannot withdraw request for approval after closing version")
+        except Versioning.VersioningError:
+            pass
+
+
+    def _test_workflow7(self):
+        # reopen a closed version
+        self.versioning = Versioning.Versioning()
+ 
+        # create new version
+        self.versioning.create_version('first', DateTime() - 1, DateTime() + 2)
+        # approve and close it
+        self.versioning.set_unapproved_version_publication_datetime(DateTime()-1)
+        self.versioning.approve_version()
+        self.versioning.close_version()
+        self._check_version_state()
+        
+        self.versioning.approve_version()
+        self._check_version_state(approved=1)
+        # did not create an new version
+        self.assertEquals('first', self.versioning.get_approved_version())
+       
 
 def test_suite():
     suite = unittest.TestSuite()
