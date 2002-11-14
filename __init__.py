de050fafc4589264db1cbd5a13be469ca2402374
Infrae <info@infrae.com>
2002-11-14 14:49:49 +0000

    OK, now it works.  Another lesson relearned: never change code without testing before committing. Not even a bit :-)
    There are two monkey patches neccessary:
    
    * utils.minimalpath
    
    * DirectoryView.minimalpath
      because it is imported as a global before we can do anything
    
    Creating new silva-roots now works for me, but please verify.
    If yes then close the issue with done-cbb and a closing message
    ...
    
    If you find a way to assert for FSS version 1.1. that couldn't
    hurt either.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.40 $
+# $Revision: 1.41 $
 import ViewRegistry, MultiViewRegistry
 import Document, Folder, Root
 import Publication, Ghost, Image, File
@@ -19,6 +19,7 @@ registerFileExtension('ico', FSImage)
 
 # -- monkey patch FileSystemSite's minimalpath until new release --
 def minimalpath(p):
+    import os
     check = [SOFTWARE_HOME+os.sep, INSTANCE_HOME+os.sep]
     if INSTANCE_HOME.startswith(SOFTWARE_HOME):
         check.reverse()
@@ -29,7 +30,7 @@ def minimalpath(p):
     return newpath
 
 from Products import FileSystemSite
-FileSystemSite.minimalpath = minimalpath
+FileSystemSite.DirectoryView.minimalpath = minimalpath
 FileSystemSite.utils.minimalpath = minimalpath
 # -- monkey patch end ---
 
