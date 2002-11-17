c7b459b2dc573097ceb375766f3e8c384689b291
Infrae <info@infrae.com>
2002-11-17 20:18:09 +0000

    do a "sudden insert" in case there is only one allowed type of element to be inserted.  deprecates the "insert_now" flag

--- widgets/insert_mode.py
+++ widgets/insert_mode.py
@@ -9,6 +9,10 @@
 ##
 node = context.REQUEST.node
 node.sec_update_last_author_info()
-context.service_editor.setNodeWidget(node, 
-   context.get_widget_path()[:-1] + ('mode_insert',))
-return context.redirect()
+allowed_types = context.get_allowed_info(context.REQUEST.wr_name,node)
+if len(allowed_types)==1:
+   return context.insert(what = allowed_types[0][1])
+else:
+   context.service_editor.setNodeWidget(node, 
+      context.get_widget_path()[:-1] + ('mode_insert',))
+   return context.redirect()
