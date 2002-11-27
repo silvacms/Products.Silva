db3e327f1be4179b4f00d39262bd3c5ac29d3e1e
Kit Blake <kitblake@infrae.com>
2002-11-27 06:25:23 +0000

    Layout tweaking

--- views/local_style.py
+++ views/local_style.py
@@ -2,6 +2,6 @@ request = context.REQUEST
 model = request.model
 
 if model.implements_container():
-    return '<!-- td.tab {background:#4c7469} -->'
+    return '<!-- td.tab {background:#4d755b} -->'
 else:
     return None
