de072791b7175096063f5c21f39143782bc41467
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2002-12-20 15:27:37 +0000

    newborn data element widgets

---
+++ widgets/element/doc_elements/dataelement/content.py
@@ -0,0 +1,12 @@
+## Script (Python) "content"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+node = context.REQUEST.node
+
+return "---data source---"
---
+++ widgets/element/doc_elements/dataelement/mode_edit/save_helper.py
@@ -0,0 +1,65 @@
+## Script (Python) "save_helper"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+request = context.REQUEST
+node = request.node
+current_path = node.getAttribute('path')
+new_path = ''
+if request.has_key('path'):
+    new_path = request['path']
+
+def removeParameterElements(node):
+    for child in node.childNodes:
+        if child.nodeType == child.ELEMENT_NODE:
+            if child.nodeName == 'parameter':
+                try:
+                    node.removeChild(child)
+                except ValueError:
+                    pass
+
+if not new_path:
+    # no path set.
+    # get rid of parameters(?)
+    # return.
+    node.removeAttribute('path')
+    removeParameterElements(node)
+    return
+
+node.setAttribute('path', new_path)
+datasource = context.get_datasource()
+datasource_parameters = datasource.parameters()
+
+if current_path == new_path:
+    # same datasource path:
+    # set parameter data from form.
+    form = datasource.parameter_values_as_form({})
+    # validate form
+    from Products.Formulator.Errors import FormValidationError
+    try:
+        result = form.validate_all(request)
+    except FormValidationError, e:
+        err = e.errors[0]
+        raise str(err.error_text)
+    removeParameterElements(node)
+    for name in datasource_parameters.keys():
+        child = node.createElement('parameter')
+        child.setAttribute('key', name)
+        child.setAttribute('value', result[name])
+        node.appendChild(child)
+else:
+    # different datasource path:
+    # get parameter defs.
+    # throw away old param elements.
+    # set new defaults.
+    removeParameterElements(node)
+    for name, (type, default_value, description) in datasource_parameters.items():
+        child = node.createElement('parameter')
+        child.setAttribute('key', name)
+        child.setAttribute('value', default_value)
+        node.appendChild(child)
