c78650a17917912df33d46c42988fd9a14aa4556
Guido Wesdorp <info@infrae.com>
2002-12-20 17:07:58 +0000

    Made some last UI changes

--- views/edit/Container/tab_edit_import_docma.py
+++ views/edit/Container/tab_edit_import_docma.py
@@ -11,11 +11,20 @@ view = context
 request = view.REQUEST
 model = request.model
 
+model.security_trigger()
+
 if not request.has_key('importfile') or not request['importfile']:
     return view.tab_edit_import_docma(message_type='error', message='Select a file for upload')
 
+if not request.has_key('email') or not request['email']:
+    return view.tab_edit_import_docma(message_type='error', message='You must enter your e-mail address')
+
+description = 'No description'
+if request.has_key('description') and request['description']:
+    description = request['description']
+
 data = request['importfile'].read()
 
-ident, status = model.service_docma.word2silva(data, request.AUTHENTICATED_USER.getId(), model.getPhysicalPath())
+ident, status = model.service_docma.word2silva(data, str(request.AUTHENTICATED_USER), request['email'], description)
 
-return view.tab_edit_import_docma(message_type='feedback', message='Your job is %s. The id of your job is %s.' % (status, ident))
+return view.tab_edit_import(message_type='feedback', message='Your job is %s. The id of your job is %s.' % (status, ident))
