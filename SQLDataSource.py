df1035f63dced2f9a22b7b32017dc6f94a9feb83
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2002-12-23 08:58:02 +0000

    supply mapping for parameters

--- SQLDataSource.py
+++ SQLDataSource.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # Zope
 from Globals import InitializeClass
 from OFS import SimpleItem
@@ -47,8 +47,7 @@ class SQLDataSource(DataSource):
         """
         if not self._sql_method:
             self._set_sql_method()
-
-        return self._sql_method(params)
+        return self._sql_method(REQUEST=params)
     
     # MODIFIERS
 
