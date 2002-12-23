c867d079d5f3c2013df40e37c5bd9d60765e6152
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2002-12-23 15:38:56 +0000

    weird hack to get the SESSION persistency/transaction correct.

--- views/edit/lookup_get_selection.py
+++ views/edit/lookup_get_selection.py
@@ -14,5 +14,6 @@ key = ('silva_lookup_selection', context.silva_root())
 
 if not session.has_key(key):
     session[key] = {}
-
+# Hack to get SESSION transaction/persistency correct
+session[key] = session[key]
 return session[key]
