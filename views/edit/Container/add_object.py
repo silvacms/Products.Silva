e164c7cad21faa82cc74ad7578c8bf6f80229de7
Infrae <info@infrae.com>
2003-01-03 19:05:04 +0000

    fix problem when selecting the disabled "select ..." option from the add content type drop down (N4 allows this). This has generated an exception, as "None" is not valid meta type.  This is now handled by an error message. The drawback is that the meta type "None" is now invalid, but hopefully nobody tries to create a content type with such a meta type.

--- views/edit/Container/add_object.py
+++ views/edit/Container/add_object.py
@@ -1,4 +1,6 @@
 ##parameters=meta_type
 # get the add view from the add views registry
+if meta_type=='None':
+    return context.tab_edit(message_type='error', message='Please select a content type to add')
 add_view = context.service_view_registry.get_view('add', meta_type)
 return add_view.add_form()
