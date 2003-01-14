17afe222d5c5fdd55490577123dbc1cb0837ef91
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-01-14 13:48:45 +0000

    bugfix for _getOb attribute errors

--- LDAPUserManagement.py
+++ LDAPUserManagement.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.6 $
+# $Revision: 1.6.2.1 $
 import types
 
 try:
@@ -15,6 +15,9 @@ class LDAPUserManagement:
     def _get_user_folder(self, object):
         # XXX hack...relatively inefficient
         while 1:
+            if not hasattr(object, '_getOb'):
+                object = object.aq_parent
+                continue
             if (object._getOb('acl_users', 0) and
                 object.acl_users.meta_type == 'LDAPUserFolder'):
                 return object.acl_users
