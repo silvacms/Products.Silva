a82fda9f66b6a1936b06ecbc4eedb5599991adec
Infrae <info@infrae.com>
2003-01-24 14:18:55 +0000

    cosmetics: move the sec_update above the save_helper. zope has less things to undo if the auth-header is missing in this case.

--- widgets/save.py
+++ widgets/save.py
@@ -7,7 +7,7 @@
 ##parameters=
 ##title=
 ##
-context.save_helper()
 context.REQUEST.node.get_content().sec_update_last_author_info()
+context.save_helper()
 context.invalidate_cache_helper()
 return context.redirect()
