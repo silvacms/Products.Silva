6160d21de7b5733ab7c65558459ec2771e0a1b0c
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-01-24 09:09:14 +0000

    add security declaration for isValid method

--- Group.py
+++ Group.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 from AccessControl import ClassSecurityInfo, Unauthorized
 from Globals import InitializeClass
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
@@ -36,6 +36,8 @@ class Group(Asset):
             # the real group is only deleted if the asset is valid
             self.service_groups.removeNormalGroup(self._group_name)
    
+    security.declareProtected(
+        SilvaPermissions.ChangeSilvaAccess, 'isValid')
     def isValid(self):
         """returns whether the group asset is valid
 
