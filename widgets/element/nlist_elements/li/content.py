da6990d67cb3f9dcb3ebf0c9da285aa512884980
Christian Zagrodnick <cz@gocept.com>
2003-01-24 14:47:56 +0000

    - Removed <br /> at end of list item, to add space there use style sheets.

--- widgets/element/nlist_elements/li/content.py
+++ widgets/element/nlist_elements/li/content.py
@@ -8,8 +8,5 @@
 ##title=
 ##
 context.service_editor.setViewer('service_sub_previewer')
-# XXX clemens hacked: add a break after a complex list element
-# adds some space to the next list item (otherwise the
-# end of this item may be closer to the start of the next item 
-# than to the rest of this list element)
-return context.service_editor.getViewer().getWidget(context.REQUEST.node).render()+ '<br />'
+return context.service_editor.getViewer().getWidget(context.REQUEST.node).\
+    render()
