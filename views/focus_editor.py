37a3d525a4d732c6e32a66490508ede775578bd3
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-01-30 14:27:37 +0000

    Views' counterpart of widgets' focus_editor javascript inclusion. Resolves http://issues.infrae.com/silva/issue94

---
+++ views/focus_editor.py
@@ -0,0 +1,22 @@
+## Script (Python) "focus_editor"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=focussed_form_element=None
+##title=
+##
+return '''
+// Called from body onload() method which is defined in the
+// macro_index. It focusses the appropiate form field.
+function focus_editor() {
+  if (document.getElementsByName) {
+    elements = document.getElementsByName('%s');
+    if (elements.length > 0) {        
+        //alert(elements);
+        elements[0].focus();
+     }
+  }
+}
+''' % (focussed_form_element)
