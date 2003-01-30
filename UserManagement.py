e544d1166002e622d4f1efc04b0d401a41676fb1
Martijn Faassen <faassen@startifact.com>
2003-01-30 21:54:26 +0000

    Removing old UserManagement files; replaced by Membership.

--- UserManagement.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2002 Infrae. All rights reserved.
-# See also LICENSE.txt
-# $Revision: 1.3 $
-from SimpleUserManagement import user_management
-# from LDAPUserManagement import user_management
