148422d4fe08734efb03da39deccd5ca9d2c692d
Kit Blake <kitblake@infrae.com>
2003-02-04 09:15:06 +0000

    checkin after the new power supply, many interface tweaks (which I can't quite specify now) including tab_preview using the local layout_macro

--- views/edit/Container/add_object.py
+++ views/edit/Container/add_object.py
@@ -1,6 +1,6 @@
 ##parameters=meta_type
 # get the add view from the add views registry
 if meta_type=='None':
-    return context.tab_edit(message_type='error', message='Please select a content type to add')
+    return context.tab_edit_new()
 add_view = context.service_view_registry.get_view('add', meta_type)
 return add_view.add_form()
