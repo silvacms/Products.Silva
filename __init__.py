fbf4d4cb272f99892a87429041546ea914d33c50
Christian Zagrodnick <cz@gocept.com>
2003-02-05 08:40:44 +0000

    - Replaced docma service icon with the nicer docma icon.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.60 $
+# $Revision: 1.61 $
 import ViewRegistry, MultiViewRegistry
 import Document, Folder, Root
 import Publication, Ghost, Image, File
@@ -82,7 +82,7 @@ def initialize(context):
         DocmaService.DocmaService,
         constructors = (DocmaService.manage_addDocmaServiceForm,
                         DocmaService.manage_addDocmaService),
-        icon="www/service_docma.png",
+        icon="globals/docma.gif",
         )
 
     # register xml import functions
