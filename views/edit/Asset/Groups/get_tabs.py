84382fb894454319a18bb4940961cf0379c83b09
Kit Blake <kitblake@infrae.com>
2003-02-09 06:11:54 +0000

    + experimental groups tab

--- views/edit/Asset/Groups/get_tabs.py
+++ views/edit/Asset/Groups/get_tabs.py
@@ -9,7 +9,8 @@
 ##
 # define:
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
-tabs = [('Edit', 'tab_edit', 'tab_access', '!', '1', '6'),
+tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
+        ('Groups', 'tab_access_groups', 'tab_access_groups', '@', '2', '7'),
        ]
 
 return tabs
