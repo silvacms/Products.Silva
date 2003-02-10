5ef2b63741a06adfe1e23b62cbb5d83fc2190997
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-02-10 17:58:58 +0000

    icons in ZMI

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.65 $
+# $Revision: 1.66 $
 import ViewRegistry, MultiViewRegistry
 import Document, Folder, Root
 import Publication, Ghost, Image, File
@@ -92,6 +92,18 @@ def initialize(context):
         EditorSupportNested.EditorSupport,
         constructors = (EditorSupportNested.manage_addEditorSupport, ),
         icon = "www/editorservice.gif"
+        )
+
+    context.registerClass(
+        Group.Group,
+        constructors = (Group.manage_addGroup, ),
+        icon = "www/group.png"
+        )
+
+    context.registerClass(
+        VirtualGroup.VirtualGroup,
+        constructors = (VirtualGroup.manage_addVirtualGroup, ),
+        icon = "www/virtualgroup.png"
         )
 
     # register xml import functions
