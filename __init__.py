3094ebfd979b2db0907587fc9cdc88682d21a3ab
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-02-10 16:56:33 +0000

    icon for editoruspport service

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.64 $
+# $Revision: 1.65 $
 import ViewRegistry, MultiViewRegistry
 import Document, Folder, Root
 import Publication, Ghost, Image, File
@@ -91,6 +91,7 @@ def initialize(context):
     context.registerClass(
         EditorSupportNested.EditorSupport,
         constructors = (EditorSupportNested.manage_addEditorSupport, ),
+        icon = "www/editorservice.gif"
         )
 
     # register xml import functions
