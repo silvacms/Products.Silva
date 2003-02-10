7db4696411c7fc43a4d34cf13991387aae6dee25
Guido Wesdorp <info@infrae.com>
2003-02-10 14:58:55 +0000

    Removed HTML markup from return value of Root.upgrade_silva, since the return value is printed as plain text.

--- Root.py
+++ Root.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.47 $
+# $Revision: 1.48 $
 # Zope
 from AccessControl import ClassSecurityInfo
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
@@ -120,7 +120,7 @@ class Root(Publication):
         import upgrade
         my_id = self.id
         upgrade.from09to091(self.aq_inner.aq_parent, self)
-        return "Upgrade of %s succeeded.<br /> A backup is in %s_09.<br />" \
+        return "Upgrade of %s succeeded.\nA backup is in %s_09." \
                % (my_id, my_id)
 
     security.declareProtected(SilvaPermissions.ViewManagementScreens,
