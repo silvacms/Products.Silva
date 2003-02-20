8db4e46f86f00f199063faad3978c42d34839073
Kit Blake <kitblake@infrae.com>
2003-02-20 06:20:58 +0000

    corrected access key

--- views/edit/Content/Indexer/get_tabs.py
+++ views/edit/Content/Indexer/get_tabs.py
@@ -10,7 +10,7 @@
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
 tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
-        ('Access', 'tab_access', 'tab_access', '$', '3', '8'),
+        ('Access', 'tab_access', 'tab_access', '#', '3', '8'),
        ]
 
 return tabs
