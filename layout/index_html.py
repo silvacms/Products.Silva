e42bdf60a783f2b9c1247e17612cbca5a311c003
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-03-07 16:40:34 +0000

    set cache header by default

--- layout/index_html.py
+++ layout/index_html.py
@@ -9,6 +9,10 @@ if hasattr(context.aq_explicit, override):
 else:
     renderer = content
 
+# Setting Headers first
+context.REQUEST.RESPONSE.setHeader('Content-Type', 'text/html;charset=utf-8')
+context.REQUEST.RESPONSE.setHeader('Cache-Control','max-age=300')
+
 # By default, the prefered view method is 'view()', unless
 # a different view_method is passed to this script.
 return getattr(context, renderer)(view_method=view_method)
