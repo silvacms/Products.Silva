dddc00bbda0f180e1139bd75c793a80935f76643
Guido Wesdorp <info@infrae.com>
2003-03-12 17:33:51 +0000

    Added functionality for single and selected document export (to slv and word)

--- views/edit/Container/tab_status_silva2word_selected.py
+++ views/edit/Container/tab_status_silva2word_selected.py
@@ -14,6 +14,13 @@ model = request.model
 # if we don't call security_trigger here, the script will be called twice and the job will be started twice
 model.security_trigger()
 
+if not request.has_key('refs') or not request['refs']:
+    return view.tab_status(message_type='error', message='No items were selected, so nothing is exported')
+
+objects = []
+for ref in request['refs'].split('||'):
+    objects.append(model.resolve_ref(ref))
+
 if request.has_key('with_sub_publications') and request['with_sub_publications']:
     with_sub_publications = 1
 
@@ -24,11 +31,11 @@ description = 'No description'
 if request.has_key('description') and request['description']:
     description = request['description']
 
-data = model.get_xml(with_sub_publications, export_last_version)
+data = model.get_xml_for_objects(objects, with_sub_publications, export_last_version)
 
 if not request['email']:
     return view.tab_status(message_type='error', message='You have not entered your e-mail address')
 
 ident, status = model.service_docma.silva2word(request['email'], data, request['template'], str(request.AUTHENTICATED_USER.getId()), description)
 
-return view.tab_status(message_type='feedback', message='Your job is %s. The id of your job is %s.' % (status, ident))
+return view.tab_status_export(message_type='feedback', message='Your job is %s. The id of your job is %s.' % (status, ident))
