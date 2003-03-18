d77e0ec7a82dc514a80e79650cc2d862df4318f2
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-03-18 14:15:12 +0000

    get the relative paths for image assets to work in zope251

--- widgets/element/doc_elements/image/get_image.py
+++ widgets/element/doc_elements/image/get_image.py
@@ -8,7 +8,15 @@
 ##title=
 ##
 try:
-    image = image_context.restrictedTraverse(str(image_path))
+    # To fix this for Zope 2.5.1
+    if image_path.startswith('/'):
+        path = str(image_path)
+    else:
+        image_path = tuple(str(image_path).split('/'))
+        context_path = image_context.getPhysicalPath()
+        path = '/'.join(context_path + image_path)
+    image = image_context.restrictedTraverse(path)
+
 except (KeyError, AttributeError, ValueError, IndexError):
     # image reference is broken (i.e. renamed)
     image = None
