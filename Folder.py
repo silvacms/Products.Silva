669cbd634c3e631409865288cf00765f8c877ebd
Infrae <info@infrae.com>
2003-03-26 12:37:21 +0000

    sort deactivated content by id on the fly, despite deactivating contents is currently, er, deactivated

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.92 $
+# $Revision: 1.93 $
 # Zope
 import Acquisition
 from Acquisition import aq_inner
@@ -585,6 +585,7 @@ class Folder(SilvaObject, Publishable, Folder.Folder):
             if (IPublishable.isImplementedBy(object) and
                 not object.is_active()):
                 result.append(object)
+        result.sort(lambda x,y: cmp(x.getId(), y.getId()) )
         return result
 
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
