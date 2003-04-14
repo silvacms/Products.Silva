b813a48bd90d5a878d8155f80b0ff9cb18f6610e
Guido Wesdorp <info@infrae.com>
2003-04-14 11:22:27 +0000

    Some typos

--- views/add/get_id_status_text.py
+++ views/add/get_id_status_text.py
@@ -6,7 +6,7 @@ view = context
 if status_code == IdCheckValues.ID_CONTAINS_BAD_CHARS:
     return """The id should only contain letters, digits and underscore &quot;_&quot; or a period &quot;.&quot;<br>
 Additionally it should start with a letter or digit.<br>
-You entered the id %s which did not match these contraints.""" % view.quotify(id)
+You entered the id %s which did not match these constraints.""" % view.quotify(id)
 
 elif status_code == IdCheckValues.ID_RESERVED_PREFIX:
     # FIXME: needs to know about the prefix
@@ -19,7 +19,7 @@ elif status_code == IdCheckValues.ID_RESERVED:
 Please use another id.""" % view.quotify(id)
 
 elif status_code == IdCheckValues.ID_IN_USE_CONTENT:
-    return """There is already a content with the id %s in this container.<br>
+    return """There is already an object with the id %s in this container.<br>
 Please use another id.""" % view.quotify(id)
 
 elif status_code == IdCheckValues.ID_IN_USE_ASSET:
