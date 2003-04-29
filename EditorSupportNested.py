9ab6edb549535d5d23c598142dd29979c5b02cec
Guido Wesdorp <info@infrae.com>
2003-04-29 09:43:51 +0000

    Fixed typo that lead to an AttributeError (on self.match)

--- EditorSupportNested.py
+++ EditorSupportNested.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.16 $
+# $Revision: 1.17 $
 import re
 from sys import exc_info
 from StringIO import StringIO
@@ -270,7 +270,7 @@ class EditorSupport(SimpleItem):
                 break
             st = st.replace(match.group(0), 
                 '%s<index name="%s">%s</index>' % (match.group(1), 
-                    self.match.group(3), 
+                    match.group(3), 
                     match.group(2)))
         st = st.replace('\n', '<br/>')
         st = self.input_convert(st).encode('UTF8')
