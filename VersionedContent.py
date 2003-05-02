3976333c996c910cb46917ba3251213969f84d8a
Guido Wesdorp <info@infrae.com>
2003-05-02 11:47:02 +0000

    Fixed bug where index methods flaked out when a version was an empty one

--- VersionedContent.py
+++ VersionedContent.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.23 $
+# $Revision: 1.24 $
 
 # Python
 from StringIO import StringIO
@@ -162,12 +162,18 @@ class CatalogedVersionedContent(VersionedContent):
         return ret
 
     def _index_version(self, version):
+        if version[0] is None:
+            return None
         getattr(self, version[0]).index_object()
         
     def _reindex_version(self, version):
+        if version[0] is None:
+            return None
         getattr(self, version[0]).reindex_object()
 
     def _unindex_version(self, version):
+        if version[0] is None:
+            return None
         getattr(self, version[0]).unindex_object()
         
 InitializeClass(CatalogedVersionedContent)
