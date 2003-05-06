2c8bd4102bc46ec5b7b7a927e0792c458859fbee
Kit Blake <kitblake@infrae.com>
2003-05-06 06:40:29 +0000

    formatting

--- views/edit/minimal_access_role_submit.py
+++ views/edit/minimal_access_role_submit.py
@@ -19,13 +19,13 @@ old_minimal_role = model.sec_minimal_view_role()
 # XXX This stuff is now tailormade for the situation, should work with more than 2 options in the future
 if new_minimal_role == 'Anonymous' and old_minimal_role != 'Anonymous':
     model.sec_open_to_public()
-    message = 'Object can be viewed by anyone'
+    message = 'No restriction is applied (open to the public)'
 elif new_minimal_role == 'Authenticated' and old_minimal_role != 'Authenticated':
     model.sec_close_to_public(1)
-    message = 'Minimal role for viewing object is now set to Authenticated'
+    message = 'Minimal role for viewing is now set to Authenticated'
 elif new_minimal_role == 'Viewer' and old_minimal_role != 'Viewer':
     model.sec_close_to_public(0)
-    message = 'Minimal role for viewing object is now set to Viewer'
+    message = 'Minimal role for viewing is now set to Viewer'
 elif new_minimal_role == old_minimal_role:
     message = 'Minimal role has not changed'
 
