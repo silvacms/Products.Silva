295c9216a9579a241599dbf64ee3e8b94e5828a8
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-05-08 16:20:51 +0000

    fixing stupid bug in tree rendering code. will be backported to 091 branch too

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.87.2.2 $
+# $Revision: 1.87.2.3 $
 # Zope
 import Acquisition
 from Acquisition import aq_inner
@@ -656,7 +656,7 @@ class Folder(SilvaObject, Publishable, Folder.Folder):
                 continue
             if item.is_transparent():
                 l.append((indent, item))
-                if depth == -1 or depth < indent:
+                if depth == -1 or indent < depth:
                     item._get_container_tree_helper(l, indent + 1, depth)
             else:
                 l.append((indent, item))
@@ -668,7 +668,7 @@ class Folder(SilvaObject, Publishable, Folder.Folder):
             if (IContainer.isImplementedBy(item) and
                 item.is_transparent()):
                 l.append((indent, item))
-                if depth == -1 or depth < indent:
+                if depth == -1 or indent < depth:
                     item._get_public_tree_helper(l, indent + 1, depth)
             else:
                 l.append((indent, item))
