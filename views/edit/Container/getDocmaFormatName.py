635dd9f44736017c585fb44e891a4ba6cf8ec32f
Kit Blake <kitblake@infrae.com>
2003-05-12 09:11:21 +0000

    docma in export + i18n + cleanup

--- views/edit/Container/getDocmaFormatName.py
+++ views/edit/Container/getDocmaFormatName.py
@@ -1,11 +1,11 @@
 ##parameters=format
-# $Id: getDocmaFormatName.py,v 1.2 2003/03/24 15:18:24 zagy Exp $
+# $Id: getDocmaFormatName.py,v 1.3 2003/05/12 09:11:21 kitblake Exp $
 
 formats = {
     'silva': 'Silva XML',
-    'word': 'Word Document',
+    'word': 'Word document',
 }
 
-return formats.get(format, 'unknown')
+return formats.get(format, 'unknown format')
 
 
