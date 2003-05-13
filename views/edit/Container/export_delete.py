9371b0a8d94e72affbf63b196e16db989a9d62d4
Kit Blake <kitblake@infrae.com>
2003-05-13 07:00:38 +0000

    the docma_queue is now a macro, with slots for the manipulators, plus a new script for deleting jobs from the export screen

---
+++ views/edit/Container/export_delete.py
@@ -0,0 +1,24 @@
+## Script (Python) "export_delete.py"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=delete_after_import=0
+##title=
+##
+view = context
+request = view.REQUEST
+model = request.model
+
+model.security_trigger()
+
+if not request.has_key('storageids') or not request['storageids']:
+    return view.tab_status_export(message_type='error', message='Select one or more jobs to delete')
+
+for item in request['storageids']:
+    sid, doctype = item.split('|')
+    model.service_docma.delete_finished_job(str(request['AUTHENTICATED_USER']), int(sid))
+
+return view.tab_status_export(message_type='feedback', message='Job deleted')
+
