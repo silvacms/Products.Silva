b3084fe8e516826b23829b14909035fc1885b5d2
Martijn Faassen <faassen@startifact.com>
2003-05-13 17:11:57 +0000

    Minor reformatting.

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.100 $
+# $Revision: 1.101 $
 # Zope
 import Acquisition
 from Acquisition import aq_inner
@@ -52,11 +52,11 @@ class Folder(SilvaObject, Publishable, Folder.Folder, CatalogPathAware):
 
     object_type = 'container'
 
-    # A hackish way, to get a Silva tab in between the standard ZMI tabs
+    # A hackish way to get a Silva tab in between the standard ZMI tabs
     inherited_manage_options = Folder.Folder.manage_options
     manage_options=(
-        (inherited_manage_options[0],)+
-        ({'label':'Silva /edit...', 'action':'edit'},)+
+        (inherited_manage_options[0],) +
+        ({'label':'Silva /edit...', 'action':'edit'},) +
         inherited_manage_options[1:]
         )
 
