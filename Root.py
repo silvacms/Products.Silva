8986fccc476ed10cbb18115686e485ad567b08fd
Guido Wesdorp <info@infrae.com>
2003-05-14 08:43:27 +0000

    Now sets content_version string after upgrade

--- Root.py
+++ Root.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.59 $
+# $Revision: 1.60 $
 # Zope
 from AccessControl import ClassSecurityInfo
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
@@ -189,6 +189,7 @@ class Root(Publication):
         import upgrade
         my_id = self.id
         upgrade.from091to092(self.aq_inner.aq_parent, self)
+        self._content_version = self.get_silva_software_version()
         return "Upgrade of %s succeeded." \
                % my_id
 
