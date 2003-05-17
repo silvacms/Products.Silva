c0834f63d8b35896b8bed07e7c04f8cb2a851132
Infrae <info@infrae.com>
2003-05-17 19:42:44 +0000

    - added an extra "allow_dup" flag to the id check; if this    flag is true the id check does not signal an invalid id,    is a silva object with the same id exists    in the current container. Should fix issue395.  - allow "-" in Silva object ids (issue385)

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.103 $
+# $Revision: 1.104 $
 # Zope
 import Acquisition
 from Acquisition import aq_inner
@@ -631,10 +631,10 @@ class Folder(SilvaObject, Publishable, Folder.Folder, CatalogPathAware):
 
     security.declareProtected(SilvaPermissions.ReadSilvaContent,
                               'is_id_valid')
-    def is_id_valid(self, id):
+    def is_id_valid(self, id, allow_dup=0):
         """Check whether id is valid.
         """
-        return helpers.check_valid_id(self, id) == helpers.IdCheckValues.ID_OK
+        return helpers.check_valid_id(self, id, allow_dup) == helpers.IdCheckValues.ID_OK
 
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
                               'get_default')
--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.13 $
+# $Revision: 1.14 $
 # Zope
 from AccessControl import ModuleSecurityInfo
 # Silva interfaces
@@ -18,7 +18,7 @@ from cgi import escape
 
 module_security =  ModuleSecurityInfo('Products.Silva.helpers')
 
-_id_re = re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_\.]*$')
+_id_re = re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_\.-]*$')
 p_ID = re.compile(r'^(.*?)([0-9]+)$')
 
 # sequence of all reserved prefixes (before the first '_')
@@ -76,7 +76,7 @@ def getNewId(old_id):
 _marker = ()
 
 module_security.declarePublic('check_valid_id')
-def check_valid_id(folder, maybe_id):
+def check_valid_id(folder, maybe_id, allow_dup=0):
     """ test if the given id is valid, returning a status code
         about its validity or reason of invalidity
     """
@@ -94,6 +94,7 @@ def check_valid_id(folder, maybe_id):
     if attr is not _marker:
         if ISilvaObject.isImplementedBy(attr):
             # there is a silva object with the same id
+            if allow_dup: return IdCheckValues.ID_OK
             attr = getattr(folder.aq_base, maybe_id, _marker)
             if attr is _marker:
                 # shadowing a content object is ok (hopefully)
