7e28d92b7d29015cfb4a3ac60e443cf9a4954856
Infrae <info@infrae.com>
2003-05-18 22:03:09 +0000

    drop duplicate "Content-Type" header setting

--- layout/index_html.py
+++ layout/index_html.py
@@ -15,5 +15,4 @@ context.REQUEST.RESPONSE.setHeader('Cache-Control','max-age=300')
 
 # By default, the prefered view method is 'view()', unless
 # a different view_method is passed to this script.
-context.REQUEST.RESPONSE.setHeader('Content-Type', 'text/html;charset=utf-8')
 return getattr(context, renderer)(view_method=view_method)
