207cf01085f322cc7cef50617d9cc3bd22558ae8
Infrae <info@infrae.com>
2003-05-29 13:33:24 +0000

    add a note that the test_cache may fail on slow or loaded machines

--- tests/test_VersionedContent.py
+++ tests/test_VersionedContent.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 import unittest
 import Zope
 Zope.startup()
@@ -180,7 +180,7 @@ class VersionedContentTestCase(unittest.TestCase):
         while DateTime() <= date:
             pass        
         self.document.set_unapproved_version_publication_datetime(DateTime())
-        # expire after 0.1 second ....
+        # expire after 0.1 second .... ugly, may fail on slow computers
         self.document.set_unapproved_version_expiration_datetime(DateTime()+1.0/(3600.0*24.0*10.0))
         date = self.document.get_unapproved_version_expiration_datetime()
         self.document.approve_version()
