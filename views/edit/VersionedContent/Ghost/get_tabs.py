b0a959a8b2964bdee9b7b8994607bc4a96ed7451
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-05-30 07:52:01 +0000

    make ghosts have a metadata tab

--- views/edit/VersionedContent/Ghost/get_tabs.py
+++ views/edit/VersionedContent/Ghost/get_tabs.py
@@ -1,16 +1,8 @@
-## Script (Python) "get_tabs"
-##bind container=container
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
-##parameters=
-##title=
-##
 # define:
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
 tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
+        ('Metadata', 'tab_metadata', 'tab_metadata', '#', '3', '8'),
         ('Publish', 'tab_status', 'tab_status', '%', '3', '8'),
        ]
 
