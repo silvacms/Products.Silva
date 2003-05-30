9730fcdb8bb0c7b8183c03928b6c0df640407451
Martijn Faassen <faassen@startifact.com>
2003-05-30 17:41:38 +0000

    Cache clear with revert to previous now seems to work.

--- Document.py
+++ Document.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.75 $
+# $Revision: 1.76 $
 # Zope
 
 from StringIO import StringIO
@@ -162,6 +162,11 @@ class DocumentVersion(CatalogedVersion):
         self.content.documentElement.writeStream(f)
         export_metadata(self, context)
 
+    def manage_afterClone(self, item):
+        # if we're a copy, clear cache
+        # XXX should be part of workflow system
+        self.service_editor.clearCache(self.content)
+        
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
                               'fulltext')
     def fulltext(self):
