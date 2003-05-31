49821751aa42b29809bbfc6dfdc1d3a60a97284e
Infrae <info@infrae.com>
2003-05-31 11:54:59 +0000

    fix publication dates in the past in "set_approved_version_publication_datetime", too

--- Versioning.py
+++ Versioning.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.33 $
+# $Revision: 1.34 $
 # Zope
 from DateTime import DateTime
 from AccessControl import ClassSecurityInfo
@@ -396,6 +396,8 @@ class Versioning:
         if dt is None:
             raise VersioningError,\
                   'Must specify publication datetime.'
+        if not dt.isFuture():
+            dt = DateTime()
         version_id, publication_datetime, expiration_datetime = \
                     self._approved_version
         self._approved_version = version_id, dt, expiration_datetime
