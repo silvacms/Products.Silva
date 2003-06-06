fad5802aec42274921d02aff3227651fc716ea1f
Guido Wesdorp <info@infrae.com>
2003-06-06 10:03:38 +0000

    Modified the names again to avoid ambiguity with the user lookup scripts (that all use the 'lookup_' prefix)

--- views/edit/obj_lookup_path_mangler.py
+++ views/edit/obj_lookup_path_mangler.py
@@ -1,6 +1,6 @@
 ##bind context=context
 ##parameters=obj_context, obj
-# $Id: lookup_path_mangler.py,v 1.1 2003/06/06 09:57:34 guido Exp $
+# $Id: obj_lookup_path_mangler.py,v 1.1 2003/06/06 10:03:38 guido Exp $
 
 obj_context = obj_context.split('/')
 obj_path = obj.getPhysicalPath()
