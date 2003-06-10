261d64e63dd25cd62a7af95b72003cf99e81ec60
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-06-10 16:38:58 +0000

    Indexer object (all non-versioned content actually) can be deleted now

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.87.2.4 $
+# $Revision: 1.87.2.5 $
 # Zope
 import Acquisition
 from Acquisition import aq_inner
