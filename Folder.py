85bf21f87c7343ff6e9fa0b8c8ce16cc004fb6b9
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-06-10 16:34:23 +0000

    make non-versioned content deletable (e.g. Indexer objects)

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.111 $
+# $Revision: 1.112 $
 # Zope
 import Acquisition
 from Acquisition import aq_inner
@@ -653,10 +653,11 @@ class Folder(SilvaObject, Publishable, Folder.Folder, CatalogPathAware):
            - if it is a container, does not contain anything of the
              above, recursively
         """
-        object = getattr(self, id)
-        if IPublishable.isImplementedBy(object):
+        object = getattr(self, id)        
+        if IVersionedContent.isImplementedBy(object) or IContainer.isImplementedBy(object):
             return not object.is_published() and not object.is_approved()
         else:
+            # XXX non-versioned content should always be deletable.
             return 1
 
 
