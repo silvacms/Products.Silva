62b584d6c695006b5c38dce28e450b37248bab63
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-06-11 15:18:22 +0000

    fixing stupid parenthesis mismatch

--- views/edit/Asset/File/submit.py
+++ views/edit/Asset/File/submit.py
@@ -1,14 +1,6 @@
-## Script (Python) "submit"
-##bind container=container
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
-##parameters=
-##title=
-##
 from Products.Silva.helpers import escape_entities
 from Products.Formulator.Errors import ValidationError, FormValidationError
+
 model = context.REQUEST.model
 view = context
 
@@ -24,7 +16,7 @@ model.sec_update_last_author_info()
 model.set_title(result['file_title'])
 
 changed.append(('title',
-                '%s to %s' % old_title, escape_entities(model.get_title()))))
+                '%s to %s' % (old_title, escape_entities(model.get_title()) ) ))
 
 # FIXME: should put in message
 return view.tab_edit(message_type="feedback", message="Properties changed: %s" % (context.quotify_list_ext(changed)))
