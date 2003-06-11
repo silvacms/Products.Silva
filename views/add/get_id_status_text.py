50f3a575d344b66a4655d77d80ed7bcb64642bd3
Kit Blake <kitblake@infrae.com>
2003-06-11 17:50:28 +0000

    changed textarea widths to 100%, changed language in tips to better describe the relaxed ids

--- views/add/get_id_status_text.py
+++ views/add/get_id_status_text.py
@@ -4,30 +4,30 @@ from Products.Silva.helpers import IdCheckValues
 view = context
 
 if status_code == IdCheckValues.ID_CONTAINS_BAD_CHARS:
-    return """The id should only contain letters, digits and underscore &quot;_&quot; or a period &quot;.&quot;<br>
-Additionally it should start with a letter or digit.<br>
-You entered the id %s which did not match these constraints.""" % view.quotify(id)
+    return """Sorry, bad characters are in the id. It should only contain letters, digits and &quot;_&quot; or &quot;-&quot; or &quot;.&quot;.<br />
+Spaces are not allowed in URLs, and the id should start with a letter or digit.<br />
+You entered the id &#xab;%s&#xbb; which did not match these constraints.""" % view.quotify(id)
 
 elif status_code == IdCheckValues.ID_RESERVED_PREFIX:
     # FIXME: needs to know about the prefix
     prefix = id.split('_')[0]+'_'
-    return """Ids starting with %s are reserved for internal use.<br>
+    return """Sorry, ids starting with &#xab;%s&#xbb; are reserved for internal use.<br />
 Please use another id.""" % view.quotify(prefix)
 
 elif status_code == IdCheckValues.ID_RESERVED:
-    return """The id %s is reserved for internal use.<br>
+    return """Sorry, the id &#xab;%s&#xbb; is reserved for internal use.<br />
 Please use another id.""" % view.quotify(id)
 
 elif status_code == IdCheckValues.ID_IN_USE_CONTENT:
-    return """There is already an object with the id %s in this container.<br>
-Please use another id.""" % view.quotify(id)
+    return """There is already an object with the id &#xab;%s&#xbb; in this container.<br />
+Please use a different one.""" % view.quotify(id)
 
 elif status_code == IdCheckValues.ID_IN_USE_ASSET:
-    return """There is an asset with the id %s in this container.<br>
+    return """There is already an asset with the id &#xab;%s&#xbb; in this container.<br />
 Please use another id.""" % view.quotify(id)
 
 
 # this should not happen
-return """(Internal Error): An invalid status %s occured while checking the id %s.<br>
-Please contact the person responsible for this Silva installation or file a bug report Yourself.""" % (view.quotify(status_code), view.quotify(id))
+return """(Internal Error): An invalid status %s occured while checking the id &#xab;%s&#xbb;.<br />
+Please contact the person responsible for this Silva installation or file a bug report yourself.""" % (view.quotify(status_code), view.quotify(id))
 
