62f4e27e49bfda804a9b4d837852c1b88a84a58b
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-06-12 13:07:16 +0000

    small type fixed

--- views/public/Indexer/render_links.py
+++ views/public/Indexer/render_links.py
@@ -5,7 +5,7 @@ for name, path in links:
     obj = context.restrictedTraverse(path, None)
     if obj is not None:
         url = obj.absolute_url()
-    else obj:
+    else:
         url = '#'
     result.append('<a class="indexer" href="%s">%s</a>' % (url, name))
 return '<br />'.join(result)
