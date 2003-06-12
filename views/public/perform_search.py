829e0ca52ec04f6534882cab3b82c42c5b7464e5
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-06-12 06:30:25 +0000

    search interface

--- views/public/perform_search.py
+++ views/public/perform_search.py
@@ -1,1 +1,22 @@
-return context.service_catalog({'fulltext': unicode(context.REQUEST.query, 'UTF-8'), 'version_status': 'public'})
+from Products.Silva.helpers import escape_entities
+from Products.Formulator.Errors import ValidationError, FormValidationError
+
+request = context.REQUEST
+catalog = context.service_catalog
+model = request.model
+view = context
+
+try:
+    result = view.search_form.validate_all_to_request(request)
+except FormValidationError, e:
+    return None
+
+query = {'version_status': 'public',}
+
+if hasattr(result, 'maintitle'):
+    query['silvamaintitle'] = result['maintitle']
+
+if hasattr(result, 'fulltext'):
+    query['fulltext'] = result['fulltext']
+
+return catalog(query)
