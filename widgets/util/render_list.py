e2e148767078e9c9534f2ab0dbda58828812c14e
Guido Wesdorp <info@infrae.com>
2003-06-18 15:19:05 +0000

    Updated rendering of nobullet lists, they used to be used a for layouting purposes because we lacked a way to do breaks in paragraphs, so we rendered them accordingly (as paragraphs with breaks), but since we do support breaks in paragraphs nowadays the list rendering can be done as expected (as a list without bullets)

--- widgets/util/render_list.py
+++ widgets/util/render_list.py
@@ -11,12 +11,10 @@ model = context.REQUEST.model
 
 tag = None
 
-if type in ['disc', 'square', 'circle']:
+if type in ['disc', 'square', 'circle', 'none']:
     tag = 'ul'
 elif type in ['1', 'I', 'i', 'A', 'a']:
     tag = 'ol'
-elif type == 'none':
-    tag = None
 
 # mapping for XHTML compatibility
 # disc, square, circle are ok 
@@ -28,16 +26,9 @@ elif type == 'none':
 # the class attribute should get the new mapping 
 classes_mapping = {'disc':'disc', 'square':'square', 'circle':'circle', 
                    '1':'decimal', 'I':'upper-roman', 'i':'lower-roman', 
-                   'A':'upper-alpha', 'a':'lower-alpha', }
+                   'A':'upper-alpha', 'a':'lower-alpha', 'none': 'nobullet'}
 mapped_class = classes_mapping.get(type, 'disc')
 
-if tag:
-    content = ''.join(['<li>%s</li>\n' % text for text in texts])
-    return '<%s class="%s">\n%s</%s>' % (
-        tag, mapped_class, content, tag)
-else:
-    if texts:
-        content = '<br />\n'.join(texts)
-        return '<p class="nobullet">\n%s\n</p>' % content
-    else:
-        return ''
+content = ''.join(['<li>%s</li>\n' % text for text in texts])
+return '<%s class="%s">\n%s</%s>' % (
+    tag, mapped_class, content, tag)
