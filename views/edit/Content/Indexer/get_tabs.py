a18c737b0d3e33ec3ae8b11c20a3d75f6058e6e7
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-06-20 09:12:11 +0000

    fixed small bug where 'select_all' wouldn't show in tab_publish when there're now 'approvables'. also made the tab_metadata available for Indexer objects.

--- views/edit/Content/Indexer/get_tabs.py
+++ views/edit/Content/Indexer/get_tabs.py
@@ -1,15 +1,7 @@
-## Script (Python) "get_tabs"
-##bind container=container
-##bind context=context
-##bind namespace=
-##bind script=script
-##bind subpath=traverse_subpath
-##parameters=
-##title=
-##
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
 tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
+        ('Settings', 'tab_metadata', 'tab_metadata', '#', '3', '8'),
         ('Access', 'tab_access', 'tab_access', '#', '3', '8'),
        ]
 
