a1dec112521f9b7b276bc6b000527992165090cd
Martijn Faassen <faassen@startifact.com>
2003-06-30 15:33:52 +0000

    Remove even more.

--- views/add/Document/add_submit_helper.py
+++
@@ -1,8 +0,0 @@
-##parameters=model, id, title, result
-model.manage_addProduct['Silva'].manage_addDocument(id, title)
-document = getattr(model, id)
-
-#subject = result['subject'] or ''
-#document.manage_addProperty('subject', subject, 'string')
-
-return document
