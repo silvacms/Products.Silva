6c1ee1ac0b6e040d1229dd768370019b3a2a6a53
Christian Zagrodnick <cz@gocept.com>
2003-07-22 07:33:17 +0000

    importing Acquisition is not required

--- Folder.py
+++ Folder.py
@@ -1,9 +1,7 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.119 $
+# $Revision: 1.120 $
 # Zope
-import Acquisition
-from Acquisition import aq_inner
 from OFS import Folder, SimpleItem
 from AccessControl import ClassSecurityInfo
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
