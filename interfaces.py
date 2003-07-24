d4cc4b7b521cb981b5c8a6242670bb0184609b2d
Christian Zagrodnick <cz@gocept.com>
2003-07-24 13:02:25 +0000

    allowing interfaces to be imported from untrusted code

--- interfaces.py
+++ interfaces.py
@@ -1,5 +1,9 @@
 import Interface
 
+from AccessControl import ModuleSecurityInfo
+module_security = ModuleSecurityInfo('Products.Silva.interfaces')
+__allow_access_to_unprotected_subobjects__ = 1
+
 class IAccessManager(Interface.Base):
     """Mixin class for objects to request local roles on the object"""
 
