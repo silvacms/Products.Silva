8557fdcfd06a1666065aa6cdd5ebb41b0cb7270a
Kit Blake <kitblake@infrae.com>
2003-07-28 06:18:00 +0000

    added explanation for why no title available

--- VersionedContent.py
+++ VersionedContent.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.38 $
+# $Revision: 1.38.2.1 $
 
 # Python
 from StringIO import StringIO
@@ -65,7 +65,7 @@ class VersionedContent(Content, Versioning, Folder.Folder):
         """
         viewable = self.get_viewable()
         if viewable is None:
-            return "[No title available]"
+            return "[No title is available because the content object is not published]"
         return viewable.get_title()
 
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
@@ -91,7 +91,7 @@ class VersionedContent(Content, Versioning, Folder.Folder):
         # Analogous to get_title
         viewable = self.get_viewable()
         if viewable is None:
-            return "[No (short) title available]"
+            return "[No (short) title is available because the content object is not published]"
         return viewable.get_short_title()
 
     security.declareProtected(
