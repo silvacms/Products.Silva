52c5a71401e4f7031acdc29da24c0e0dcd052605
Kit Blake <kitblake@infrae.com>
2003-07-28 06:08:24 +0000

    updated editor screen, removed metadata tab, improved error messages

--- views/edit/VersionedContent/Ghost/get_tabs.py
+++ views/edit/VersionedContent/Ghost/get_tabs.py
@@ -1,9 +1,8 @@
 # define:
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
-tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
+tabs = [('Editor', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
-        ('Metadata', 'tab_metadata', 'tab_metadata', '#', '3', '8'),
-        ('Publish', 'tab_status', 'tab_status', '%', '3', '8'),
+        ('Publish', 'tab_status', 'tab_status', '#', '3', '8'),
        ]
 
 return tabs
