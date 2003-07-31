39c18c02f05b65756b69b1d32edbd59e08468cf9
Christian Zagrodnick <cz@gocept.com>
2003-07-31 11:19:56 +0000

    allowing assets to be deleted

--- Asset.py
+++ Asset.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.9 $
+# $Revision: 1.10 $
 # Zope
 from Globals import InitializeClass
 from OFS import SimpleItem
@@ -33,5 +33,13 @@ class Asset(SilvaObject, SimpleItem.SimpleItem, CatalogPathAware):
     def manage_afterClone(self, item):
         Asset.inheritedAttribute('manage_afterClone')(self, item)
         self.index_object()
+
+    def is_deletable(self):
+        """assets are delteable
+
+            NOTE: once there is reference management those should only be
+            deletable if not referenced
+        """
+        return 1
         
 InitializeClass(Asset)
