a95fca4b1eaba43b2cf493ddec92b299b71612b9
Kit Blake <kitblake@infrae.com>
2003-08-04 05:15:27 +0000

    a range of formatting adjustments, applied to or resulting from the new Ghost Folders

--- views/edit/Content/Indexer/get_tabs.py
+++ views/edit/Content/Indexer/get_tabs.py
@@ -2,7 +2,7 @@
 tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
         ('Settings', 'tab_metadata', 'tab_metadata', '#', '3', '8'),
-        ('Access', 'tab_access', 'tab_access', '#', '3', '8'),
+        ('Access', 'tab_access', 'tab_access', '$', '4', '9'),
        ]
 
 return tabs
