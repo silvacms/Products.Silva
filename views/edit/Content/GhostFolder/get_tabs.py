21f309d3d5a6245b649288a9c351f1ece10be68b
Kit Blake <kitblake@infrae.com>
2003-08-08 03:52:45 +0000

    Switched the color scheme back to overview blue, and activated the sidebar (since it looks like we're going to keep showing the contents of a Ghost Folder)

--- views/edit/Content/GhostFolder/get_tabs.py
+++ views/edit/Content/GhostFolder/get_tabs.py
@@ -1,9 +1,9 @@
 # define:
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
-tabs = [('Editor', 'tab_edit', 'tab_edit', '!', '1', '6'),
+tabs = [('Haunts', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
         ('Access', 'tab_access', 'tab_access', '#', '3', '8'),
-        ('Status', 'tab_status', 'tab_status', '$', '4', '9'),
+        ('Publish', 'tab_status', 'tab_status', '$', '4', '9'),
        ]
 
 return tabs
--- views/edit/VersionedContent/Ghost/get_tabs.py
+++ views/edit/VersionedContent/Ghost/get_tabs.py
@@ -2,7 +2,8 @@
 # name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
 tabs = [('Editor', 'tab_edit', 'tab_edit', '!', '1', '6'),
         ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
-        ('Publish', 'tab_status', 'tab_status', '#', '3', '8'),
+        ('Access', 'tab_access', 'tab_access', '#', '3', '8'),
+        ('Status', 'tab_status', 'tab_status', '$', '4', '9'),
        ]
 
 return tabs
