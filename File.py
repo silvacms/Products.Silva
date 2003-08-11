a74fdf342a6b1e9d894cda9a5e45d8d7fa42d9b9
Martijn Faassen <faassen@startifact.com>
2003-08-11 13:58:54 +0000

    Files are protected by view permission. This way access to them can be restricted.

--- File.py
+++ File.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.23 $
+# $Revision: 1.24 $
 
 # Python
 import os
@@ -120,7 +120,7 @@ class File(Asset):
 
     # Overide index_html in public presentation templates.
     security.declareProtected(
-        SilvaPermissions.AccessContentsInformation, 'index_html')
+        SilvaPermissions.View, 'index_html')
     def index_html(self, REQUEST=None):
         """Get to file
         """
