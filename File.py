06a8bdb8b243177f45a1746118d9d7db615ff44c
Martijn Faassen <faassen@startifact.com>
2003-08-11 13:59:49 +0000

    Files should be restricted with the View permission, not AccessContentsInformation. This way access to them can be restricted.

--- File.py
+++ File.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.17.2.1 $
+# $Revision: 1.17.2.2 $
 
 # Python
 import os
@@ -118,7 +118,7 @@ class File(Asset):
 
     # Overide index_html in public presentation templates.
     security.declareProtected(
-        SilvaPermissions.AccessContentsInformation, 'index_html')
+        SilvaPermissions.View, 'index_html')
     def index_html(self, REQUEST=None):
         """Get to file
         """
