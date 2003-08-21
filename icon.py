2f36e9fb9ed9a3f418223d41bc8289191e67c5b4
Christian Zagrodnick <cz@gocept.com>
2003-08-21 10:45:38 +0000

    added icon registry
    it is not integrated yet, just the plain registry

---
+++ icon.py
@@ -0,0 +1,135 @@
+# Copyright (c) 2002 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: icon.py,v 1.1 2003/08/21 10:45:37 zagy Exp $
+
+"""Sivla icon registry"""
+
+# python
+import os
+from bisect import insort
+
+# zope
+import Globals
+import OFS.misc_
+
+# Silva
+from Products.Silva.interfaces import \
+    IIcon, IFile, ISilvaObject
+    
+
+class AdaptationError(Exception):
+    """thrown if an object cannot be adapted by an adapter"""
+
+class Adapter:
+    
+    __adapts__ = None
+    
+    def __init__(self, adapt):
+        assert self.__adapts__ is not None
+        if not self.__adapts__.isImplementedBy(adapt):
+            raise AdaptationError, "%r doesn't implement %r" % (
+                adapt, self.__adapts__)
+        self.adapted = adapt
+        
+
+class MetaTypeAdapter(Adapter):
+
+    __implements__ = IIcon
+    __adapts__ = ISilvaObject
+    
+    def getIconIdentifier(self):
+        return ('meta_type', self.adapted.meta_type)
+
+
+class SilvaFileAdapter(Adapter):
+
+    __implements__ = IIcon
+    __adapts__ = IFile
+
+    def getIconIdentifier(self):
+        i = ('mime_type', self.adapted.get_mime_type())
+        try:
+            IconRegistry.getIconByIdentifier(i)
+        except KeyError:
+            return MetaTypeAdapter(self.adapted).getIconIdentifier()
+        else:
+            return i
+
+    
+class _RegistredAdapter:
+
+    def __init__(self, adapter, priority):
+        self.adapter = adapter
+        self.priority = priority
+
+    def __cmp__(self, other):
+        assert isinstance(other, _RegistredAdapter)
+        return -cmp(self.priority, other.priority) # reverse order
+
+
+class _IconRegistry:
+
+    def __init__(self):
+        self._adapters = []
+        self._icon_mapping = {}
+    
+    def getIcon(self, object):
+        adapter = self.getAdapter(object)
+        identifier = adapter.getIconIdentifier()
+        return self.getIconByIdentifier(identifier)
+
+    def getIconByIdentifier(self, identifier):
+        icon = self._icon_mapping.get(identifier, None)
+        if icon is None:
+            raise KeyError, "No icon for %r" % (identifier, )
+        return icon
+
+    def getAdapter(self, object):
+        if IIcon.isImplementedBy(object):
+            # no need to adapt
+            return object
+        for ra in self._adapters:
+            adapter_class = ra.adapter
+            try:
+                adapter = adapter_class(object)
+            except AdaptationError:
+                adapter = None
+            else:
+                break
+        if adapter is None:
+            raise ValueError, "No adapter for %r" % object
+        return adapter
+        
+    def registerAdapter(self, adapter, priority):
+        """registeres adapter with priority
+
+            adapter: adapter instance
+            priority: the larger the higher
+        """
+        insort(self._adapters, _RegistredAdapter(adapter, priority))
+        
+
+    def registerIcon(self, identifier, product, icon, context):
+        """register icon
+
+            identifier: icon identifier as returned from getIconIdentifier()
+            product: the product the icon resides (i.e. 'Silva')
+            icon: path to icon (i.e. 'www/root.png')
+            context: module context of icon (i.e. globals())
+
+            raises ValueError if product doesn't exist
+            NOTE: this will overwrite previous icon declarations
+        """
+        # NOTE: code copied from App.ProductContext, modified though
+        name = os.path.split(icon)[1]
+        icon = Globals.ImageFile(icon, context)
+        icon.__roles__ = None
+        if not hasattr(OFS.misc_.misc_, product):
+            raise ValueError, "The product %r doesn't exist" % product
+        getattr(OFS.misc_.misc_, product)[name] = icon
+        self._icon_mapping[identifier] = 'misc_/%s/%s' % (product, name)
+            
+
+
+IconRegistry = _IconRegistry()
+
---
+++ tests/test_icon.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2002, 2003 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: test_icon.py,v 1.1 2003/08/21 10:45:38 zagy Exp $
+
+import Zope
+Zope.startup()
+
+from ZPublisher.HTTPRequest import FileUpload
+from Globals import ImageFile
+
+import unittest
+from StringIO import StringIO
+
+from Products.Silva import Root
+from Products.Silva.icon import *
+from Products.Silva.interfaces import IAsset
+from Products.Silva.tests.base import SilvaTestCase
+
+
+class AdapterTest(SilvaTestCase):
+
+    def test_MetaTypeAdapter(self):
+        a = MetaTypeAdapter(self.silva)
+        self.assertEquals(a.getIconIdentifier(), ('meta_type', 'Silva Root'))
+        self.assertRaises(AdaptationError, MetaTypeAdapter, "astring")
+
+
+class R:
+    pass
+    
+class RegistryTest(SilvaTestCase):
+
+    def setUp(self):
+        SilvaTestCase.setUp(self)
+        fields = R()
+        fields.filename = 'afilename'
+        fields.headers = {'content-type': 'application/pdf'}
+        fields.file = StringIO("a nice pdf ;)")
+        upload = FileUpload(fields)
+        self.silva.manage_addProduct['Silva'].manage_addFile('fileasset',
+            'a nice title', upload)
+        fields = R()
+        fields.filename = 'anoterfilename'
+        fields.headers = {}
+        fields.file = StringIO("just plain data")
+        upload = FileUpload(fields)
+        self.silva.manage_addProduct['Silva'].manage_addFile('datafile',
+            'data file', upload)
+    
+    def test_registry(self):
+        self.assertEquals(self.silva.fileasset.get_mime_type(),
+            'application/pdf')
+        r = IconRegistry
+        r.registerAdapter(MetaTypeAdapter, 0)
+        r.registerAdapter(SilvaFileAdapter, 10)
+        self.assertEquals(len(r._adapters), 2)
+        self.assertEquals(r._adapters[0].priority, 10)
+        self.assertEquals(r._adapters[1].priority, 0)
+        adapter = r.getAdapter(self.silva.fileasset)
+        self.assert_(isinstance(adapter, SilvaFileAdapter))
+        adapter = r.getAdapter(self.silva)
+        self.assert_(isinstance(adapter, MetaTypeAdapter))
+        
+        self.assertRaises(ValueError, r.registerIcon,
+            ('meta_type', 'Silva Root'), 'ProductWhichDoesNotExist',
+            'www/members.png', Root.__dict__)
+            
+        r.registerIcon(('meta_type', 'Silva Root'), 'Silva',
+            'www/members.png', Root.__dict__)
+        r.registerIcon(('meta_type', 'Silva File'), 'Silva',
+            'www/silvafile.png', Root.__dict__)
+        r.registerIcon(('mime_type', 'application/pdf'), 'Silva',
+            'www/user.png', Root.__dict__)
+            
+        icon = r.getIcon(self.silva)
+        self.assertEquals(icon, 'misc_/Silva/members.png')
+        icon = r.getIcon(self.silva.fileasset)
+        self.assertEquals(icon, 'misc_/Silva/user.png')
+        icon = r.getIcon(self.silva.datafile)
+        self.assertEquals(icon, 'misc_/Silva/silvafile.png')
+        
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(AdapterTest))
+    suite.addTest(unittest.makeSuite(RegistryTest))
+    return suite
+
+def main():
+    unittest.TextTestRunner().run(test_suite())
+
+if __name__ == '__main__':
+    main()
+    
