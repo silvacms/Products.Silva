f52fb3803e740346d983efc81c742d5dbea04c75
Christian Zagrodnick <cz@gocept.com>
2003-08-26 14:05:07 +0000

    added get reference to ghost folder and add views

---
+++ views/edit/Container/GhostFolder/get_start_container.py
@@ -0,0 +1,19 @@
+## Script (Python) "get_start_container"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=Lookup the container to start listing assets
+##
+request = context.REQUEST
+model = request.model
+assert model.meta_type == 'Silva Ghost Folder', '%r is no ghost folder' % model
+status = model.get_link_status()
+if status == model.LINK_OK:
+    content = model.get_content()
+    if content is not None:
+        return content.get_container()
+return model.aq_parent.get_container()
+
