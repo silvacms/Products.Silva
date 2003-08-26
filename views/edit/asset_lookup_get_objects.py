7311b42946071bd62177d48e76f4150326021b83
Christian Zagrodnick <cz@gocept.com>
2003-08-26 13:26:41 +0000

    more possibilities to filter

--- views/edit/asset_lookup_get_objects.py
+++ views/edit/asset_lookup_get_objects.py
@@ -13,6 +13,14 @@ model = context.REQUEST.model
 if filter == 'Asset':
     return model.get_assets()
 if filter == 'Content':
+    return [ content
+        for content in model.get_ordered_publishables()
+        if content.implements_content()]
+if filter == 'Container':
+    return [ content
+        for content in model.get_ordered_publishables()
+        if content.implements_container()]
+if filter == 'Publishables':
     return model.get_ordered_publishables()
 return model.objectValues(filter)
 
