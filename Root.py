55e784699b37af78acec3a15df37014e405a1943
Christian Zagrodnick <cz@gocept.com>
2003-09-29 08:16:13 +0000

    getting real content version

--- Root.py
+++ Root.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.73 $
+# $Revision: 1.74 $
 
 # Zope
 from AccessControl import ClassSecurityInfo
@@ -187,7 +187,7 @@ class Root(Publication):
             returns nothing
             an exception is raised on error
         """
-        from_version = '0.9.2' # self.get_silva_content_version()
+        from_version = self.get_silva_content_version()
         to_version = self.get_silva_software_version()
         from Products.Silva import upgrade
         upgrade.registry.upgrade(self, from_version, to_version)
