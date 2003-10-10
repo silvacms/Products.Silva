bda85e59b51ece31fd3fba6d1d1d4b4a4a397128
Martijn Faassen <faassen@startifact.com>
2003-10-10 18:04:45 +0000

    Made it work with the new framework.

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Id: test_file.py,v 1.1 2003/02/17 14:55:39 zagy Exp $
+# $Id: test_file.py,v 1.2 2003/10/10 18:04:45 faassen Exp $
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
 
-import unittest
+import SilvaTestCase
 
-import Zope
-Zope.startup()
 from Products.Silva import File
 
-
-class FileTest(unittest.TestCase):
+class FileTest(SilvaTestCase.SilvaTestCase):
    
     def test_cookpath(self):
         
@@ -21,17 +21,14 @@ class FileTest(unittest.TestCase):
             ('foo', 'bar', 'baz'))
         self.assertEqual(File.cookPath("foo/bar/./baz"), 
             ('foo', 'bar', 'baz'))
-        
     
-    
-def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(FileTest))
-    return suite
-
-def main():
-    unittest.TextTestRunner().run(test_suite())
-
 if __name__ == '__main__':
-    main()
-    
+    framework()
+else:
+    # While framework.py provides its own test_suite()
+    # method the testrunner utility does not.
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(FileTest))
+        return suite
