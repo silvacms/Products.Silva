54d1853a2fc5d30181857b7597d8d00991afdebb
Martijn Faassen <faassen@startifact.com>
2003-10-11 18:43:32 +0000

    test_SimpleMembership is also back in the testing business.

--- tests/test_SimpleMembership.py
+++ tests/test_SimpleMembership.py
@@ -1,44 +1,28 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.2 $
-import unittest
-import Zope
-#import ZODB
-#import OFS.Application
-from Testing import makerequest
-# access "internal" class to fake request authentification
-from AccessControl.User import SimpleUser
-#from Products.Silva import Document, Folder, Root #, Ghost, Publication
+# $Revision: 1.3 $
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
 
-class SimpleMembershipTestCase(unittest.TestCase):
+import SilvaTestCase
+
+class SimpleMembershipTestCase(SilvaTestCase.SilvaTestCase):
     """Test simple membership.
     """
 
-    def setUp(self):
-        get_transaction().begin()
-        self.connection = Zope.DB.open()
-        self.root = makerequest.makerequest(
-            self.connection.root()['Application'])
-        self.root.manage_addProduct['Silva'].manage_addRoot('root', 'Root')
-        self.sroot = self.root.root
-        self.sroot.manage_addFolder('Members')
-        self.sroot.manage_addProduct['Silva'].manage_addSimpleMemberService(
-            'service_members')
-        self.members = self.sroot.Members
+    def afterSetUp(self):
+        self.members = self.root.Members
         # add user folder and some test users
-        self.sroot.manage_addUserFolder()
-        acl_users = self.sroot.acl_users
+        self.root.manage_addUserFolder()
+        acl_users = self.root.acl_users
         acl_users._addUser('alpha', 'alpha', 'alpha', [], [])
         acl_users._addUser('beta', 'beta', 'beta', [], [])
         acl_users._addUser('gamma', 'gamma', 'gamma', [], [])
         acl_users._addUser('alpha2', 'alpha2', 'alpha2', [], [])
-
-    def tearDown(self):
-        get_transaction().abort()
-        self.connection.close()
     
     def test_is_user(self):
-        service_members = self.sroot.service_members
+        service_members = self.root.service_members
         self.assert_(service_members.is_user('alpha'))
         self.assert_(service_members.is_user('beta'))
         self.assert_(service_members.is_user('gamma'))
@@ -46,13 +30,13 @@ class SimpleMembershipTestCase(unittest.TestCase):
         self.assert_(not service_members.is_user('delta'))
         
     def test_get_member(self):
-        service_members = self.sroot.service_members
+        service_members = self.root.service_members
         self.assertEquals(service_members.get_member('alpha').userid(), 'alpha')
         self.assertEquals(service_members.get_member('beta').userid(), 'beta')
         self.assertEquals(service_members.get_member('delta'), None)
 
     def test_find_members(self):
-        service_members = self.sroot.service_members
+        service_members = self.root.service_members
         members = service_members.find_members('beta')
         self.assertEquals(len(members), 1)
         self.assertEquals(members[0].userid(), 'beta')
@@ -64,14 +48,12 @@ class SimpleMembershipTestCase(unittest.TestCase):
                      (members[0].userid() == 'alpha2' and
                       members[1].userid() == 'alpha'))
     
-
-def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(SimpleMembershipTestCase, 'test'))
-    return suite
-    
-def main():
-    unittest.TextTestRunner().run(test_suite())
-
 if __name__ == '__main__':
-    main()
+    framework()
+else:
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(SimpleMembershipTestCase))
+        return suite
+
