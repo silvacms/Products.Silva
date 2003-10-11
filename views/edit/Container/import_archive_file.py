229c1a3a0649f8946ca3b7350664663abef0a55a
Kit Blake <kitblake@infrae.com>
2003-10-11 06:55:54 +0000

    fixed error if no file was selected

--- views/edit/Container/import_archive_file.py
+++ views/edit/Container/import_archive_file.py
@@ -3,8 +3,8 @@ request = view.REQUEST
 model = request.model
 
 if not request.has_key('importfile') or not request['importfile']:
-    return view.tab_edit_import_xml(
-        message_type='error', message='Select a file for upload')
+    return view.tab_edit_import(
+        message_type='error', message='Select a file for upload.')
 
 feedback = model.archive_file_import(
     request.get('importfile'), 
@@ -21,7 +21,7 @@ else:
     msg = []
     message_type='feedback'
     if succeeded:    
-        msg.append('Added %s' % view.quotify_list(succeeded))
+        msg.append('added %s' % view.quotify_list(succeeded))
     else:
         message_type='alert'
 
