7479a04bde93551a49b04e78da9ec350ad69ebdf
Martijn Faassen <faassen@startifact.com>
2003-10-11 19:11:57 +0000

    Some code cleanups.

--- tests/test_CatalogedVersioning.py
+++ tests/test_CatalogedVersioning.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.6 $
+# $Revision: 1.7 $
 import os, sys
 if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
@@ -20,9 +20,6 @@ from Products.SilvaDocument.Document import Document, DocumentVersion
 
 # XXX ugh would really like to avoid this..
 Document.cb_isMoveable = lambda self: 1
-
-def _verifyObjectPaste(self, ob):
-    return
 
 class CatalogedVersioningTestCase(SilvaTestCase.SilvaTestCase):
     def afterSetUp(self):
@@ -131,7 +128,3 @@ else:
         suite = unittest.TestSuite()
         suite.addTest(unittest.makeSuite(CatalogedVersioningTestCase))
         return suite
-
-
-if __name__ == '__main__':
-    main()
