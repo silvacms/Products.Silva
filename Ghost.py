bbe0d1f7db14b1936ff0326cd4549066573bc829
Christian Zagrodnick <cz@gocept.com>
2003-10-14 15:07:24 +0000

    renaming comes a bit later, checked in wrong file before

--- Ghost.py
+++ Ghost.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.75 $
+# $Revision: 1.76 $
 
 # Zope
 from OFS import SimpleItem
@@ -82,8 +82,8 @@ class GhostBase:
 
 
     security.declareProtected(
-        SilvaPermissions.ChangeSilvaContent, 'set_haunted_url')
-    def set_haunted_url(self, content_url):
+        SilvaPermissions.ChangeSilvaContent, 'set_content_url')
+    def set_content_url(self, content_url):
         """Set content url.
         """
         # simplify url
