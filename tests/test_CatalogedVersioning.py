fcd10babca6943be2206cecbb623a7b40378db4d
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-10-17 14:48:41 +0000

    Removed assertions for closed versions in catalog (they shouldn't be in the catalog anymore).

--- tests/test_CatalogedVersioning.py
+++ tests/test_CatalogedVersioning.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.7 $
-import os, sys
+# $Revision: 1.8 $
+import os, sys, time
 if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
@@ -91,21 +91,17 @@ class CatalogedVersioningTestCase(SilvaTestCase.SilvaTestCase):
         self.assertInCatalog([version], version_status='public')
         self.test.close_version()
         self.assertEquals('last_closed', version.version_status())
-        self.assertInCatalog([version], version_status='last_closed')
         self.test.create_copy()
         new_version = self.test.get_editable()
-        self.assertInCatalog([version], version_status='last_closed')
         self.assertInCatalog([new_version], version_status='unapproved')
         self.test.set_unapproved_version_publication_datetime(now - 1)
         self.test.approve_version()
         self.assertInCatalog([new_version], version_status='approved')
         self.test.close_version()
-
         self.assertEquals('last_closed', new_version.version_status())
         self.assertEquals('closed', version.version_status())
-        self.assertInCatalog([new_version], version_status='last_closed')
-        self.assertInCatalog([version], version_status='closed')     
-
+        self.assertNotInCatalog([version], version_status='closed')     
+        
     def test_move(self):
         version = self.test.get_editable()
         self.assertInCatalog([self.test.get_editable()])
