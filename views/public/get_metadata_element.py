39155cdd05b5b4c22cde6cf51df97c12cef0c42f
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-11-17 14:54:14 +0000

    Move the files to the location they belong.

---
+++ views/public/get_metadata_element.py
@@ -0,0 +1,23 @@
+from Products.SilvaMetadata.Exceptions import BindingError
+
+# Get the data for a particular element of a particular set for
+# the viewable version of this object.
+
+request = context.REQUEST
+content = context.get_viewable()
+
+if content is None:
+    return None
+
+ms = context.service_metadata
+
+try:
+    binding = ms.getMetadata(content)
+except BindingError, be:
+    # No binding found..
+    return None
+
+if binding is None:
+    return None
+
+return binding.get(set_name, element_name)
