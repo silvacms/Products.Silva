21e1b139a3735dd17e47291426c903ad4772a0fa
Kit Blake <kitblake@infrae.com>
2003-11-23 12:55:55 +0000

    language

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.149 $
+# $Revision: 1.150 $
 
 # Zope
 from OFS import Folder, SimpleItem
@@ -46,7 +46,8 @@ class Folder(CatalogPathAware, SilvaObject, Publishable, Folder.Folder):
        hierarchy that a Visitor sees. Folders on the top level
        define sections of a publication, subfolders define chapters, etc.
        Note that unlike publications, folders are transparent, meaning you
-       can see through them in the Contents and Publish screens.
+       can see through them in the sidebar tree navigation and the Publish 
+       screen.
     """
     security = ClassSecurityInfo()
 
