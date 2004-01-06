8c04405bd74aaa63044f772b9107aadca0df2261
Guido Wesdorp <info@infrae.com>
2004-01-06 16:34:24 +0000

    Added some code to invalidate the widget cache on revert to previous.

--- views/edit/VersionedContent/tab_status_revert_to_previous.py
+++ views/edit/VersionedContent/tab_status_revert_to_previous.py
@@ -11,4 +11,11 @@ model = context.REQUEST.model
 model.sec_update_last_author_info()
 view = context
 model.revert_to_previous()
+#invalidate the cache
+try:
+    del context.REQUEST.SESSION['xmlwidgets_service_editor']
+except AttributeError:
+    pass
+except KeyError:
+    pass
 return view.tab_status(message_type="feedback", message="Reverted to previous version.")
