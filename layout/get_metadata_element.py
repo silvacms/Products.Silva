47ec53dace1f821e18f5e95da4a19ec71a0f5da9
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2004-01-06 09:08:05 +0000

    I think we should get the REQUEST from the context object, not from the container, if only for consistency sake.
    Added more comments on the use of this script.

--- layout/get_metadata_element.py
+++ layout/get_metadata_element.py
@@ -4,10 +4,16 @@ from Products.SilvaMetadata.Exceptions import BindingError
 # This is a copy of views/public/get_metadata_element used in the SMI.
 # FIXME: do we need a better location for this functionality?
 #
+# One way to get to the get_metadata_element in the "public" view of
+# a Silva object is this:
+#
+#   method = obj.public['get_metadata_element']
+#   data = method(setname, elementname)
+#
 # Get the data for a particular element of a particular set for
 # the viewable version of this object.
 
-request = container.REQUEST
+request = context.REQUEST
 content = context.get_viewable()
 
 if content is None:
