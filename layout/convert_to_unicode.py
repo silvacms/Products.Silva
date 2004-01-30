204486122e3ae263345d18313a196097b1f8ca45
Guido Wesdorp <info@infrae.com>
2004-01-30 16:39:41 +0000

    Script to convert arbitrary incoming data into a unicode string.

---
+++ layout/convert_to_unicode.py
@@ -0,0 +1,46 @@
+"""Python script to convert incoming data to Unicode
+
+Incoming data can either be a single or a multi-dimension datatype.
+"""
+
+def convert_tuple(t, charset):
+    """Convert a tuple to unicode (recursive)"""
+    new = []
+    for item in t:
+        new.append(convert_item(item, charset))
+    return tuple(new)
+
+def convert_list(l, charset):
+    """Convert a list to unicode (recursive)"""
+    new = []
+    for item in l:
+        new.append(convert_item(item, charset))
+    return new
+
+def convert_dict(d):
+    """Convert a dict to unicode (recursive)"""
+    new = {}
+    for k, v in d.keys():
+        new[convert_item(k, charset)] = convert_item(v, charset)
+    return new
+
+# yes, it's nasty, but our scripts always expect a string back...
+def convert_item(i, charset):
+    """Convert an item to unicode"""
+    if same_type(i, []):
+        return '[\'%s\']' % '\', \''.join(convert_list(i, charset))
+    elif same_type(i, ()):
+        return '(\'%s\')' % '\', \''.join(convert_tuple(i, charset))
+    elif same_type(i, {}):
+        return '{\'%s\'}' % '\', \''.join(['%s\': \'%s' % (k, v) for k, v in convert_dict(i, charset).items()])
+    elif same_type(i, u''):
+        return i
+    elif same_type(i, ''):
+        return unicode(i, charset)
+    else:
+        # basic datatype or unknown datatype, convert to
+        # unicode string and return
+        return unicode(str(i), charset)
+
+return convert_item(incoming, charset)
+
