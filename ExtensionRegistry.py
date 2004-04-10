1a1129fe22d6eca32ff20ce89583648ca35b511d
Kit Blake <kitblake@infrae.com>
2004-04-10 08:41:43 +0000

    + comment for new list ordering, so it's easier to find next time

--- ExtensionRegistry.py
+++ ExtensionRegistry.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.7.6.1 $
+# $Revision: 1.7.6.1.12.1 $
 
 from bisect import insort_right
 
@@ -50,6 +50,10 @@ class ExtensionRegistry:
         version_classname = classname + 'Version'
         __traceback_info__ = (module, classname, version_classname)
         meta_type = klass.meta_type
+        # The order of the items in the contents screen new list is
+        # determined by the addable_priority, defined in each item's
+        # py file. Might be better if this was centrally defined.
+        # Silva News items begin at 3 (the current bottom).
         priority = getattr(module, 'addable_priority', 0)
         # Register Silva Addable
         context.registerClass(
