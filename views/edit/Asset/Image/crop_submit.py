95a5ba21e3b6bb523ed2ab4986b2ac90204a2ee8
Kit Blake <kitblake@infrae.com>
2004-04-16 05:22:38 +0000

    language

--- views/edit/Asset/Image/crop_submit.py
+++ views/edit/Asset/Image/crop_submit.py
@@ -17,7 +17,7 @@ y = result['y']
 if model.canScale() and model.canCrop(size, x, y):
     model.cropImage(size, x, y)
 else:
-    return view.tab_edit(message_type='error', message='The Image can not\
+    return view.tab_edit(message_type='error', message='The image can not\
  be cropped with the given coordinates. Maybe you selected one of the edges\
- of the image or you have choosen to big dimensions?')
+ of the image or the dimensions are too large.')
 return view.tab_edit(message_type=msg_type, message=' '.join(msg))
