bd1242de928e84b3cbc5ad251d6913219bbd3e9f
Infrae <info@infrae.com>
2004-04-29 15:22:40 +0000

    - removed overwriting title with an empty string, which fixes issue #869

--- SimpleMembership.py
+++ SimpleMembership.py
@@ -150,9 +150,8 @@ class SimpleMemberService(SimpleItem.SimpleItem):
     security.declareProtected('View management screens', 'manage_main')
     manage_main = manage_editForm
 
-    def __init__(self, id, title):
+    def __init__(self, id):
         self.id = id
-        self.title = title
         self._allow_authentication_requests = 0
 
     # XXX will be used by access tab and should be opened wider if this
@@ -228,9 +227,9 @@ manage_addSimpleMemberServiceForm = PageTemplateFile(
     "www/simpleMemberServiceAdd", globals(),
     __name__='manage_addSimpleMemberServiceForm')
 
-def manage_addSimpleMemberService(self, id, title='', REQUEST=None):
+def manage_addSimpleMemberService(self, id, REQUEST=None):
     """Add a Simple Member Service."""
-    object = SimpleMemberService(id, title)
+    object = SimpleMemberService(id)
     self._setObject(id, object)
     add_and_edit(self, id, REQUEST)
     return ''
