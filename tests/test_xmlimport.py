29e77b6a7c251a0ae51d4d66e5c4cb53653ea38c
Eric Casteleijn <ec@trapit.com>
2004-05-11 10:31:24 +0000

    added test for xml import of document and folder

--- tests/test_xmlimport.py
+++ tests/test_xmlimport.py
@@ -13,6 +13,21 @@ from Products.Silva.silvaxml import silva_import
 from Products.Silva.silvaxml.xmlimport import SaxImportHandler
 
 class SetTestCase(SilvaTestCase.SilvaTestCase):
+    def test_folder_import(self):
+        importfolder = self.add_folder(
+            self.root,
+            'importfolder',
+            'This is <boo>a</boo> testfolder',
+            policy_name='Auto TOC')
+        silva_import.initializeElementRegistry()
+        source_file = open('data/test_folder.xml', 'r')
+        handler = SaxImportHandler(importfolder)
+        parser = xml.sax.make_parser()
+        parser.setFeature(feature_namespaces, 1)
+        parser.setContentHandler(handler)
+        parser.parse(source_file)
+        source_file.close()
+        
     def test_document_import(self):
         importfolder = self.add_folder(
             self.root,
@@ -27,6 +42,19 @@ class SetTestCase(SilvaTestCase.SilvaTestCase):
         parser.setContentHandler(handler)
         parser.parse(source_file)
         source_file.close()
+        document_version = importfolder.testfolder.test_document.get_editable()
+        self.assertEquals(
+            document_version.get_title(),
+            'This is (surprise!) a document'
+            )
+        metadata_service = getToolByName(document_version, 'portal_metadata')
+        binding = metadata_service.getMetadata(document_version)
+        self.assertEquals(
+            binding._getData('silva-extra').data['location'],
+            'http://nohost/root/testfolder/test_document')
+        doc = document_version.content.documentElement.__str__()
+        self.assertEquals(doc,
+        u'<doc>\n            <p>\n            <em foo="bar">\u627f\u8afe\u5e83\u544a\uff0a\u65e2\u306b\u3001\uff12\u5104\u3001\uff13\u5104\u3001\uff15\u5104\uff19\u5343\u4e07\u5186\u53ce\u5165\u8005\u304c\u7d9a\u51fa<strong>boo</strong>\n              baz</em>\n            </p>\n          </doc>')
         
     def test_link_import(self):
         importfolder = self.add_folder(
