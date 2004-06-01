5c1c6c2411fd8e20f74aec4749341356a1fd6bf4
Guido Wesdorp <info@infrae.com>
2004-06-01 13:11:37 +0000

    Removed debug print message ('kupu imported')

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.112.6.6.6.7 $
+# $Revision: 1.112.6.6.6.8 $
 
 import ContainerPolicy
 
@@ -145,7 +145,6 @@ def initialize(context):
     except ImportError:
         pass
     else:
-        print 'kupu imported'
         registerDirectory('%s/common' % os.path.dirname(kupu.__file__), globals())
         registerDirectory('%s/silva' % os.path.dirname(kupu.__file__), globals())
 
