cfcdf2f72b87b23d884600c003c76148326cdea7
Infrae <info@infrae.com>
2004-07-15 14:07:09 +0000

    This commit was manufactured by cvs2svn to create tag 'Silva-1_0'.

--- views/edit/Content/SimpleContent/get_tabs.py
+++
@@ -1,8 +0,0 @@
-# name, id/None, up_id, toplink_accesskey, tab_accesskey, uplink_accesskey
-tabs = [('Edit', 'tab_edit', 'tab_edit', '!', '1', '6'),
-        ('Preview', 'tab_preview', 'tab_preview', '@', '2', '7'),
-        ('Settings', 'tab_metadata', 'tab_metadata', '#', '3', '8'),
-        ('Access', 'tab_access', 'tab_access', '#', '3', '8'),
-       ]
-
-return tabs
