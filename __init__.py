4136d8e9d43be295eda0b2b2dff6673114cb6f03
Guido Wesdorp <info@infrae.com>
2004-08-06 13:39:10 +0000

    Added a bit of code to allow using the Sprout package as a Zope Product rather then installing it on the PYTHONPATH.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.117 $
+# $Revision: 1.118 $
 
 import ContainerPolicy
+
+try:
+    # some people may have put Sprout in the Products directory
+    # rather then somewhere in the PYTHONPATH, this makes Silva
+    # import it
+    import Products.Sprout
+except ImportError:
+    pass
 
 def initialize(context):
     from Products.Silva import icon
