9d09c876f9f9ced837da105484bba00b574941ef
Infrae <info@infrae.com>
2004-08-10 00:12:16 +0000

    New files. The thing to adapt a Silva Document to something that can be rendered with a renderer.
    THE TESTS JUST BROKE DUE TO RECENT CHANGES! I'm checking it in so that if Eric
    wants to work on hooking up his stylesheet to the ImagesOnRight renderer,
    this code'll be there for him to use, otherwise I intend to fix it tomorrow
    morning myself.

---
+++ adapters/renderable.py
@@ -0,0 +1,21 @@
+from Products.Silva.adapters import adapter
+from Products.Silva.transform.interfaces import IRenderable
+from Products.Silva.transform.Exceptions import InvalidRendererError
+
+class RenderableAdapter(adapter.Adapter):
+
+    __implements__ = IRenderable
+
+    def setRendererId(self, renderer_id):
+        metadata_system = self.context.service_metadata
+        binding = metadata_system.getMetadata(self.context.get_editable())
+        binding.setValues(
+            'silva-extra', {'renderer_id' : renderer_id}, reindex = 1)
+
+    def getRendererId(self):
+        metadata_system = self.context.service_metadata
+        binding = metadata_system.getMetadata(self.context.get_editable())
+        return binding['silva-extra']['renderer_id']
+
+    def getMetaType(self):
+        pass
