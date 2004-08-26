873d27d3a9f889fc96632b90bd83d675ccdf6a1b
Infrae <info@infrae.com>
2004-08-26 20:45:34 +0000

    Added unit test to ensure that the correct exception is getting raised when libxml/libxsl2 raises an exception.

--- tests/test_render_images_on_right.py
+++ tests/test_render_images_on_right.py
@@ -11,6 +11,7 @@ import SilvaTestCase
 from Products.Silva.silvaxml import xmlimport
 from Products.Silva.transform.interfaces import IRenderer
 from Products.Silva.transform.renderers.RenderImagesOnRight import RenderImagesOnRight
+from Products.Silva.transform.renderers.XSLTRendererBase import RenderError
 
 class RenderImagesOnRightTest(SilvaTestCase.SilvaTestCase):
 
@@ -50,6 +51,36 @@ class RenderImagesOnRightTest(SilvaTestCase.SilvaTestCase):
         images_on_right = RenderImagesOnRight()
         self.assertEquals(images_on_right.render(obj), '<?xml version="1.0"?>\n<table><tr><td valign="top">unapproved<h2 class="heading">This is a rendering test</h2><p xmlns:doc="http://infrae.com/ns/silva_document" xmlns:silva="http://infrae.com/ns/silva" class="p">This is a test of the XSLT rendering functionality.</p></td><td valign="top"><a href="bar.html"><img src="foo"/></a><br/></td></tr></table>\n')
 
+    def test_error_handling(self):
+        try:
+            import libxslt
+        except ImportError:
+            return
+
+        class BrokenRenderImagesOnRight(RenderImagesOnRight):
+            def __init__(self):
+                RenderImagesOnRight.__init__(self)
+                self._stylesheet_path = "data/images_to_the_right_broken.xslt"
+
+        importfolder = self.add_folder(
+            self.root,
+            'silva_xslt',
+            'This is <boo>a</boo> testfolder',
+            policy_name='Auto TOC')
+        xmlimport.initializeXMLImportRegistry()
+        importer = xmlimport.theXMLImporter
+        test_settings = xmlimport.ImportSettings()
+        test_info = xmlimport.ImportInfo()
+        source_file = open("data/test_document2.xml")
+        importer.importFromFile(
+            source_file, result = importfolder,
+            settings = test_settings, info = test_info)
+        source_file.close()
+        # XXX get a (which?) version
+        obj = self.root.silva_xslt.test_document
+
+        images_on_right = BrokenRenderImagesOnRight()
+        self.assertRaises(RenderError, images_on_right.render, obj)
 
 if __name__ == '__main__':
     framework()
