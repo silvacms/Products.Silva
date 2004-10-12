76b6b2763e2ef2ecd62e53e5406adcb52925859a
Walco Van Loon <info@infrae.com>
2004-10-12 09:07:27 +0000

    Fix for incorrect behaviour of get_public_tree_all() when recursing.

--- Folder.py
+++ Folder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.159 $
+# $Revision: 1.160 $
 
 # Zope
 from OFS import Folder, SimpleItem
@@ -806,7 +806,7 @@ class Folder(CatalogPathAware, SilvaObject, Publishable, Folder.Folder):
                 (item.is_transparent() or include_non_transparent_containers)):
                 l.append((indent, item))
                 if depth == -1 or indent < depth:
-                    item._get_public_tree_helper(l, indent + 1, depth)
+                    item._get_public_tree_helper(l, indent + 1, depth, include_non_transparent_containers)
             else:
                 l.append((indent, item))
 
