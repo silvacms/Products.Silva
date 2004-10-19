dbed1dc74842da9583c560e721bd8616f7597fda
Eric Casteleijn <ec@trapit.com>
2004-10-19 08:46:33 +0000

    changed icon for Renderer Service

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.126 $
+# $Revision: 1.127 $
 
 import ContainerPolicy
 
@@ -87,7 +87,7 @@ def initialize(context):
         RendererRegistryService.RendererRegistryService,
         constructors = (RendererRegistryService.manage_addRendererRegistryServiceForm,
                         RendererRegistryService.manage_addRendererRegistryService),
-        icon = 'www/extension_service.gif'
+        icon = 'www/renderer_service.png'
         )
 
     context.registerClass(
