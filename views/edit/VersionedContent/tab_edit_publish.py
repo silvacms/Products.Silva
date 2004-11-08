bfdf4c7314c2b321f5ece2597618e1dbd094fd24
Guido Wesdorp <info@infrae.com>
2004-11-08 18:07:33 +0000

    Added a new button in the top button row ('middleground') of VersionedContent's tab_edit with the text 'open to public now...', which is only visible for people with ApproveSilvaContent rights and only if the VersionedContent object has an unapproved version, and allows to publish an object right from tab_edit. Also added a script that performs a publish action for that purpose, publishes a version directly, and without an expiration time.

---
+++ views/edit/VersionedContent/tab_edit_publish.py
@@ -0,0 +1,23 @@
+model = context.REQUEST.model
+view = context
+
+if not model.get_unapproved_version():
+    # SHORTCUT: To allow approval of closed docs with no new version available,
+    # first create a new version. This "shortcuts" the workflow.
+    # See also edit/Container/tab_status_approve.py
+    if model.is_version_published():
+        return view.tab_edit(
+            message_type="error", 
+            message="There is no unapproved version to approve.")
+    model.create_copy()
+
+import DateTime
+
+model.set_unapproved_version_publication_datetime(DateTime.DateTime())
+
+model.approve_version()
+
+if hasattr(model, 'service_messages'):
+    model.service_messages.send_pending_messages()
+    
+return view.tab_edit(message_type="feedback", message="Version approved.")
