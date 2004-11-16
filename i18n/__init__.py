e5d3cbc515d817a83e15e52b73047c619b6c34dd
Walco Van Loon <info@infrae.com>
2004-11-16 15:33:26 +0000

    Changed policy to use explicitly use unicode strings in i18n translations.

--- i18n/__init__.py
+++ i18n/__init__.py
@@ -1,5 +1,5 @@
 try:
     from Products.PlacelessTranslationService.MessageID import MessageIDFactory
-    translate = MessageIDFactory('silva')
+    translate = MessageIDFactory('silva', True)
 except ImportError:
     translate = lambda x: x
