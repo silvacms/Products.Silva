70c327630813449cc430fbb46cff9134d7af054b
Walco Van Loon <info@infrae.com>
2004-11-23 09:04:03 +0000

    Fixed broken i18n-ing.

--- views/edit/Container/tab_edit_paste.py
+++ views/edit/Container/tab_edit_paste.py
@@ -13,7 +13,7 @@ model = context.REQUEST.model
 view = context
 
 if not context.REQUEST.has_key('__cp'):
-    return view.tab_edit(message_type="error", _(message="No content available to paste."))
+    return view.tab_edit(message_type="error", message=_("No content available to paste."))
 
 message = model.action_paste(context.REQUEST)
 return view.tab_edit(message_type="feedback", message=message)
