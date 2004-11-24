62dc5154b2f0c4be4daec13bd792c70d0c6f01f5
Eric Casteleijn <ec@trapit.com>
2004-11-24 14:01:30 +0000

    removed all space separated i18n:attributes, re-extracted and fixed a bug in get_id-status_test.py

--- views/add/get_id_status_text.py
+++ views/add/get_id_status_text.py
@@ -13,38 +13,38 @@ if same_type(id, ""):
 view = context
 status_code = id.validate()
 
-if status_code ==id.CONTAINS_BAD_CHARS:
+if status_code == id.CONTAINS_BAD_CHARS:
     return unicode_("""Sorry, strange characters are in the id. It should only
         contain letters, digits and &#8216;_&#8217; or &#8216;-&#8217; or 
         &#8216;.&#8217;<br />Spaces are not allowed in Internet addresses, 
-        and the id should start with a letter or digit."""))
+        and the id should start with a letter or digit.""")
 elif status_code == id.RESERVED_PREFIX:
     # FIXME: needs to know about the prefix
     prefix = str(id).split('_')[0]+'_'
     message = _("""Sorry, ids starting with ${prefix} are reserved for
         internal use.<br />Please use another id.""")
     message.set_mapping({'prefix': view.quotify(prefix)})
-    return message
+    return unicode(message)
 elif status_code == id.RESERVED:
     message = _("""Sorry, the id ${id} is reserved for internal use.<br />
         Please use another id.""")
     message.set_mapping({'id': view.quotify(id)})
-    return message
+    return unicode(message)
 elif status_code == id.IN_USE_CONTENT:
     message = _("""There is already an object with the id ${id} in this 
         folder.<br />Please use a different one.""")
     message.set_mapping({'id': view.quotify(id)})
-    return message
+    return unicode(message)
 elif status_code == id.IN_USE_ASSET:
     message = _("""There is already an asset with the id ${id} in this 
         folder.<br />Please use another id.""")
     message.set_mapping({'id': view.quotify(id)})
-    return message
+    return unicode(message)
 elif status_code == id.RESERVED_POSTFIX:
     message = _("""Sorry, the id ${id} ends with invalid characters.<br />
         Please use another id.""")
     message.set_mapping({'id': view.quotify(id)})
-    return message
+    return unicode(message)
 
 # this should not happen
 message = _("""(Internal Error): An invalid status ${status_code} occured 
@@ -54,4 +54,5 @@ message.set_mapping({
     'status_code': view.quotify(status_code), 
     'id': view.quotify(id)
     })
-return message
+
+return unicode(message)
