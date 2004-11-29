24eda175a92386847c506cb16b54ade087d2ad64
Guido Wesdorp <info@infrae.com>
2004-11-29 14:03:06 +0000

    Moved a Python statement to a seperate script to allow i18n.

---
+++ views/edit/Container/tab_edit_new_link_title.py
@@ -0,0 +1,13 @@
+## Script (Python) "tab_edit_new_link_title"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=addable_name
+##title=
+#
+from Products.Silva.i18n import translate as _
+msg = _('Create a ${addable_name}')
+msg.set_mapping({'addable_name': addable_name})
+return msg
