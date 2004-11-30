906aaa5bb0c0f2317599795218c951bb33d6deb7
Guido Wesdorp <info@infrae.com>
2004-11-30 12:16:35 +0000

    Created Python script to allow translation of a title attribute.

---
+++ views/edit/Container/tab_status_get_modify_title.py
@@ -0,0 +1,14 @@
+## Script (Python) "tab_status_get_name"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=meta_type
+##title=
+##
+from Products.Silva.i18n import translate as _
+
+msg = _('modify ${meta_type}')
+msg.set_mapping({'id': id})
+return msg
