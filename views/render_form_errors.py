ceb8c0c6414eff4d44bf010de6a1df50c67615ad
Walco Van Loon <info@infrae.com>
2004-12-01 15:47:58 +0000

    Fixed syntax problem.

--- views/render_form_errors.py
+++ views/render_form_errors.py
@@ -11,7 +11,6 @@ from Products.Silva.i18n import translate as _
 result = []
 for error in validation_error.errors:
     result.append('<li class="error">%s: %s</li>\n' % (error.field['title'], error.error_text))
-
-return """<dl style="margin:0; padding:0.3em 0 0.2em 0;"><dt>""" + 
+return ("""<dl style="margin:0; padding:0.3em 0 0.2em 0;"><dt>""" + 
 unicode(_("Sorry, there are problems with these form fields:")) + 
-"""</dt><dd><ul class="tips">""" + ' '.join(result) + "</ul></dd></dl>"
+"""</dt><dd><ul class="tips">""" + ' '.join(result) + "</ul></dd></dl>")
