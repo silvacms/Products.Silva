062d22fe9932deb6e0fefb212df0c35c8ffe1784
Eric Casteleijn <ec@trapit.com>
2004-12-09 16:10:16 +0000

    fixed the renderable adapter that got updated a little bit too enthousiastically.

--- adapters/renderable.py
+++ adapters/renderable.py
@@ -6,15 +6,28 @@ class RenderableAdapter(adapter.Adapter):
 
     __implements__ = IRenderable
 
-    def view(self):
-        """Display the view of this version using the selected renderer.
+    def preview(self):
+        """Display the preview of this object using the selected renderer.
         
         Returns the rendered content or None if no renderer can be
         found.
         """
-        renderer_name = self.context.get_renderer_name()
+        content = self.context.get_previewable()
+        return self._view_helper(content)
+    
+    def view(self):
+        """Display the view of this object using the selected renderer.
+        
+        Returns the rendered content or None if no renderer can be
+        found.
+        """
+        content = self.context.get_viewable()
+        return self._view_helper(content)
+        
+    def _view_helper(self, content):
+        renderer_name = content.get_renderer_name()
         renderer = self.context.service_renderer_registry.getRenderer(
-            self.context.get_silva_object().meta_type, renderer_name)
+            content.get_silva_object().meta_type, renderer_name)
         if renderer is None:
             return None
         return unicode(renderer.render(self.context), 'UTF-8')
