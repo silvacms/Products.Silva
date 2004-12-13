4f9580703789ca5bd5921e6a2882bdd716a693af
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2004-12-13 08:40:48 +0000

    Commenting out sec.declarations and adding some notes.

--- adapters/haunted.py
+++ adapters/haunted.py
@@ -10,7 +10,8 @@ from Products.Silva import SilvaPermissions
 from Products.Silva.adapters import adapter
 
 class Haunted(adapter.Adapter):
-    """
+    """Adapted content for retrieving the 'iterator' of haunting 
+    objects (Ghosts).
     """
     
     __implements__ = (interfaces.IHaunted, )
@@ -35,14 +36,17 @@ Globals.InitializeClass(Haunted)
 # Jumping through security hoops to get the adapter
 # somewhat accessible to Python scripts
 
-allow_module('Products.Silva.adapters.haunted')
+# Security declaration commented for the moment, as this adapter
+# is not yet used by the view-layer.
+#allow_module('Products.Silva.adapters.haunted')
 
-__allow_access_to_unprotected_subobjects__ = True
+#__allow_access_to_unprotected_subobjects__ = True
     
-module_security = ModuleSecurityInfo('Products.Silva.adapters.haunted')
+#module_security = ModuleSecurityInfo('Products.Silva.adapters.haunted')
     
-module_security.declareProtected(
-    SilvaPermissions.ApproveSilvaContent, 'getHaunted')
+#module_security.declareProtected(
+#    SilvaPermissions.ApproveSilvaContent, 'getHaunted')
+
 def getHaunted(context):
     if interfaces.IContainer.isImplementedBy(context):
         return Haunted(context).__of__(context)
