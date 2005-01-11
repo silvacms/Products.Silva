00d712133c5c2c83eead55cad642d16a6297085d
Infrae <info@infrae.com>
2005-01-11 12:50:28 +0000

    This commit was manufactured by cvs2svn to create tag 'Silva-0_9_3_7'.

--- layout/get_layout_macro.py
+++
@@ -1,9 +0,0 @@
-layout_macro = 'layout_macro.html'
-#layout_folder = context.get_layout_folder()
-#if layout_folder:
-#    lm = getattr(layout_folder, layout_macro)
-#else:
-#    lm = getattr(context, layout_macro)
-lm = getattr(context, layout_macro)
-return lm.macros['layout']
-
