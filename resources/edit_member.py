5c7ad1bd3b0d86d1317b07be9dd5fa42ea94d0a3
Guido Wesdorp <info@infrae.com>
2005-01-20 16:28:58 +0000

    Fixed problem that made that certain Membership extensions (that have a different id for the user then for the Member object for that user) fail.

--- resources/edit_member.py
+++ resources/edit_member.py
@@ -3,8 +3,6 @@ request = view.REQUEST
 
 view.get_root().security_trigger()
 userid = request.AUTHENTICATED_USER.getId()
+
 member = view.service_members.get_member(userid)
-
-return getattr(view.Members, userid).edit['tab_edit']()
-
-return member.edit()
+return getattr(view.Members, member.id).edit['tab_edit']()
