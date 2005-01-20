d8c9a373cf6ce4d6285bd76a78aa4e9b701b89df
Guido Wesdorp <info@infrae.com>
2005-01-20 10:05:41 +0000

    Fixed a problem that made tab_edit_make_copy try to create versions after a Kupu save (tab_edit_make_copy *returns* tab_edit, so the URL remains .../tab_edit_make_copy, Kupu does a page reload after save, so on each save tab_edit_make_copy would get called again), even though creating a new version didn't actually happen that made feedback messages got stuck in the feedback area. Now the script checks if there's already an editable version and if so just returns tab_edit without any feedback.

--- views/edit/VersionedContent/tab_edit_make_copy.py
+++ views/edit/VersionedContent/tab_edit_make_copy.py
@@ -10,7 +10,14 @@
 from Products.Silva.i18n import translate as _
 
 model = context.REQUEST.model
-model.sec_update_last_author_info()
 view = context
+
+# we might be called 'accidentally' if after creating a new copy the editor
+# does a client-side reload of the current URL (after a save or something),
+# in that case just return tab_edit
+if model.get_editable():
+    return view.tab_edit()
+
+model.sec_update_last_author_info()
 model.create_copy()
 return view.tab_edit(message_type="feedback", message=_("New version created."))
