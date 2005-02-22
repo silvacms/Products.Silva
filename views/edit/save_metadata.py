2c9c242a1d345fd1080456c631d19b88fdb5ba93
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2005-02-22 09:14:03 +0000

    Merge of jw-use_forms_for_metadata_validation-branch.
    Change the save_metadata script to make use of setValuesFromRequest(),
    which in turn uses of "getMetadataForm()" for validating user input and
    setting the values.
    
    These changes solve the infamous 'checkbox' problem.

--- views/edit/save_metadata.py
+++ views/edit/save_metadata.py
@@ -3,23 +3,11 @@ from Products.Silva.i18n import translate as _
 request = context.REQUEST
 model = request.model
 view = context
-
 ms = context.service_metadata
+
 editable = model.get_editable()
 binding = ms.getMetadata(editable)
-
-values = {}
-for set_name in binding.getSetNames():
-    values[set_name] = {}
-    if request.form.has_key(set_name):
-        for key, value in request.form[set_name].items():
-            values[set_name][key] = value
-
-all_errors = {}
-for set_name in binding.getSetNames():
-    errors = binding.setValues(set_name, values[set_name], reindex=1)
-    if errors:
-        all_errors[set_name] = errors
+all_errors = binding.setValuesFromRequest(request)
 
 if request.form.has_key('renderer_name_select'):
     model.set_renderer_name(request.form['renderer_name_select'])
@@ -27,13 +15,14 @@ if request.form.has_key('renderer_name_select'):
 if all_errors:
     # There were errors...
     type = 'error'
-    msg = _('The data that was submitted did not validate properly.')
+    msg = _(
+        'The data that was submitted did not validate properly '
+        '(you might want to use the browser back button to adjust '
+        'the form values).')
 else:
     type = 'feedback'
     msg = _('Metadata saved.')
     model.sec_update_last_author_info()
 
 return view.tab_metadata(
-    form_errors=all_errors,
-    message_type=type,
-    message=msg)
+    form_errors=all_errors, message_type=type, message=msg)
