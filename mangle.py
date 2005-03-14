5b92d89d1d0306e88344793c4e8c1fe39888f4b4
Infrae <info@infrae.com>
2005-03-14 23:00:20 +0000

    add "convertOutputForI18Nname" as an ugly hack to get around an issue with i18n:name expecting plain strings, not unicode. Hope that longish name makes it easy to get rid of it if a better solution is found

--- mangle.py
+++ mangle.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-1 -*-
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Id: mangle.py,v 1.30 2005/03/14 15:37:55 jw Exp $
+# $Id: mangle.py,v 1.31 2005/03/14 23:00:20 clemens Exp $
 # Python
 import string
 import re
@@ -490,6 +490,13 @@ class _String:
 
 module_security.declarePublic('String')
 String = _String()
+
+module_security.declarePublic('convertOutputForI18Nname')
+def convertOutputForI18Nname(text):
+    """Very ugly hack to get around a problem with i18n:name
+       which expects plain strings as input, not unicode strings
+    """
+    return text.encode(String._default_encoding)
 
 def generateAnchorName(s):
     """Generate a valid name for an anchor.
