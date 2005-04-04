c98412449cb7bbad0cf433666d61735fcacb6f95
Guido Wesdorp <info@infrae.com>
2005-04-04 16:24:36 +0000

    Added a monkey patch for some Unicode problem in Zope's TALInterpreter. Instead of calling 'str' on the value of an expression before passing it to 'cgi.escape', something called 'ustr' is used now (which is a function defined in Zope somewhere that does the same as 'str()', except that it returns unicode strings 1:1).

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.132 $
+# $Revision: 1.132.4.1 $
 # Zope
 import zLOG
 # Silva
@@ -315,3 +315,10 @@ def initialize_icons():
 def initialize_upgrade():
     from Products.Silva import upgrade_120
     upgrade_120.initialize()
+
+#------------------------------------------------------------------------------
+# Monkey patches
+#------------------------------------------------------------------------------
+
+import monkey
+monkey.patch_all()
