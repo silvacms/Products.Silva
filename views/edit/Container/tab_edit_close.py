00e873a7f008b532000074f071b5cd145453e361
Guido Wesdorp <info@infrae.com>
2005-05-11 16:51:48 +0000

    Added some additional buttons.

---
+++ views/edit/Container/tab_edit_close.py
@@ -0,0 +1,47 @@
+##parameters=ids=None
+from Products.Silva.i18n import translate as _
+
+request = context.REQUEST
+model = request.model
+view = context
+
+from DateTime import DateTime
+
+# Check whether there's any checkboxes checked at all...
+if not ids:
+    return view.tab_edit(
+        message_type='error', 
+        message=_('Nothing was selected, so nothing was closed.'))
+
+closed_ids = []
+not_closed = []
+msg = []
+
+get_name = context.tab_status_get_name
+
+for id in ids:
+    obj = getattr(model.aq_inner, id)
+    if obj is None:
+        continue
+    if not obj.implements_versioning():
+        not_closed.append((get_name(obj), _('not a versionable object')))
+        continue
+    if not obj.is_version_published():
+        not_closed.append((get_name(obj), _('is not published')))
+        continue
+    obj.close_version()
+    #obj.deactivate()
+    closed_ids.append(get_name(obj))
+
+if closed_ids:
+    request.set('redisplay_timing_form', 0)
+    message = _('Closed: ${ids}')
+    message.set_mapping({'ids': view.quotify_list(closed_ids)})
+    msg.append(unicode(message))
+
+if not_closed:
+    message = _('<span class="error">could not close: ${ids}</span>')
+    message.set_mapping({'ids': view.quotify_list_ext(not_closed)})
+    msg.append(unicode(message))
+
+return view.tab_edit(message_type='feedback', message=(', '.join(msg)) )
---
+++ views/edit/Container/tab_edit_open_now.py
@@ -0,0 +1,68 @@
+##parameters=ids=None
+from Products.Silva.i18n import translate as _
+
+request = context.REQUEST
+model = request.model
+view = context
+
+from DateTime import DateTime
+
+# Check whether there's any checkboxes checked at all...
+if not ids:
+    return view.tab_edit(
+        message_type='error', 
+        message=_('Nothing was selected, so nothing was approved.'))
+
+now = DateTime()
+
+approved_ids = []
+not_approved = []
+msg = []
+
+get_name = context.tab_status_get_name
+
+for id in ids:
+    obj = getattr(model.aq_inner, id)
+    if obj is None:
+        continue
+    if not obj.implements_versioning():
+        not_approved.append((get_name(obj), _('not a versionable object')))
+        continue
+    if obj.is_version_approved():
+        not_approved.append((get_name(obj), _('version already approved')))
+        continue
+    if not obj.can_approve():
+        not_approved.append((get_name(obj), 
+            _('content object or one of its containers is deactivated')))
+        continue
+    if not obj.get_unapproved_version():
+    # SHORTCUT: To allow approval of closed docs with no new version available,
+    # first create a new version. This "shortcuts" the workflow.
+    # To revert this behaviour, *uncomment* the next two lines, and *comment*
+    # the consecutive four lines.
+
+    #    not_approved.append((get_name(obj), _('no unapproved version available')))
+    #    continue
+        if obj.is_version_published():
+            not_approved.append((get_name(obj), _('version already public')))
+            continue
+        obj.create_copy()
+    # publish
+    obj.set_unapproved_version_publication_datetime(now)
+    obj.approve_version()
+    approved_ids.append(get_name(obj))
+
+if approved_ids:
+    message = _('Approval on: ${ids}')
+    message.set_mapping({'ids': view.quotify_list(approved_ids)})
+    msg.append(unicode(message))
+
+if not_approved:    
+    message = _('<span class="error">could not approve: ${ids}</span>')
+    message.set_mapping({'ids': view.quotify_list_ext(not_approved)})
+    msg.append(unicode(message))
+
+if hasattr(context, 'service_messages'):
+    context.service_messages.send_pending_messages()
+
+return view.tab_edit(message_type='feedback', message=(', '.join(msg)))
