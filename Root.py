d37173d2d9628b9b7f27b6f573b331d6f240d956
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2005-06-02 08:03:52 +0000

    Updated software version string.

--- Root.py
+++ Root.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.90 $
+# $Revision: 1.91 $
 
 # Zope
 from AccessControl import ClassSecurityInfo
@@ -179,7 +179,7 @@ class Root(Publication):
     def get_silva_software_version(self):
         """The version of the Silva software.
         """
-        return '1.2'
+        return '1.3'
     
     security.declareProtected(SilvaPermissions.ReadSilvaContent,
                               'get_silva_content_version')
