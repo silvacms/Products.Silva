4c9d35dff43bcd1c2941a9e77c6d9152bac83f9a
Infrae <info@infrae.com>
2005-09-03 22:07:31 +0000

    treat the form title the same as the error text. has been necessary to avoid a encoding error if testing with german translation.

--- views/render_form_errors.py
+++ views/render_form_errors.py
@@ -11,12 +11,16 @@ from Products.Silva.i18n import translate as _
 result = []
 for error in validation_error.errors:
     error_text = error.error_text
-    # if error_text is a MessageIDUnicode instance, call it to transform it to
+    title = error.field['title']
+    # if error_text or title is a MessageIDUnicode instance,
+    # call it to transform it to
     # a plain string first
     if callable(error_text):
         error_text = error_text()
+    if callable(title):
+        title = title()
     result.append('<li class="error">%s: %s</li>\n' % 
-                    (error.field['title'], error.error_text()))
+                    (title, error_text))
 return ("""<dl style="margin:0; padding:0.3em 0 0.2em 0;"><dt>""" + 
 unicode(_("Sorry, there are problems with these form fields:")) + 
 """</dt><dd><ul class="tips">""" + ' '.join(result) + "</ul></dd></dl>")
