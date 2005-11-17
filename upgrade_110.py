df32aa669ed27b8b45d86657831fbe158221f43d
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2005-11-17 10:07:10 +0000

    Register an refresh all upgrader, instead of a dummy.

--- upgrade_110.py
+++ upgrade_110.py
@@ -2,17 +2,8 @@
 import zLOG
 # silva imports
 from Products.Silva.interfaces import IUpgrader
-from Products.Silva import upgrade
-
-class DummyUpgrader:
-
-    __implements__ = IUpgrader
-    
-    def upgrade(self, silvaroot):
-        return silvaroot
-    
-# There's not real upgrade from 1.0 to 1.1 however, we still register
-# a dummy upgrader to keep the upgrader version registry coherent.
+from Products.Silva import upgrade, upgrade_100
 
 def initialize():
-    upgrade.registry.registerUpgrader(DummyUpgrader(), '1.1', 'Silva Root')
+    upgrade.registry.registerUpgrader(
+        upgrade_100.RefreshAll(), '1.1', 'Silva Root')
--- upgrade_130.py
+++ upgrade_130.py
@@ -2,11 +2,8 @@
 import zLOG
 # silva imports
 from Products.Silva.interfaces import IUpgrader
-from Products.Silva import upgrade, upgrade_110
-
-# There's not real upgrade from 1.2 to 1.3 however, we still register
-# a dummy upgrader to keep the upgrader version registry coherent.
+from Products.Silva import upgrade, upgrade_100
 
 def initialize():
     upgrade.registry.registerUpgrader(
-        upgrade_110.DummyUpgrader(), '1.3', 'Silva Root')
+        upgrade_100.RefreshAll(), '1.3', 'Silva Root')
