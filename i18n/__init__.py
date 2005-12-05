2b3d19832c959896ea05088e10a09bd51798bae8
Martijn Faassen <faassen@startifact.com>
2005-12-05 17:17:05 +0000

    Text fixes.

--- i18n/__init__.py
+++ i18n/__init__.py
@@ -2,9 +2,8 @@
 
     from Products.Silva.i18n import translate as _
 
-and will provide, if PlacelessTranslationService is installed, a 
-MessageIDFactory that returns MessageIDs for i18n'ing Product code 
-and Python scripts.
+and will provide a MessageIDFactory that returns MessageIDs for
+i18n'ing Product code and Python scripts.
 """
 from zope.i18nmessageid import MessageIDFactory
 
