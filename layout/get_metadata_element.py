43ba0d40739d804d9ecf912b462a31b3c3579cbb
Eric Casteleijn <ec@trapit.com>
2005-12-15 19:12:15 +0000

    fixed bug where acquired metadata wasnt found

--- layout/get_metadata_element.py
+++ layout/get_metadata_element.py
@@ -1,13 +1,4 @@
-##parameters=set_name,element_name
-
 # This is a copy of views/public/get_metadata_element used in the SMI.
-# FIXME: do we need a better location for this functionality?
-#
-# One way to get to the get_metadata_element in the "public" view of
-# a Silva object is this:
-#
-#   method = obj.public['get_metadata_element']
-#   data = method(setname, elementname)
 #
 # Get the data for a particular element of a particular set for
 # the viewable version of this object.
@@ -17,5 +8,5 @@ content = context.get_viewable()
 if content is None:
     return None
 
-return context.service_metadata.getMetadataValue(
-    content, set_name, element_name)
+binding = context.service_metadata.getMetadata(content)
+return binding.get(set_name, element_id=element_name)
