d60be2220fe6aff731ccd4fddc95bb7534f14249
Martijn Faassen <faassen@startifact.com>
2006-01-13 18:10:16 +0000

    Add a facility to get the language name from the z3 locales database, for display in user settings form.

--- adapters/languageprovider.py
+++ adapters/languageprovider.py
@@ -9,11 +9,13 @@ from Products.Silva.adapters import adapter
 from Products.Silva.adapters import interfaces
 from Products.Silva import SilvaPermissions
 
+from Products.Silva.i18n import translate as _
+
 from zope.i18n.interfaces import ITranslationDomain, IUserPreferredLanguages
 from zope.app import zapi
 from zope.publisher.browser import BrowserLanguages
 
-# this isn't a real z3 adapter yet as it's used by view code
+# XXX this isn't a real z3 adapter yet as it's used by view code
 class LanguageProvider(adapter.Adapter):
     """
     """
@@ -29,6 +31,58 @@ class LanguageProvider(adapter.Adapter):
         # 'getAvailableLanguages' functionality apparently..
         return silva_domain._catalogs.keys()
 
+    security.declarePublic('getLanguageName')
+    def getLanguageName(self, language_id):
+        """Get the name of the language, in the current language.
+        """
+        self._setupLocale()
+        name = self.context.REQUEST.locale.displayNames.languages.get(
+            language_id)
+        if name is None:
+            # if for some reason the language name is unknown, show id
+            name = language_id
+        return name
+    
+    def _setupLocale(self):
+        request = self.context.REQUEST
+        # we already got it set up
+        if hasattr(request, 'locale'):
+            return
+        # request doesn't have locale in Zope 2; this code should
+        # ideally move somewhere into Five
+        from zope.i18n.locales import locales, LoadLocaleError
+        
+        langs = IUserPreferredLanguages(request).getPreferredLanguages()
+        for httplang in langs:
+            parts = (httplang.split('-') + [None, None])[:3]
+            try:
+                request.locale = locales.getLocale(*parts)
+                return
+            except LoadLocaleError:
+                # Just try the next combination
+                pass
+        else:
+            # No combination gave us an existing locale, so use the default,
+            # which is guaranteed to exist
+            request.locale = locales.getLocale(None, None, None)
+        
+    def getLocale(self):
+        # XXX zope 3 has locale on the request; Five doesn't have that yet
+        langs = IUserPreferredLanguages(
+            self.context.REQUEST).getPreferredLanguages()
+        for httplang in langs:
+            parts = (httplang.split('-') + [None, None])[:3]
+            try:
+                self._locale = locales.getLocale(*parts)
+                return
+            except LoadLocaleError:
+                # Just try the next combination
+                pass
+
+            
+        language = self.getPreferredLanguage()
+        
+        
     security.declarePublic('setPreferredLanguage')
     def setPreferredLanguage(self, language):
         path = '/'.join(
