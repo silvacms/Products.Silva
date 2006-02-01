7faeacfcba2c010560bac91b8b2230d5eb8b4841
Martijn Faassen <faassen@startifact.com>
2006-02-01 13:44:43 +0000

    Layout macro dos to unix.

--- layout/get_layout_macro.py
+++ layout/get_layout_macro.py
@@ -1,9 +1,5 @@
+## Script (Python) "get_layout_macro"
 layout_macro = 'layout_macro.html'
-#layout_folder = context.get_layout_folder()
-#if layout_folder:
-#    lm = getattr(layout_folder, layout_macro)
-#else:
-#    lm = getattr(context, layout_macro)
 lm = getattr(context, layout_macro)
 return lm.macros['layout']
 
