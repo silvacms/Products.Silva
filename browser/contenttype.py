2898e8b679dd9c5a8dff562cc150d61ecb5615ad
Eric Casteleijn <ec@trapit.com>
2006-03-28 15:58:36 +0000

    added a new view

---
+++ browser/contenttype.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2002-2005 Infrae. All rights reserved.
+# See also LICENSE.txt
+# Five
+from Products.Five import BrowserView
+
+class Headers(BrowserView):
+    def __call__(self):
+        self.setHeaders([('Content-Type', 'text/html; charset=UTF-8')])
+        headers = [('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT'),
+            ('Last-Modified', 
+                DateTime("GMT").strftime("%a, %d %b %Y %H:%M:%S GMT")),
+            ('Cache-Control', 'no-cache, must-revalidate'),
+            ('Cache-Control', 'post-check=0, pre-check=0'),
+            ('Pragma', 'no-cache'),
+            ]
+        self.setHeaders(headers)
+        
+    def setHeaders(self, headers):
+        response = context.REQUEST.RESPONSE
+        placed = []
+        for key, value in headers:
+            if key not in placed:
+                response.setHeader(key, value)
+                placed.append(key)
+            else:
+                response.addHeader(key, value)
