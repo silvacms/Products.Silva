97658c5d97a11e2e08ab9049005e9d6dabe6715b
Eric Casteleijn <ec@trapit.com>
2006-03-29 11:54:30 +0000

    slight refactoring of the headers views

--- browser/headers.py
+++ browser/headers.py
@@ -5,8 +5,23 @@ from DateTime import DateTime
 from Products.Five import BrowserView
 
 class Headers(BrowserView):
-    def __call__(self):
-        self._setHeaders([('Content-Type', 'text/html; charset=UTF-8')])
+    """docstring
+    """
+    def set_content_type_and_cache(self):
+        """docstring
+        """
+        content_tags = self.set_content_type()
+        cache_tags = self.set_cache()
+        return content_tags + '/n' + cache_tags
+        
+    def set_content_type(self):
+        """docstring
+        """
+        return self.set_headers([('Content-Type', 'text/html; charset=UTF-8')])
+    
+    def set_cache(self):
+        """docstring
+        """
         headers = [('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT'),
             ('Last-Modified', 
                 DateTime("GMT").strftime("%a, %d %b %Y %H:%M:%S GMT")),
@@ -14,14 +29,19 @@ class Headers(BrowserView):
             ('Cache-Control', 'post-check=0, pre-check=0'),
             ('Pragma', 'no-cache'),
             ]
-        self._setHeaders(headers)
+        return self.set_headers(headers)
         
-    def _setHeaders(self, headers):
+    def set_headers(self, headers):
+        """docstring
+        """
         response = self.request.RESPONSE
         placed = []
+        tags = []
         for key, value in headers:
+            tags.append("""<meta http-equiv="%s" content="%s" />""" % (key, value))
             if key not in placed:
                 response.setHeader(key, value)
                 placed.append(key)
             else:
                 response.addHeader(key, value)
+        return '/n'.join(tags)
