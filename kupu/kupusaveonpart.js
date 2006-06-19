7a001a891addf9070f7ba7eb63b51bfd2139409c
Martijn Faassen <faassen@startifact.com>
2006-06-19 16:00:17 +0000

    Guido didn't have this file checked in. As a result, kupu didn't work. Now that we have it checked in, the new functionality that's in this file still doesn't work, but it's better than before. :)

---
+++ kupu/kupusaveonpart.js
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ *
+ * Copyright (c) 2003-2005 Kupu Contributors. All rights reserved.
+ *
+ * This software is distributed under the terms of the Kupu
+ * License. See LICENSE.txt for license text. For a list of Kupu
+ * Contributors see CREDITS.txt.
+ *
+ *****************************************************************************/
+
+// $Id: kupusaveonpart.js 25438 2006-04-06 10:27:12Z guido $
+
+function saveOnPart(event) {
+    /* ask the user if (s)he wants to save the document before leaving */
+    if (kupu.content_changed && confirm(
+            _('You have unsaved changes. Are you sure you want to ' +
+                'leave the editor?')
+            )) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        };
+        return false;
+    };
+    return true;
+};
