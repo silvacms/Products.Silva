c22fc4c3a699f41b18f6c79b39beb70805f428e1
Eric Casteleijn <ec@trapit.com>
2006-06-21 18:33:07 +0000

    backport of fix for ghostfolder export

--- views/edit/redirect.py
+++ views/edit/redirect.py
@@ -8,11 +8,12 @@
 ##title=
 ##
 model = context.REQUEST.model
-view = context
-content_url = model.content_url() + '/edit/tab_edit'
+#view = context
+#content_url = model.content_url() + '/edit/tab_edit'
 
-agent = context.REQUEST['HTTP_USER_AGENT']
-if agent.startswith('Mozilla/4.77') or agent.find('Konqueror') > -1:
-    return '<html><head><META HTTP-EQUIV="refresh" CONTENT="0; URL=%s"></head><body bgcolor="#FFFFFF"></body></html>' % content_url  
-else:
-    return context.REQUEST.RESPONSE.redirect(content_url)
+#agent = context.REQUEST['HTTP_USER_AGENT']
+#if agent.startswith('Mozilla/4.77') or agent.find('Konqueror') > -1:
+#    return '<html><head><META HTTP-EQUIV="refresh" CONTENT="0; URL=%s"></head><body bgcolor="#FFFFFF"></body></html>' % content_url  
+#else:
+#    return context.REQUEST.RESPONSE.redirect(content_url)
+return model.content_url()
