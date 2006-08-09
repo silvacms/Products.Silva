ddaeaca47e36d27a74c3795ff63fd762b961e9a8
Wim Boucquaert <wim.boucquaert@gmail.com>
2006-08-09 14:42:29 +0000

    fix for issue 1608, could need improvements, free to check

--- views/edit/VersionedContent/tab_edit_revoke_approval.py
+++ views/edit/VersionedContent/tab_edit_revoke_approval.py
@@ -8,7 +8,18 @@
 ##title=
 ##
 from Products.Silva.i18n import translate as _
-model = context.REQUEST.model
-view = context
+
+request = context.REQUEST
+model = request.model
+
+came_from_view = request.get('came_from_view', 'tab_edit_revoke_approval')
+view = model.edit[came_from_view]
+
 model.unapprove_version()
-return view.tab_edit(message_type="feedback", message=_("Revoked approval."))
+
+# if user is in the editor screen and approving, return to edit/tab_edit
+if came_from_view == 'tab_edit_revoke_approval':
+    request.RESPONSE.redirect('%s/edit/tab_edit' % (model.absolute_url()))
+    return ''
+    
+return view(message_type="feedback", message=_("Revoked approval."))
