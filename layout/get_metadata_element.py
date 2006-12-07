c40788a15d5ca86d4d76a9b9b59b6a68946731c3
Eric Casteleijn <ec@trapit.com>
2006-12-07 19:22:16 +0000

    another evildoer

--- layout/get_metadata_element.py
+++ layout/get_metadata_element.py
@@ -1,5 +1,6 @@
 ##parameters=set_name,element_name
 
+# XXX
 # This is a copy of views/public/get_metadata_element used in the SMI.
 #
 # Get the data for a particular element of a particular set for
@@ -10,5 +11,4 @@ content = context.get_viewable()
 if content is None:
     return None
 
-binding = context.service_metadata.getMetadata(content)
-return binding.get(set_name, element_id=element_name)
+return context.service_metadata.getMetadata(content, set_name, element_name)
