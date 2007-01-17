e67aa8d63ddd75e84bcc9ad4fbc447877ab90291
Kit Blake <kitblake@infrae.com>
2007-01-17 11:50:15 +0000

    For forms with only one error, display only one line of feedback (as usual)

--- views/render_form_errors.py
+++ views/render_form_errors.py
@@ -12,16 +12,31 @@ from zope.i18n import translate
 
 result = []
 request = context.REQUEST
-for error in validation_error.errors:
+
+if len(validation_error.errors) == 1:
+
+    error = validation_error.errors[0]
     error_text = error.error_text
     title = error.field['title']
 
     # translate error_text and title first
     error_text = translate(error_text, context=request)
-    title = translate(title, context=request)
+    title = translate(title, context=request)    
+
+    result.append('%s: %s' % (title, error_text))
+    return (''.join(result))
+
+else:
+
+    for error in validation_error.errors:
+        error_text = error.error_text
+        title = error.field['title']
     
-    result.append('<li class="error">%s: %s</li>\n' % 
-                    (title, error_text))
+        # translate error_text and title first
+        error_text = translate(error_text, context=request)
+        title = translate(title, context=request)
+    
+        result.append('<li class="error">%s: %s</li>\n' %  (title, error_text))
 
 return ("""<dl style="margin:0;"><dt>""" + 
 translate(_("Sorry, there are problems with these form fields:"),
