63201afbadd7b671037f47bb8634f7952391ee18
Eric Casteleijn <ec@trapit.com>
2007-01-24 18:05:13 +0000

    fixed copyright notices and added base tag

--- adapters/virtualhosting.py
+++ adapters/virtualhosting.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2002-2006 Infrae. All rights reserved.
+# Copyright (c) 2002-2007 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id: virtualhosting.py,v 1.8 2006/01/24 16:12:01 faassen Exp $
 #
--- tests/test_SimpleMembership.py
+++ tests/test_SimpleMembership.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2002-2006 Infrae. All rights reserved.
+# Copyright (c) 2002-2007 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Revision: 1.6 $
 import SilvaTestCase
