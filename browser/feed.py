26f78e2465e0340af2ce3cfb9eb07bf83e5775df
Eric Casteleijn <ec@trapit.com>
2007-02-23 18:37:42 +0000

    fake atom feed working. Must convert this to tests and build a real one

--- browser/feed.py
+++ browser/feed.py
@@ -1,19 +1,75 @@
 import Globals
 from zope.interface import implements
-from Products.Silva.interfaces import IContainer, IDocument
+from Products.Silva.interfaces import IContainer
 from AccessControl import getSecurityManager
 from Products.Five import BrowserView
 
-class AtomView(BrowserView):
-    """View on Silva containers to get their atom feed representation."""
+class FeedView(BrowserView):
+    """Base class for feed representation."""
 
-    def render(self, items=0):
-        return None
+    def get_data(self):
+        """ prepare the data needed by a feed
+        """
+        return {
+            'id': 'id',
+            'title': 'title',
+            'description': 'description',
+            'url': 'url',
+            'authors': ['author1', 'author2'],
+            'date_updated': '2007-02-23T19:12:12ZCET',
+            'entries': [
+                {
+                    'title': 'title',
+                    'url': 'url',
+                    'id': 'id1',
+                    'date_published': '2007-02-23T18:12:12ZCET',
+                    'date_updated': '2007-02-23T18:12:12ZCET',
+                    'authors': ['author1', 'author2'],
+                    'keywords': ['cool', 'feeds'],
+                    'description': 'testing<br /><strong>1, 2</strong>'
+                    },
+                {
+                    'title': 'title',
+                    'url': 'url',
+                    'id': 'id2',
+                    'date_published': '2007-02-23T18:12:12ZCET',
+                    'date_updated': '2007-02-23T18:12:12ZCET',
+                    'authors': ['author1', 'author2'],
+                    'keywords': ['cool', 'feeds'],
+                    'description': 'testing<br /><strong>1, 2</strong>'
+                    },
+                {
+                    'title': 'title',
+                    'url': 'url',
+                    'id': 'id4',
+                    'date_published': '2007-02-23T19:12:12ZCET',
+                    'date_updated': '2007-02-23T19:12:12ZCET',
+                    'authors': ['author1', 'author2'],
+                    'keywords': ['cool', 'feeds'],
+                    'description': 'testing<br /><strong>1, 2</strong>'
+                    },
+                {
+                    'title': 'title',
+                    'url': 'url',
+                    'id': 'id3',
+                    'date_published': '2007-02-23T18:12:12ZCET',
+                    'date_updated': '2007-02-23T18:12:12ZCET',
+                    'authors': ['author1', 'author2'],
+                    'keywords': ['cool', 'feeds'],
+                    'description': 'testing<br /><strong>1, 2</strong>'
+                    },],
+            }
+
+#class AtomView(FeedView):
+#    """View on Silva containers to get their atom feed representation."""
+
+#    def render(self, items=0):
+#        return None
 
 
-class RssView(BrowserView):
-    """View on Silva containers to get their rss feed representation."""
+#class RssView(FeedView):
+#    """View on Silva containers to get their rss feed representation."""
 
-    def render(self, items=0):
-        return None
+#    def render(self, items=0):
+#        return None
     
