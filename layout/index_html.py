8f8d57ac48644745b8464cdd431b245c4b2e1a5a
Kit Blake <kitblake@infrae.com>
2007-03-26 06:11:59 +0000

    Removed legacy code from the old layout service

--- layout/index_html.py
+++ layout/index_html.py
@@ -13,15 +13,6 @@ else:
 context.REQUEST.RESPONSE.setHeader('Content-Type', 'text/html;charset=utf-8')
 context.REQUEST.RESPONSE.setHeader('Cache-Control','max-age=300')
 
-
-## use layout content if a layout has been setup
-#if renderer == content:
-#    layout_folder = context.get_layout_folder()
-#    if layout_folder:
-#        index = getattr(layout_folder, content)
-#        return index(model=context, view_method=view_method)
-
-
 # By default, the prefered view method is 'view()', unless
 # a different view_method is passed to this script.
 return getattr(context, renderer)(view_method=view_method)
