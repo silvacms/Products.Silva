a5e0028bfeffc459891a11d365a5497503e1e136
Eric Casteleijn <ec@trapit.com>
2007-06-15 08:41:09 +0000

    serious typo in python script

--- views/edit/tab_access_deny_access.py
+++ views/edit/tab_access_deny_access.py
@@ -21,7 +21,7 @@ messages = []
 for userid, role in [r.split('|') for r in request['requests']]:
     model.deny_role(userid, role)
     msg = _('&#xab;${user_id}&#xbb; has been denied the ${role} role',
-            mapping=='user_id': userid, 'role': role})
+            mapping={'user_id': userid, 'role': role})
     messages.append(translate(msg))
 
 model.send_messages()
