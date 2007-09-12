ecb1c76de2ddf3ba0ec18dc24d0a6e5e2522ef77
Andy Altepeter <aaltepet@altepeter.net>
2007-09-12 13:25:13 +0000

    Silva - allow protected access to some roleinfo items

--- roleinfo.py
+++ roleinfo.py
@@ -7,11 +7,19 @@ BUILTIN_VIEWER_ROLES = ('Anonymous', 'Authenticated')
 SILVA_VIEWER_ROLES = ('Viewer', 'Viewer +', 'Viewer ++')
 VIEWER_ROLES = BUILTIN_VIEWER_ROLES + SILVA_VIEWER_ROLES
 
+module_security.declareProtected('View','CHIEF_ROLES')
 CHIEF_ROLES = ('ChiefEditor', 'Manager')
 EDITOR_ROLES = ('Editor',) + CHIEF_ROLES
 AUTHOR_ROLES = ('Author',) + EDITOR_ROLES
+#aaltepet: 1/12/06: we want to be able test reader_roles
+#from pythonscripts.  e.g. to see if current user
+#can access smi
+module_security.declareProtected('Change Silva access',
+                                 'READER_ROLES')
 READER_ROLES = ('Reader',) + AUTHOR_ROLES
 
+module_security.declareProtected('View',
+                                 'ALL_ROLES')
 ALL_ROLES = VIEWER_ROLES + READER_ROLES
 
 _i18n_markers = (
