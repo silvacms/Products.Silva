bc3f2b31927cfdae417f542e8983d8ea6f98fa7f
Eric Casteleijn <ec@trapit.com>
2007-10-25 11:24:53 +0000

    reverted changes, will have to fix cook first to not return invalid ids or this will lead to infinite while loops when people start using the optional argument.

--- mangle.py
+++ mangle.py
@@ -236,7 +236,7 @@ class Id:
         
         return self.OK
 
-    def new(self, raiseoninvalid=True):
+    def new(self):
         """changes id based on the old id to get a potentially unique id
 
             if old_id ends with a number, the number is increased, otherwise 2,
@@ -246,7 +246,7 @@ class Id:
             raises ValueError if id is not valid, unless raiseoninvalid is set to False
         """
         self._allow_dup = 1
-        if raiseoninvalid and not self.isValid():
+        if not self.isValid():
             raise ValueError, "The id %r is not valid" % (self._maybe_id, )
         m = self._number_postfix.match(self._maybe_id)
         if m is None:
