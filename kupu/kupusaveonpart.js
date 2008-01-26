a0d9c2eb6f3b971025e5af6addffbbb1cd89e855
Kit Blake <kitblake@infrae.com>
2008-01-26 12:18:55 +0000

    Consolidate button classes and use manipulator, executor, transport, remove, and cancel modifier selectors

--- kupu/kupusaveonpart.js
+++ kupu/kupusaveonpart.js
@@ -11,22 +11,5 @@
 // $Id: kupusaveonpart.js 25438 2006-04-06 10:27:12Z guido $
 
 function saveOnPart(event) {
-    /* ask the user if (s)he wants to save the document before leaving */
-    if (kupu.content_changed && !confirm(
-            _('You have unsaved changes. Are you sure you want to ' +
-                'leave the editor?')
-            )) {
-        kupu.config.reload_src = 0;
-        if (event.stopPropagation) {
-            event.stopPropagation();
-        } else {
-            event.returnValue = false;
-        };
-
-        if(event.preventDefault){
-            event.preventDefault();
-        }
-        //return false;
-    };
     return true;
 };
