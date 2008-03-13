42b483db91584b0e3868d08860816190c105d276
Andy Altepeter <aaltepet@altepeter.net>
2008-03-13 01:28:00 +0000

    101780: worked around a ZCatalog bug which causes         containers to be unindexed twice when a parent container is removed.

--- monkey.py
+++ monkey.py
@@ -6,7 +6,20 @@ def allow_translate():
     from AccessControl import allow_module
     # XXX is this opening up too much..?
     allow_module('zope.i18n')
+
+#Zope 2.10 contains a ZCatalog bug which causes
+# folders to be unindexed twice when a parent
+# container is removed.
+# see: https://bugs.launchpad.net/silva/+bug/101780
+# XXX NOTE: this needs to be removed once the bug is
+#           fixed in ZCatalog
+def fixupCatalogPathAwareness():
+    from Products.ZCatalog.CatalogPathAwareness import CatalogPathAware
+    def manage_beforeDelete(self, item, container):
+        self.unindex_object()
+    CatalogPathAware.manage_beforeDelete = manage_beforeDelete
     
 def patch_all():
     # perform all patches
     allow_translate()
+    fixupCatalogPathAwareness()
