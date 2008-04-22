d5417ad9b2c38680790b44915934f181142ebed2
Eric Casteleijn <ec@trapit.com>
2008-04-22 09:23:25 +0000

    forward port of converters fix

--- converters.py
+++ converters.py
@@ -1,21 +1,20 @@
 import os, tempfile
+from subprocess import Popen, PIPE
+
 
 def execute(cmd):
-    try:
-        import win32pipe
-        popen = win32pipe.popen4
-    except ImportError:
-        popen = os.popen4
-
-    fp_in, fp_out = popen(cmd)
-    fp_in.close()
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    fp_err = p.stderr
+    fp_out = p.stdout
     data = fp_out.read()
     fp_out.close()
+    err = fp_err.read()
+    fp_err.close()
+    return err, data
 
-    return data       
+PDF_TO_TEXT_AVAILABLE = execute('pdftotext -v -')[0].startswith('pdftotext')
+WORD_TO_TEXT_AVAILABLE = execute('antiword -v')[0].startswith('antiword') 
 
-PDF_TO_TEXT_AVAILABLE = execute('pdftotext -v -').startswith('pdftotext')
-WORD_TO_TEXT_AVAILABLE = execute('antiword -v').startswith('antiword') 
 
 def get_converter_for_mimetype(mimetype):
     converter = {
@@ -28,61 +27,64 @@ def get_converter_for_mimetype(mimetype):
         return
     return converter()
 
+
 class WordConverter(object):
 
     def convert(self, data, request):
         if not WORD_TO_TEXT_AVAILABLE:
             return
-
         fname = tempfile.mktemp('.doc', 'silva_')
         fp = open(fname, 'w+b')
         fp.write(data)
         fp.close()
-
-        #converted = execute('antiword -f -m UTF-8 "%s" -' % fname)
-        converted = execute('antiword "%s"' % fname)
-
+        err, converted = execute('antiword "%s"' % fname)
         os.unlink(fname)
-        if converted.endswith('is not a Word Document.') or converted.startswith(
-            "I'm afraid"):
+        if err:
+            # XXX This, as far as I can tell is never actually
+            # presented back to the user. Did something change or did
+            # this just never work?
             request.form['message_type']='feedback'
             request.form['message'] = """File uploaded succesfully.
             <span class="error">
-                The uploaded file does not appear to be a valid Word file.
-            </span>"""
+            The uploaded file does not appear to be a valid Word file:
+            <br /><br />%s
+            </span>""" % err
             return None
         try:
             decoded = unicode(converted, 'utf8')
             return decoded
         except UnicodeDecodeError:
             return None
+
 
 class PDFConverter(object):
 
     def convert(self, data, request):
         if not PDF_TO_TEXT_AVAILABLE:
             return
-
         fname = tempfile.mktemp('.pdf', 'silva_')
         fp = open(fname, 'w+b')
         fp.write(data)
         fp.close()
-        
-        converted = execute('pdftotext -enc UTF-8 "%s" -' % fname)    
-
+        err, converted = execute('pdftotext -enc UTF-8 "%s" -' % fname)
         os.unlink(fname)
-        if 'PDF file is damaged' in converted:
+        if err:
+            # XXX This, as far as I can tell is never actually
+            # presented back to the user. Did something change or did
+            # this just never work?
             request.form['message_type']='feedback'
             request.form['message'] = """File uploaded succesfully. 
             <span class="error">
-                The uploaded file does not appear to be a valid PDF file.
-            </span>"""
+            The uploaded file does not appear to be a valid PDF file:
+            <br /><br />%s
+            </span>""" % err
             return None
         try:
             decoded = unicode(converted, 'utf8')
             return decoded
         except UnicodeDecodeError:
             return None
+
 
 class TextConverter(object):
     def convert(self, data, request):
