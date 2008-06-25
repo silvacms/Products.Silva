522a89e966fd946d4b52258f8b6dad05286d59cd
Andy Altepeter <aaltepet@altepeter.net>
2008-06-25 15:22:38 +0000

    fix issue with breadcrumbs breaking when using silvalayout

--- SilvaObject.py
+++ SilvaObject.py
@@ -291,6 +291,12 @@ class SilvaObject(Security, ViewCode):
             if item == root: # XXX does equality always work in Zope?
                 break
             item = item.aq_parent
+            #if using SilvaLayout, eventually an items parent will be the
+            #view class.  This needs to be skipped over.  I'm not sure
+            #which is the "correct" interface (IBrowserView or IBrowserPage),
+            #but they both seem to work.
+            if IBrowserView.providedBy(item) or IBrowserPage.providedBy(item):
+                item = item.aq_parent
         result.reverse()
         return result
 
