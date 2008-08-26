5458c1d63264021b07fef0803152460b46120890
Sylvain Viollon <thefunny@gmail.com>
2008-08-26 11:40:43 +0000

    - More tests.

---
+++ tests/grok/contents.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+"""
+
+  You can grok your contents.
+
+
+  A version content should have a versionClass defined:
+
+    >>> grokkify('Products.Silva.tests.grok.contents_noversionclass_fixture')
+    Traceback (most recent call last):
+       ...
+    GrokError: You need to provide a version class for MyVersionedContent.
+
+
+"""
+
