a10bb0b88ceb00711c6f1dc7ea95ab9e3150b1eb
Sylvain Viollon <thefunny@gmail.com>
2008-10-01 09:52:57 +0000

    For backward compatibility.

---
+++ adapters/interfaces.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2002-2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+import zope.deprecation 
+zope.deprecation.moved('Products.Silva.interfaces.adapters', 'Silva 2.3')
+
+
