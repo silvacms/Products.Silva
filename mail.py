6d5fa332334f53c5ea3078d0212f3a26d77a87b1
Sylvain Viollon <thefunny@gmail.com>
2008-12-01 11:07:28 +0000

    Fix default parameters and encoding.

--- mail.py
+++ mail.py
@@ -9,12 +9,11 @@ from Products.MailHost.MailHost import _encode, _mungeHeaders
 module_security =  ModuleSecurityInfo('Products.Silva.helpers')
 
 module_security.declareProtected('View', 'sendmail')
-def sendmail(mh, message, mto, mfrom, subject):
+def sendmail(mh, message, mto=None, mfrom=None, subject=None):
     """Send a fraking mail, should work with regular Zope Mailhost,
     and MaildropHost.
     """
-    messageText, mto, mfrom = _mungeHeaders(message.encode('iso8859-1'),
-                                            mto, mfrom, subject)
+    messageText, mto, mfrom = _mungeHeaders(message, mto, mfrom, subject)
     messageText = _encode(messageText, None)
     mh._send(mfrom, mto, messageText)
 
