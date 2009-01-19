200bc7c92bed008db4970cf5810395bc426948b7
Sylvain Viollon <thefunny@gmail.com>
2009-01-19 12:22:33 +0000

    Refactor test case to move out tests which doesn't need to be run in Zope.

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -2,6 +2,8 @@
 # See also LICENSE.txt
 # $Id$
 
+from unittest import TestCase
+
 # Zope
 from zope.interface.verify import verifyObject
 
@@ -12,15 +14,16 @@ from Products.Silva import upgrade
 import SilvaTestCase
 
 
-class UpgradeTestCase(SilvaTestCase.SilvaTestCase):
-    """Test for the upgrade machinery.
+class UpgradeUtilitiesTestCase(TestCase):
+    """Test utilities which determines which version step should be
+    run.
     """
 
-    def test_upgrade_registry(self):
-        verifyObject(IUpgradeRegistry, upgrade.registry)
+    def setUp(self):
+        self.versions = ['1.2', '1.4', '1.5a1', '1.5b2', '1.6a2', '2.0', '2.0.1', '2.1.3.4', '2.2', '2.3']
 
     def test_get_version_index(self):
-        versions = ['1.2', '1.4', '1.5a1', '1.5b2', '1.6a2', '2.0', '2.0.1', '2.1.3.4', '2.2', '2.3']
+        versions = self.versions
         self.assertEquals(upgrade.get_version_index(versions, '1.5a1'), 3)
         self.assertEquals(upgrade.get_version_index(versions, '1.7b2dev-r3456'), 5)
         self.assertEquals(upgrade.get_version_index(versions, '2.0'), 6)
@@ -31,7 +34,7 @@ class UpgradeTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(upgrade.get_version_index(versions, '2.4'), 10)
 
     def test_get_upgrade_chain(self):
-        versions = ['1.2', '1.4', '1.5a1', '1.5b2', '1.6a2', '2.0', '2.0.1', '2.1.3.4', '2.2', '2.3']
+        versions = self.versions
         self.assertEquals(upgrade.get_upgrade_chain(versions, '1.3', '2.0'), ['1.4', '1.5a1', '1.5b2', '1.6a2', '2.0'])
         self.assertEquals(upgrade.get_upgrade_chain(versions, '1.5', '2.0'), ['1.6a2', '2.0'])
         self.assertEquals(upgrade.get_upgrade_chain(versions, '1.7', '1.9'), [])
@@ -39,8 +42,17 @@ class UpgradeTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(upgrade.get_upgrade_chain(versions, '2.2', '2.3'), ['2.3',])
 
 
+class UpgradeTestCase(SilvaTestCase.SilvaTestCase):
+    """Test for the upgrade machinery.
+    """
+
+    def test_upgrade_registry(self):
+        verifyObject(IUpgradeRegistry, upgrade.registry)
+
+
 import unittest
 def test_suite():
     suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(UpgradeUtilitiesTestCase))
     suite.addTest(unittest.makeSuite(UpgradeTestCase))
     return suite
