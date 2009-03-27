09b0bcbec79fd98ec4050a08a2eb6aae1efffc7e
Andy Altepeter <aaltepet@altepeter.net>
2009-03-27 20:48:30 +0000

    - Added an optional 'edit link reference' button to the   reference lookup window formulator widget.  Clicking this   button on a valid silva reference will open up that reference's   edit tab in a new window.
    - changed presentation lookupwindow widget displays in
      kupu.  The link tools lookup widget has the new 'button with
      image' style for the get button, an edit reference button, and
      these buttons are displayed to the left and right (resp.) of the
      input field.

--- lookupwindowfield.py
+++ lookupwindowfield.py
@@ -13,6 +13,48 @@ from Products.Formulator.Validator import StringValidator
 from Products.Formulator.Widget import render_element,TextWidget
 from Products.Silva.adapters.path import getPathAdapter
 
+from Products.Five import BrowserView
+from urlparse import urlparse
+from zExceptions import BadRequest
+from Products.Silva.interfaces import ISilvaObject
+class EditButtonRedirector(BrowserView):
+    """This view is used by the ReferenceLookupWindow's 'edit reference'
+       button to redirect to the reference object's edit screen.
+       It will raise a BadRequest error if the reference/location has a
+       scheme or netloc, if the location cannot be found, or if it is not
+       a Silva Object.  
+       
+       The decision to not raise NotFounds is kind of due
+       to security (e.g. using this redirector to fish for valid paths)
+       And also because the 'loc' parameter is a requirement for this
+       view, and if it is invalid, it is a bad request, not a not
+       found or some other error."""
+
+    def __call__(self,loc,checkonly='n'):
+        request = self.request
+        parts = urlparse(loc)
+        if parts[0] != '' or parts[1] != '':
+            raise BadRequest("Invalid location.  Unable to redirect to the edit view of absolute urls.")
+        path = parts[2]
+        if path.startswith('/'):
+            #this will convert an absolute path without a netloc
+            # to a zope path, taking into account virtualhosting
+            path = request.physicalPathFromURL(path)
+        #attempt to traverse to this object
+        ob = self.context.restrictedTraverse(path,None)
+        if not ob:
+            raise BadRequest("Invalid location.  Unable to redirect to the edit view.")
+        if ISilvaObject.providedBy(ob):
+            if checkonly == 'y':
+                return "valid"
+            else:
+                request.response.redirect(ob.absolute_url() + '/edit')
+                return "redirecting"
+        raise BadRequest("Invalid location.  Target must be a Silva Object in order to edit")
+
+        
+        
+
 class LookupWindowValidator(StringValidator):
     
     def validate(self, field, key, REQUEST):
@@ -41,8 +83,8 @@ class LookupWindowWidget(TextWidget):
         description='onclick handler implementation',
         default=default_onclick_handler,
         required=0,
-        width='30', 
-        height='4')
+        width='20', 
+        height='3')
     
     def render(self, field, key, value, request):
         widget = []
@@ -97,16 +139,41 @@ class LookupWindowWidget(TextWidget):
         return field.get_value('onclick') % interpolate
     
 class ReferenceLookupWindowWidget(LookupWindowWidget):
+    property_names = LookupWindowWidget.property_names  + ['button_label','show_edit_button']
+    
+    button_label = fields.StringField(
+        'button_label',
+        title='Label for get reference button',
+        description='The label for the get reference button',
+        default='get reference...',
+        required=0)
+    
+    show_edit_button = fields.CheckBoxField(
+        'show_edit_button', 
+        title='Show Edit Reference Button', 
+        description="Adds an 'Edit Reference' button to the widget.  When clicked, this will open a new window and redirect to the edit tab for the reference, if it is a valid Silva relative reference.  NOTE: only works in kupu.",
+        required=0,
+        default="")
+
     def render(self, field, key, value, request):
         widget = []
+        widget.append('<table class="kupu-link-reference-table" cellpadding="0" cellspacing="0"><tr><td width="24">')
         widget.append(
             render_element(
-                'input', 
-                type='button', 
-                name=key + '_button', 
-                css_class='button transporter',
-                value='get reference...',
-                extra='onclick="%s"' % self._onclick_handler(field, key)))
+                'button',
+                name=key + '_button',
+                css_class="kupu-button kupu-link-reference button transporter",
+                title=field.get_value('button_label') or 'get reference...',
+                onclick="%s;return false;" % self._onclick_handler(field, key),
+                contents=render_element(
+                    'img',
+                    width='16',
+                    height='16',
+                    src='/silva/globals/system-search.png'
+                    )
+            )
+        )
+        widget.append('</td><td>')
         widget.append(
             render_element(
                 'input', 
@@ -117,6 +184,35 @@ class ReferenceLookupWindowWidget(LookupWindowWidget):
                 size=field.get_value('display_width'), 
                 maxlength=field.get_value('display_maxwidth'),
                 extra=field.get_value('extra')))
+        widget.append('</td>')
+        if field.get_value('show_edit_button'):
+            widget.append('<td width="24">')
+            url = ''
+            request = getattr(field, 'REQUEST', None)
+            if request:
+                model = getattr(request, 'model', None)
+                if model and request.has_key('docref'):
+                    # we're in an ExternalSource, use the document in which it is
+                    # placed instead of the source as the model
+                    url = model.resolve_ref(
+                        quote(request['docref'])).absolute_url()
+            widget.append(
+                render_element(
+                    'button',
+                    name=key + '_editbutton',
+                    css_class="kupu-button kupu-link-reference button transporter",
+                    title='edit...',
+                    onclick="reference.editReference('%s','%s');return false;"%(key,url),
+                    contents=render_element(
+                        'img',
+                        width='16',
+                        height='16',
+                        src='/silva/globals/edit-find.png'
+                    )
+                )
+            )
+            widget.append('</td>')
+        widget.append('</tr></table>')
         return ' '.join(widget)
     
 class LookupWindowField(StringField):
