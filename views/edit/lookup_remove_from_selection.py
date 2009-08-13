d5981f705bb3bc1d644c970108ef300094fcd68e
Andy Altepeter <aaltepet@altepeter.net>
2009-08-13 16:38:58 +0000

    The user clipboard 'remove user' button now returns to the correct add user form.

--- views/edit/lookup_remove_from_selection.py
+++ views/edit/lookup_remove_from_selection.py
@@ -4,8 +4,14 @@ from Products.Silva.i18n import translate as _
 model = context.REQUEST.model
 view = context
 
+def return_results(message_type,message):
+    if not model.sec_can_find_users():
+        return view.lookup_ui(message_type=message_type, message=message)
+    else:
+        return view.lookup_ui_direct(message_type=message_type, message=message)
+
 if userids is None:
-    return view.lookup_ui(
+    return return_results(
         message_type="error", message=_("No users removed."))
 
 selection = view.lookup_get_selection()
@@ -13,6 +19,6 @@ for userid in userids:
     if selection.has_key(userid): 
         del selection[userid]
 
-return view.lookup_ui(
+return return_results(
     message_type="feedback", 
     message=_("Users removed from selection."))
