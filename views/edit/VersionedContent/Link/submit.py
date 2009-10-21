15178b57dfe1a44c2eebb129cde76034d27617e3
Sylvain Viollon <thefunny@gmail.com>
2009-10-21 09:05:37 +0000

    Correct the order of the actions, so if you change the type of the link AND the link, the link is changed according the rules of the correct type.

--- views/edit/VersionedContent/Link/submit.py
+++ views/edit/VersionedContent/Link/submit.py
@@ -3,10 +3,11 @@ from Products.Silva.i18n import translate as _
 model = context.REQUEST.model
 view = context
 url = unicode(url, 'UTF-8')
-model.sec_update_last_author_info()
 
-model.get_editable().set_url(url)
 model.get_editable().set_link_type(link_type)
+model.get_editable().set_url(url)
+
+model.sec_update_last_author_info()
 
 return view.tab_edit(message_type='feedback', message=_("Link changed"))
 
