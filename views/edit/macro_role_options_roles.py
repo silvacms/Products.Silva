9da252a05e9a7b9e2d314d1cec473f6957ea677b
Kit Blake <kitblake@infrae.com>
2009-10-25 08:54:26 +0000

    Remove 'zope roles' from users listing, there's already manager under 'silva roles' and when a Chief Editor is logged the zope roles optgroup is empty

--- views/edit/macro_role_options_roles.py
+++ views/edit/macro_role_options_roles.py
@@ -2,11 +2,10 @@ from Products.Silva.roleinfo import BUILTIN_VIEWER_ROLES, SILVA_VIEWER_ROLES, RE
 
 ar_roles = {'zope roles': BUILTIN_VIEWER_ROLES,
        'public silva roles' : SILVA_VIEWER_ROLES,
-       'silva roles' : READER_ROLES,}
+       'silva roles' : READER_ROLES}
 
 local_roles = {
       'public silva roles' : SILVA_VIEWER_ROLES,
-      'silva roles' : READER_ROLES,
-      'zope roles' : ('Manager', )}
+      'silva roles' : READER_ROLES}
 
 return { 'ar_roles': ar_roles, 'local_roles': local_roles }
