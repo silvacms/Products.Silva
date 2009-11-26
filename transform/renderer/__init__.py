dd82e1bd1dd3a9a3190daa29f13491cbf18a1627
Antonin Amand <antonin.amand@gmail.com>
2009-11-26 15:21:54 +0000

    add some check and logging on lxml c shared libraries versions

--- transform/renderer/__init__.py
+++ transform/renderer/__init__.py
@@ -1,1 +1,22 @@
-# this is a package
+# This will check that versions of dynamic c libraries used by lxml
+# are not different than the one it was compiled against.
+from lxml import etree
+import logging
+
+logger = logging.getLogger('silva.transform.renderer')
+
+libxml  = etree.LIBXML_VERSION == etree.LIBXML_COMPILED_VERSION
+libxslt = etree.LIBXSLT_VERSION == etree.LIBXSLT_COMPILED_VERSION
+
+message = """%s used %s and compiled %s versions differ. It is likely that \
+ld library path is incorrect (see: man ld.so)"""
+
+if not libxml:
+    logger.error(
+        message %
+            ('libxml', etree.LIBXML_VERSION, etree.LIBXML_COMPILED_VERSION,))
+
+if not libxslt:
+    logger.error(
+        message %
+            ('libxml', etree.LIBXSLT_VERSION, etree.LIBXSLT_COMPILED_VERSION,))
