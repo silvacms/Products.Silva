1a826a1ed2877781daf4b1a62352ff609de04688
Sylvain Viollon <thefunny@gmail.com>
2010-05-06 15:20:43 +0000

    Pil is always installed.

--- views/add/pil_installed.py
+++ views/add/pil_installed.py
@@ -1,7 +1,3 @@
 #this script is here just to import and return
-#the havePIL variable defined in Products.Silva.__init__.py
-#I don't seem to be able to get havePIL directly from
-#the Image formulator form
-from Products.Silva import havePIL
-
-return havePIL
+#we always have PIL
+return True
