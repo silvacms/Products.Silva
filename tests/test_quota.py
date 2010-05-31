22fbf62372f5d70edafa005015a71ddaebabe862
Sylvain Viollon <thefunny@gmail.com>
2010-05-31 13:32:02 +0000

    Update test and so by herebe fix it.

--- tests/test_quota.py
+++ tests/test_quota.py
@@ -3,31 +3,30 @@
 # $Id$
 
 # Python
-import os.path
+import unittest
+
 from zope.interface.verify import verifyObject
 
+from Products.Silva.tests.helpers import open_test_file
+from Products.Silva.testing import FunctionalLayer
 from silva.core import interfaces
-from Products.Silva.tests.test_archivefileimport import ArchiveFileImport
-import SilvaTestCase
 
-
-data_directory = os.path.join(os.path.dirname(__file__), 'data')
-zipfile1 = os.path.join(data_directory, 'test1.zip')
-zipfile2 = os.path.join(data_directory, 'test2.zip')
-imagefile1 = os.path.join(data_directory, 'torvald.jpg')
-imagefile2 = os.path.join(data_directory, 'testimage.gif')
-
-def fileSize(filename):
+def test_file_size(filename):
     """Return the size of a file.
     """
-    fd = open(filename, 'r')
-    fd.seek(0, 2)
-    return fd.tell()
+    with open_test_file(filename, mode='r') as file_handle:
+        file_handle.seek(0, 2)
+        return file_handle.tell()
 
 
-class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
+class QuotaTest(unittest.TestCase):
     """Test quota system implementation.
     """
+    layer = FunctionalLayer
+
+    def setUp(self):
+        self.root = self.layer.get_application()
+        self.layer.login('editor')
 
     def _enable_quota(self):
         self.root.service_extensions.enable_quota_subsystem()
@@ -44,11 +43,17 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         """
         self._enable_quota()
 
-        root = self.root
-        pub1 = self.add_publication(root, 'pub1', 'Publication 1')
-        folder1 = self.add_folder(pub1, 'folder1', 'Folder 1')
-        pub2 = self.add_publication(folder1, 'pub2', 'Publication 2')
-        pub3 = self.add_publication(folder1, 'pub3', 'Publication 3')
+        self.root.manage_addProduct['Silva'].manage_addPublication(
+            'pub1', 'Publication 1')
+        pub1 = self.root.pub1
+        pub1.manage_addProduct['Silva'].manage_addFolder('folder1', 'Folder 1')
+        folder1 = pub1.folder1
+        folder1.manage_addProduct['Silva'].manage_addPublication(
+            'pub2', 'Publication 2')
+        pub2 = folder1.pub2
+        folder1.manage_addProduct['Silva'].manage_addPublication(
+            'pub3', 'Publication 3')
+        pub3 = folder1.pub3
 
         # By default, the quota is 0
         self.assertEqual(pub1.get_current_quota(), 0)
@@ -61,7 +66,7 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         # FIXME: we can't test it like this. We have to make
         # functional tests (invalid request object in tests).
 
-    def test_folderAction(self):
+    def test_folder_action(self):
         """Test that folder action update used space.
         Content structure:
 
@@ -75,9 +80,14 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         self._enable_quota()
 
         root = self.root
-        folder1 = self.add_folder(root, 'folder1', 'FooFolder 1')
-        folder2 = self.add_folder(root, 'folder2', 'FooFolder 2')
-        subfolder1 = self.add_folder(folder1, 'subfolder', 'Sub FooFolder')
+        factory = self.root.manage_addProduct['Silva']
+        factory.manage_addFolder('folder1', 'FooFolder 1')
+        folder1 = getattr(root, 'folder1')
+        factory.manage_addFolder('folder2', 'FooFolder 2')
+        folder2 = getattr(root, 'folder2')
+        factory = folder1.manage_addProduct['Silva']
+        factory.manage_addFolder('subfolder1', 'Sub FooFolder')
+        subfolder1 = getattr(folder1, 'subfolder1')
 
         # By default, all used_space should be at 0
         self.assertEqual(root.used_space, 0)
@@ -85,17 +95,20 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         self.assertEqual(subfolder1.used_space, 0)
 
         # Add a file
-        zip1 = self.add_file(subfolder1, 'zipfile1.zip', 'Zip File',
-                             file=open(zipfile2))
+        factory = subfolder1.manage_addProduct['Silva']
+        factory.manage_addFile(
+            'zipfile1.zip', 'Zip File', open_test_file('test2.zip'))
+        zip1 = getattr(subfolder1, 'zipfile1.zip')
+
         verifyObject(interfaces.IAsset, zip1)
-        zip2_size = fileSize(zipfile2)
+        zip2_size = test_file_size('test2.zip')
         self.assertEqual(zip1.get_file_size(), zip2_size)
         # And check used space
         self.assertEqual(subfolder1.used_space, zip2_size)
         self.assertEqual(root.used_space, zip2_size)
         # Change file data
-        zip1.set_file_data(open(zipfile1))
-        zip1_size = fileSize(zipfile1)
+        zip1.set_file_data(open_test_file('test1.zip'))
+        zip1_size = test_file_size('test1.zip')
         self.assertEqual(zip1.get_file_size(), zip1_size)
         # And check used space
         self.assertEqual(subfolder1.used_space, zip1_size)
@@ -103,17 +116,19 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         self.assertEqual(folder2.used_space, 0)
 
         # Add an image
-        image1 = self.add_image(folder2, 'image1.jpg', 'Image File',
-                                file=open(imagefile2))
+        factory = folder2.manage_addProduct['Silva']
+        factory.manage_addImage(
+            'image1.jpg', 'Image File', open_test_file('torvald.jpg'))
+        image1 = getattr(folder2, 'image1.jpg')
         verifyObject(interfaces.IAsset, image1)
-        image2_size = fileSize(imagefile2)
+        image2_size = test_file_size('torvald.jpg')
         self.assertEqual(image1.get_file_size(), image2_size)
         # And check used space
         self.assertEqual(root.used_space, zip1_size + image2_size)
         self.assertEqual(folder2.used_space, image2_size)
         # Change image and check size
-        image1.set_image(open(imagefile1))
-        image1_size = fileSize(imagefile1)
+        image1.set_image(open_test_file('testimage.gif'))
+        image1_size = test_file_size('testimage.gif')
         self.assertEqual(image1.get_file_size(), image1_size)
         # And check used space
         self.assertEqual(subfolder1.used_space, zip1_size)
@@ -121,31 +136,35 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         self.assertEqual(folder2.used_space, image1_size)
 
         # Try cut and paste
-        folder1.action_cut(['subfolder'], self.app.REQUEST)
-        folder2.action_paste(self.app.REQUEST)
+        folder1.action_cut(['subfolder1'], self.root.REQUEST)
+        folder2.action_paste(self.root.REQUEST)
+
         # And check used space
         self.assertEqual(folder1.used_space, 0)
         self.assertEqual(root.used_space, zip1_size + image1_size)
         self.assertEqual(folder2.used_space, zip1_size + image1_size)
 
         # Try cut and ghost paste
-        folder2.action_cut(['subfolder'], self.app.REQUEST)
-        folder1.action_paste_to_ghost(self.app.REQUEST)
+        folder2.action_cut(['subfolder1'], self.root.REQUEST)
+        folder1.action_paste_to_ghost(self.root.REQUEST)
+
         # And check used space
         self.assertEqual(folder1.used_space, zip1_size)
         self.assertEqual(root.used_space, (2 * zip1_size) + image1_size)
         self.assertEqual(folder2.used_space, zip1_size + image1_size)
 
         # Delete the ghost
-        folder1.action_delete(['subfolder'])
+        folder1.action_delete(['subfolder1'])
+
         # And check used space
         self.assertEqual(folder1.used_space, 0)
         self.assertEqual(root.used_space, zip1_size + image1_size)
         self.assertEqual(folder2.used_space, zip1_size + image1_size)
 
         # Try copy and paste
-        folder2.action_copy(['image1.jpg'], self.app.REQUEST)
-        folder1.action_paste(self.app.REQUEST)
+        folder2.action_copy(['image1.jpg'], self.root.REQUEST)
+        folder1.action_paste(self.root.REQUEST)
+
         # And check used space
         self.assertEqual(folder1.used_space, image1_size)
         self.assertEqual(root.used_space, zip1_size + (2 * image1_size))
@@ -157,22 +176,35 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         root.action_delete(['folder1'])
         self.assertEqual(root.used_space, 0)
 
-
-    def test_zipImport(self):
+    def test_zip_import(self):
         """Test the import of Zip file with quota system activited.
         """
         self._enable_quota()
-        self.importArchiveFileDefaultSettingsNoSubdirsInArchive()
+        factory = self.root.manage_addProduct['Silva']
+        factory.manage_addFolder('folder', 'Folder')
+
+        importer = interfaces.IArchiveFileImporter(self.root.folder)
+        importer.importArchive(open_test_file('test1.zip'))
+
         self.assertNotEqual(self.root.used_space, 0)
 
+        self.root.manage_delObjects(['folder'])
+        self.assertEqual(self.root.used_space, 0)
 
-    def test_zipOtherImport(self):
+    def test_zip_import2(self):
         """Test the import of Zip file with quota system activited.
         """
         self._enable_quota()
-        self.importArchiveFileDefaultSettings()
+        factory = self.root.manage_addProduct['Silva']
+        factory.manage_addFolder('folder', 'Folder')
+
+        importer = interfaces.IArchiveFileImporter(self.root.folder)
+        importer.importArchive(open_test_file('test2.zip'))
+
         self.assertNotEqual(self.root.used_space, 0)
 
+        self.root.manage_delObjects(['folder'])
+        self.assertEqual(self.root.used_space, 0)
 
     def test_extension(self):
         """Test that we can disable the extension.  After, create some
@@ -189,15 +221,24 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         self.assertEqual(s_ext.get_quota_subsystem_status(), False)
 
         # Create some content
-        folder1 = self.add_folder(root, 'folder1', 'FooFolder 1')
-        folder2 = self.add_folder(root, 'folder2', 'FooFolder 2')
-        subfolder1 = self.add_folder(folder1, 'subfolder', 'Sub FooFolder')
-        zip1 = self.add_file(subfolder1, 'zipfile1.zip', 'Zip File',
-                             file=open(zipfile1))
-        zip1_size = fileSize(zipfile1)
-        image1 = self.add_image(folder2, 'image1.jpg', 'Image File',
-                                file=open(imagefile1))
-        image1_size = fileSize(imagefile1)
+        factory = self.root.manage_addProduct['Silva']
+        factory.manage_addFolder('folder1', 'FooFolder 1')
+        folder1 = getattr(root, 'folder1')
+        factory.manage_addFolder('folder2', 'FooFolder 2')
+        folder2 = getattr(root, 'folder2')
+        factory = folder1.manage_addProduct['Silva']
+        factory.manage_addFolder('subfolder', 'Sub FooFolder')
+        subfolder1 = getattr(folder1, 'subfolder')
+        factory = subfolder1.manage_addProduct['Silva']
+        factory.manage_addFile(
+            'zipfile1.zip', 'Zip File', open_test_file('test1.zip'))
+        zip1 = getattr(subfolder1, 'zipfile1.zip')
+        zip1_size = test_file_size('test1.zip')
+        factory = folder2.manage_addProduct['Silva']
+        factory.manage_addImage(
+            'image1.jpg', 'Image File', open_test_file('torvald.jpg'))
+        image1 = getattr(folder2, 'image1.jpg')
+        image1_size = test_file_size('torvald.jpg')
 
         # No counting should be done (extensions not activated, and
         # site empty)
@@ -207,6 +248,7 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
 
         # Activate it again
         s_ext.enable_quota_subsystem()
+
         # Values should be updated
         self.assertEqual(root.used_space, zip1_size + image1_size)
         self.assertEqual(folder1.used_space, zip1_size)
@@ -215,14 +257,17 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
 
         # Disable
         s_ext.disable_quota_subsystem()
+
         # Do some changes
         folder1.action_delete(['subfolder'])
-        root.action_copy(['folder2'], self.app.REQUEST)
-        folder1.action_paste(self.app.REQUEST)
+        root.action_copy(['folder2'], self.root.REQUEST)
+        folder1.action_paste(self.root.REQUEST)
+
         # Nothing had change
         self.assertEqual(root.used_space, zip1_size + image1_size)
         self.assertEqual(folder1.used_space, zip1_size)
         self.assertEqual(folder2.used_space, image1_size)
+
         # Re-enable
         s_ext.enable_quota_subsystem()
         # Values should be updated
@@ -231,7 +276,6 @@ class QuotaTest(SilvaTestCase.SilvaTestCase, ArchiveFileImport):
         self.assertEqual(folder2.used_space, image1_size)
 
 
-import unittest
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(QuotaTest))
