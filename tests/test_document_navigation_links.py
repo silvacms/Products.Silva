fc69440b180af9dcfe07787392272cc14d3ec461
Sylvain Viollon <thefunny@gmail.com>
2010-08-03 09:06:43 +0000

    Cleanup test file, remove test on stub get_real_container method.

--- tests/test_document_navigation_links.py
+++ tests/test_document_navigation_links.py
@@ -1,25 +1,26 @@
 # Copyright (c) 2003-2010 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id $
+
 import SilvaTestCase
 
 from DateTime import DateTime
 
 class ContainerTestCase(SilvaTestCase.SilvaTestCase):
-    
+
     def afterSetUp(self):
-        
+
         self.u = u = self.add_document(self.root, 'u', 'Document U')
-        
+
         self.root.manage_addProduct['Silva'].manage_addPublication('a', 'A')
         self.a = self.root.a
-        
+
         self.v = self.add_folder(
             self.root,
             'v',
-            'Folder V') 
+            'Folder V')
         self.indexv = indexv = self.add_document(self.v, 'indexv', 'Document W')
-            
+
         self.w = w = self.add_document(self.root, 'w', 'Document W')
         self.x = x = self.add_document(self.a, 'x', 'Document X')
         self.y = y = self.add_document(self.a, 'y', 'Document Y')
@@ -32,7 +33,7 @@ class ContainerTestCase(SilvaTestCase.SilvaTestCase):
 
         self.c = self.add_folder(
             self.b,
-            'c', 
+            'c',
             'C')
 
         self.d = d = self.add_document(self.c, 'd', 'Document D')
@@ -48,69 +49,48 @@ class ContainerTestCase(SilvaTestCase.SilvaTestCase):
 
         # 2nd subtree
         self.h = self.add_folder(
-            self.a, 
-            'h', 
+            self.a,
+            'h',
             'Folder H')
 
         self.i = i = self.add_document(self.h, 'i', 'Document I')
-        
+
         self.j = self.add_folder(
-            self.h, 
-            'j', 
+            self.h,
+            'j',
             'Folder J')
         self.k = k = self.add_document(self.j, 'k', 'Document K')
         self.l = l = self.add_document(self.j, 'l', 'Document L')
-    
+
 
         # 3rd subtree
         self.m = self.add_folder(
             self.a,
             'm',
             'Folder M')
-        
+
         self.n = self.add_folder(
             self.m,
             'n',
             'Folder N')
-            
+
         self.o = self.add_folder(
             self.n,
             'o',
             'Subfolder O')
         self.i2 = i2 = self.add_document(self.o, 'index', 'Document I')
         self.p = p = self.add_document(self.n, 'p', 'Document P')
-        
+
         # document heap
         self.q = q = self.add_document(self.root, 'q', 'Document Q')
         self.r = r = self.add_document(self.root, 'r', 'Document R')
         self.s = s = self.add_document(self.root, 's', 'Document S')
         self.t = t = self.add_document(self.a, 't', 'Document T')
-        
+
         for doc in [d,e,f,i,k,l,p,i1,i2,q,r,s,t,u,w,indexv,x,y]:
             doc.set_unapproved_version_publication_datetime(DateTime() - 1)
             # should now be published
             doc.approve_version()
-            
-    def test_get_real_container(self):
-        # folder
-        container = self.m.get_real_container()
-        self.assertEquals(self.a, container)
-
-        # publication
-        container = self.a.get_real_container()
-        self.assertEquals(self.root, container)
-
-        # first document in a publication
-        container = self.t.get_real_container()
-        self.assertEquals(self.a, container)
-
-        # subfolder
-        container = self.n.get_real_container()
-        self.assertEquals(self.m, container)
-        
-        # document
-        container = self.r.get_real_container()
-        self.assertEquals(self.root, container)
 
     def test_get_document_navigation_links(self):
         links = self.a.get_document_navigation_links()
@@ -121,7 +101,7 @@ class ContainerTestCase(SilvaTestCase.SilvaTestCase):
 
 
 class SimpleTestCase(SilvaTestCase.SilvaTestCase):
-    
+
     def afterSetUp(self):
         # document heap
         self.a = a = self.add_document(self.root, 'a', 'Document A')
@@ -130,7 +110,7 @@ class SimpleTestCase(SilvaTestCase.SilvaTestCase):
         self.d = d = self.add_document(self.root, 'd', 'Document D')
         self.e = e = self.add_document(self.root, 'e', 'Document E')
         self.f = f = self.add_document(self.root, 'f', 'Document F')
-        
+
         # publish documents
         for doc in [a,b,c,d,e,f]:
             doc.set_unapproved_version_publication_datetime(DateTime() - 1)
@@ -141,7 +121,7 @@ class SimpleTestCase(SilvaTestCase.SilvaTestCase):
         # test when viewing root, with an index
         links = self.root.get_document_navigation_links()
         self.assertEquals({}, links)
-        
+
         # test when viewing doc e, which is the last
         links = self.root.a.get_document_navigation_links()
         self.assertEquals(self.root, links['top'])
@@ -149,7 +129,7 @@ class SimpleTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(self.b, links['next'])
         self.assertEquals(self.f, links['last'])
         self.assertEquals(4, len(links))
-        
+
         # test when viewing doc d, which is the middle
         links = self.root.d.get_document_navigation_links()
         self.assertEquals(self.root, links['top'])
@@ -159,7 +139,7 @@ class SimpleTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(self.e, links['next'])
         self.assertEquals(self.f, links['last'])
         self.assertEquals(6, len(links))
-        
+
         # test when viewing doc f, which is the last document
         links = self.root.f.get_document_navigation_links()
         self.assertEquals(self.root, links['top'])
@@ -177,7 +157,7 @@ class SimpleTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(self.c, links['next'])
         self.assertEquals(self.f, links['last'])
         self.assertEquals(6, len(links))
-        
+
         # test when viewing at the second doc (e)
         links = self.root.e.get_document_navigation_links()
         self.assertEquals(self.root, links['top'])
@@ -187,8 +167,8 @@ class SimpleTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(self.f, links['next'])
         self.assertEquals(self.f, links['last'])
         self.assertEquals(6, len(links))
-    
-        
+
+
 class SubFolderIndexerTestCase(SilvaTestCase.SilvaTestCase):
 
     def afterSetUp(self):
@@ -198,7 +178,7 @@ class SubFolderIndexerTestCase(SilvaTestCase.SilvaTestCase):
             'Folder with an Indexer',
             policy_name='Silva AutoTOC')
         self.folder2 = self.add_folder(
-            self.root, 
+            self.root,
             'folder2',
             'Folder 2')
 
@@ -207,7 +187,7 @@ class SubFolderIndexerTestCase(SilvaTestCase.SilvaTestCase):
             'folder3',
             'Folder 3')
         self.subfolder = self.add_folder(
-            self.folder, 
+            self.folder,
             'subfolder',
             'Subfolder')
 
@@ -235,15 +215,15 @@ class SubFolderIndexerTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals(4, len(links))
 
 class AutoTocTestCase(SilvaTestCase.SilvaTestCase):
-    
+
     def afterSetUp(self):
         self.a = a = self.add_document(self.root, 'a', 'Index')
         self.b = b = self.add_document(self.root, 'b', 'Document B')
-        
+
         for doc in [a, b]:
             doc.set_unapproved_version_publication_datetime(DateTime() - 1)
             doc.approve_version()
-        
+
         self.root.manage_addProduct['Silva'].manage_addAutoTOC('foobar',
         'Title')
         self.root.manage_addProduct['Silva'].manage_addAutoTOC('dummy',
@@ -266,7 +246,7 @@ class AutoTocTestCase(SilvaTestCase.SilvaTestCase):
         self.assertEquals({}, links)
 
 class AutoTocDepthTestCase(SilvaTestCase.SilvaTestCase):
-    
+
     def afterSetUp(self):
         # This is the autotoc that we test against:
         self.root.manage_addProduct['Silva'].manage_addAutoTOC(
@@ -309,18 +289,18 @@ class AutoTocDepthTestCase(SilvaTestCase.SilvaTestCase):
             self.assert_(self.get_anchor_for('folder%s' % (l+1)) not in html)
 
 class MartijnTestCase(SilvaTestCase.SilvaTestCase):
-                                                                                
+
     def afterSetUp(self):
         self.secundus = self.add_document(self.root, 'secundus',
             'Secundus')
-        self.tertius = self.add_document(self.root, 'tertius', 
+        self.tertius = self.add_document(self.root, 'tertius',
             'Tertius')
 
         self.folder_one = self.add_folder(
-            self.root, 
+            self.root,
             'folder_1',
             'Folder 1')
-        
+
         self.tertius.set_unapproved_version_publication_datetime(DateTime()
         - 1)
         self.tertius.approve_version()
@@ -332,25 +312,25 @@ class MartijnTestCase(SilvaTestCase.SilvaTestCase):
 
         links = self.root.index.get_document_navigation_links()
         self.assertEquals({}, links)
-       
+
         links = self.root.tertius.get_document_navigation_links()
         self.assertEquals('..', links['up'])
         self.assertEquals(self.root, links['top'])
         self.assertEquals(2, len(links))
 
 class MartijnSecondTestCase(SilvaTestCase.SilvaTestCase):
-                                                                                
+
     def afterSetUp(self):
         self.secundus = self.add_document(self.root, 'secundus',
             'Secundus')
-        self.tertius = self.add_document(self.root, 'tertius', 
+        self.tertius = self.add_document(self.root, 'tertius',
             'Tertius')
 
         self.folder_one = self.add_folder(
-            self.root, 
+            self.root,
             'folder_1',
             'Folder 1',)
-        
+
         self.secundus.set_unapproved_version_publication_datetime(DateTime()
         - 1)
         self.secundus.approve_version()
@@ -362,7 +342,7 @@ class MartijnSecondTestCase(SilvaTestCase.SilvaTestCase):
         # test martijn situation
         links = self.root.get_document_navigation_links()
         self.assertEquals({}, links)
-    
+
         links = self.root.tertius.get_document_navigation_links()
         self.assertEquals(self.root, links['top'])
         self.assertEquals('..', links['up'])
