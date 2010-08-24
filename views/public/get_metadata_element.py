fdccfb6365a67e833a8ff969692db7600722953e
Sylvain Viollon <thefunny@gmail.com>
2010-08-24 15:40:58 +0000

    Cleanup more unused scripts and templates.

--- views/public/get_metadata_element.py
+++
@@ -1,7 +0,0 @@
-##parameters=set_name,element_name
-request = context.REQUEST
-model = request.model
-version = model.get_viewable()
-ms = context.service_metadata
-
-return ms.getMetadataValue(version, set_name, element_name)
