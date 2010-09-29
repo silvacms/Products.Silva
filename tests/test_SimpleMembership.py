4d17a5ea483bc0ef458f2289c0b09621119511b4
Sylvain Viollon <thefunny@gmail.com>
2010-09-29 14:34:16 +0000

    Modernise TestCase.

--- tests/test_SimpleMembership.py
+++ tests/test_SimpleMembership.py
@@ -2,76 +2,77 @@
 # See also LICENSE.txt
 # $Id$
 
-
-from zope.interface.verify import verifyObject
-
-from silva.core.services.interfaces import IMemberService
-from Products.Silva.tests import  SilvaTestCase
-
+import unittest
 import urllib
 
-class SimpleMembershipTestCase(SilvaTestCase.SilvaTestCase):
+from zope.interface.verify import verifyObject
+from zope.component import queryUtility
+from silva.core.services.interfaces import IMemberService
+from Products.Silva.testing import FunctionalLayer, TestCase
+
+
+class SimpleMembershipTestCase(TestCase):
     """Test simple membership.
     """
+    layer = FunctionalLayer
 
-    def afterSetUp(self):
-        self.members = self.root.Members
-        # add user folder and some test users
-        self.root.manage_addUserFolder()
-        acl_users = self.root.acl_users
-        acl_users._addUser('alpha', 'alpha', 'alpha', [], [])
-        acl_users._addUser('beta', 'beta', 'beta', [], [])
-        acl_users._addUser('gamma', 'gamma', 'gamma', [], [])
-        acl_users._addUser('alpha2', 'alpha2', 'alpha2', [], [])
+    def setUp(self):
+        self.root = self.layer.get_application()
+        self.layer.login('manager')
 
     def test_service(self):
-        self.failUnless(verifyObject(
-                IMemberService, self.root.service_members))
+        service = queryUtility(IMemberService)
+        self.assertTrue(verifyObject(IMemberService, service))
+        self.assertEqual(self.root.service_members, service)
 
     def test_is_user(self):
-        service_members = self.root.service_members
-        self.assert_(service_members.is_user('alpha'))
-        self.assert_(service_members.is_user('beta'))
-        self.assert_(service_members.is_user('gamma'))
-        self.assert_(service_members.is_user('alpha2'))
-        self.assert_(not service_members.is_user('delta'))
+        service = queryUtility(IMemberService)
+        self.assertTrue(service.is_user('manager'))
+        self.assertTrue(service.is_user('viewer'))
+        self.assertTrue(service.is_user('reader'))
+        self.assertTrue(service.is_user('editor'))
+        self.assertFalse(service.is_user('delta_forces'))
 
     def test_get_member(self):
-        service_members = self.root.service_members
-        self.assertEquals(service_members.get_member('alpha').userid(), 'alpha')
-        self.assertEquals(service_members.get_member('beta').userid(), 'beta')
-        self.assertEquals(service_members.get_member('delta'), None)
+        service = queryUtility(IMemberService)
+        self.assertEqual(service.get_member('manager').userid(), 'manager')
+        self.assertEqual(service.get_member('viewer').userid(), 'viewer')
+        self.assertEqual(service.get_member('delta'), None)
 
     def test_find_members(self):
-        service_members = self.root.service_members
-        members = service_members.find_members('beta')
-        self.assertEquals(len(members), 1)
-        self.assertEquals(members[0].userid(), 'beta')
+        service = queryUtility(IMemberService)
+        members = service.find_members('manager')
+        self.assertEqual(len(members), 1)
+        self.assertEqual(members[0].userid(), 'manager')
 
-        members = service_members.find_members('alpha')
-        self.assertEquals(len(members), 2)
-        self.assert_((members[0].userid() == 'alpha' and
-                      members[1].userid() == 'alpha2') or
-                     (members[0].userid() == 'alpha2' and
-                      members[1].userid() == 'alpha'))
+        members = service.find_members('ed')
+        self.assertEqual(len(members), 2)
+        userids = sorted(map(lambda m: m.userid(), members))
+        self.assertEqual(userids, ['chiefeditor', 'editor'])
 
     def test_avatar(self):
-        service_members = self.root.service_members
-        alpha = service_members.get_member('alpha')
-        self.assertEquals(None, alpha.avatar())
-        string = '<img src="' + self.root.get_root_url() + '/globals/avatar.png" alt="alpha\'s avatar" title="alpha\'s avatar" style="height: 32px; width: 32px" />'
-        self.assertEquals(string, alpha.avatar_tag())
-        alpha.set_email('user@example.com')
-        self.assertEquals('user@example.com', alpha.avatar())
-        string = '<img src="https://secure.gravatar.com/avatar.php?default=' + urllib.quote(self.root.get_root_url(),'') + '%2Fglobals%2Favatar.png&size=32&gravatar_id=b58996c504c5638798eb6b511e6f49af" alt="alpha\'s avatar" title="alpha\'s avatar" style="height: 32px; width: 32px" />'
-        self.assertEquals(string, alpha.avatar_tag())
-        string = '<img src="https://secure.gravatar.com/avatar.php?default=' + urllib.quote(self.root.get_root_url(),'') + '%2Fglobals%2Favatar.png&size=16&gravatar_id=b58996c504c5638798eb6b511e6f49af" alt="alpha\'s avatar" title="alpha\'s avatar" style="height: 16px; width: 16px" />'
-        self.assertEquals(string, alpha.extra('avatar_tag:16'))
-        
+        # XXX Data code should not generate HTML.
+        #service = queryUtility(IMemberService)
+        service = self.root.service_members
 
-import unittest
+        user = service.get_member('manager')
+        self.assertEqual(None, user.avatar())
+
+        self.assertXMLEqual(
+            user.avatar_tag(),
+            '<img src="' + self.root.get_root_url() + '/globals/avatar.png" alt="manager\'s avatar" title="manager\'s avatar" style="height: 32px; width: 32px" />')
+        user.set_email('user@example.com')
+        self.assertEqual('user@example.com', user.avatar())
+        self.assertXMLEqual(
+            user.avatar_tag(),
+            '<img src="https://secure.gravatar.com/avatar.php?default=' + urllib.quote(self.root.get_root_url(),'') + '%2Fglobals%2Favatar.png&size=32&gravatar_id=b58996c504c5638798eb6b511e6f49af" alt="manager\'s avatar" title="manager\'s avatar" style="height: 32px; width: 32px" />')
+
+        self.assertXMLEqual(
+            user.extra('avatar_tag:16'),
+            '<img src="https://secure.gravatar.com/avatar.php?default=' + urllib.quote(self.root.get_root_url(),'') + '%2Fglobals%2Favatar.png&size=16&gravatar_id=b58996c504c5638798eb6b511e6f49af" alt="manager\'s avatar" title="manager\'s avatar" style="height: 16px; width: 16px" />')
+
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(SimpleMembershipTestCase))
     return suite
-
