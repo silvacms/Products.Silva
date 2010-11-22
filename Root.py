12a64b64c494be11b7144d7b6b3235a2b7fe031f
Sylvain Viollon <thefunny@gmail.com>
2010-11-22 14:32:53 +0000

    Update default value.

--- Root.py
+++ Root.py
@@ -116,7 +116,7 @@ class Root(Publication, site.Site):
     silvaconf.factory('manage_addRootForm')
     silvaconf.factory('manage_addRoot')
 
-    _smi_skin = 'silva.core.smi.interfaces.ISMILayer'
+    _smi_skin = 'silva.core.smi.interfaces.ISMISilvaLayer'
     _properties = Publication._properties + (
         {'id': '_smi_skin',
          'label': 'Skin SMI',
