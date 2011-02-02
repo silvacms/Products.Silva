102399dd254ccb13c1bc8e39965a2dfc9e1ab101
Andy Altepeter <aaltepet@altepeter.net>
2011-02-02 09:34:02 -0600

    fix smitablist

--- Products/Silva/rest/smitablist.py
+++ Products/Silva/rest/smitablist.py
@@ -1,30 +1,42 @@
-from infrae.rest import REST
+from zope.interface import alsoProvides
 from five import grok
+from zope.component import getMultiAdapter
+from zope.contentprovider.interfaces import IContentProvider
 
 import Acquisition 
 
+from infrae.rest import REST
 from silva.core.interfaces import (ISilvaObject, 
                                    IVersionedContent, )
-
 #                                       IVersionedAsset)
+from silva.core.smi.interfaces import ISMILayer
 
 #in Silva 2.1, grok views STILL need to be zope 2 - acquisition
 # enabled (i.e. inherit from Acquisition.Implicit
 #XXXaa still needed in 2.3?
 class SMITabList(REST, Acquisition.Implicit):
     """REST API to retrieve a JSON-formatted list of
-       this silva object's tabs (name, id)
+       this silva object's (context) tab info (name, id)
     """
-    grok.name('smi-tab-list')
+    grok.name('smitablist.get_tabs')
     grok.context(ISilvaObject)
     grok.require('silva.ReadSilvaContent')
     
     def GET(self):
         """Return the list of this object's tabs (name, id)"""
-        sv = self.context.service_view_registry
-        meta_type = self.context.meta_type
-        view = sv.get_view('edit', meta_type)
         
-        # remove all but the name and id
-        tabs = [ t[:2] for t in view.get_tabs() ]
+        #the tabs are viewlets on the `smimenutabs` content provider.
+        # to get to the viewlets, request needs to provide ISMILayer,
+        # then need to get the edit tab (any tab would do),
+        # then get the content provider
+        # update() will set up the viewlets
+        alsoProvides(self.request, ISMILayer)
+        view = getMultiAdapter((self.context, self.request),
+                               name="tab_edit_extra")
+        menutabs = getMultiAdapter((self.context, self.request, view),
+                                   name="smimenutabs")
+        menutabs.update()
+        tabs = []
+        for tab in menutabs.viewlets:
+            tabs.append([tab.name,tab.path])
         return self.json_response(tabs)
