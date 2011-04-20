d58438f1c0ff20913363794dbd7004f1ddd60076
Sylvain Viollon <thefunny@gmail.com>
2011-04-20 19:02:47 +0200

    Update / fix tests.

--- Products/Silva/tests/test_mangle.py
+++ Products/Silva/tests/test_mangle.py
@@ -5,90 +5,93 @@
 # Python
 import unittest
 
-# Silva
-from StringIO import StringIO
-
 # Zope 2
 from Products.Silva import mangle
+from Products.Silva.testing import FunctionalLayer
 from silva.core.interfaces import IAsset
 
-import SilvaTestCase
 
-class MangleIdTest(SilvaTestCase.SilvaTestCase):
+class MangleIdTestCase(unittest.TestCase):
+    layer = FunctionalLayer
 
-    def afterSetUp(self):
-        self.folder = folder = self.addObject(self.silva, 'Folder', 'fold',
-            title='fold', create_default=0)
-        self.addObject(
-            folder, 'AutoTOC', 'a_content', title='a_content')
-        self.addObject(
-            folder, 'File', 'an_asset', title='an_asset',
-            file=StringIO("foobar"))
-        self.addObject(self.folder, "PageTemplate", "pt_test", "PageTemplates")
+    def setUp(self):
+        self.root = self.layer.get_application()
+        self.layer.login('editor')
+
+        factory = self.root.manage_addProduct['Silva']
+        factory.manage_addFolder('folder', 'folder')
+
+        factory = self.root.folder.manage_addProduct['Silva']
+        factory.manage_addAutoTOC('info', 'Content')
+        factory.manage_addFile('data', 'Asset')
+
+        factory = self.root.folder.manage_addProduct['PageTemplates']
+        factory.manage_addPageTemplate('pt_test')
 
     def test_validate(self):
-        id = mangle.Id(self.folder, 'some_id')
+        id = mangle.Id(self.root.folder, 'some_id')
         self.assertEquals(id.validate(), id.OK)
 
-        id = mangle.Id(self.folder, 'a_content')
+        id = mangle.Id(self.root.folder, 'info')
         self.assertEqual(id.validate(), id.IN_USE_CONTENT)
 
-        id = mangle.Id(self.folder, 'an_asset')
+        id = mangle.Id(self.root.folder, 'data')
         self.assertEqual(id.validate(), id.IN_USE_ASSET)
 
-        id = mangle.Id(self.folder, 'a_content', allow_dup=1)
+        id = mangle.Id(self.root.folder, 'info', allow_dup=1)
         self.assertEqual(id.validate(), id.OK)
 
-        id = mangle.Id(self.folder, 'an_asset', allow_dup=1)
+        id = mangle.Id(self.root.folder, 'data', allow_dup=1)
         self.assertEqual(id.validate(), id.OK)
 
-        id = mangle.Id(self.folder, 'service_foobar')
+        id = mangle.Id(self.root.folder, 'service_foobar')
         self.assertEqual(id.validate(), id.RESERVED_PREFIX)
 
         # no explicitely forbidden, but would shadow method:
-        id = mangle.Id(self.folder, 'implements_asset')
+        id = mangle.Id(self.root.folder, 'content')
         self.assertEqual(id.validate(), id.RESERVED)
 
-        id = mangle.Id(self.folder, '&*$()')
+        id = mangle.Id(self.root.folder, '&*$()')
         self.assertEqual(id.validate(), id.CONTAINS_BAD_CHARS)
 
-        id = mangle.Id(self.folder, 'index_html')
+        id = mangle.Id(self.root.folder, 'index_html')
         self.assertEqual(id.validate(), id.RESERVED)
 
-        id = mangle.Id(self.folder, 'index.html')
+        id = mangle.Id(self.root.folder, 'index.html')
         self.assertEqual(id.validate(), id.OK)
 
-        id = mangle.Id(self.folder, 'index-html')
+        id = mangle.Id(self.root.folder, 'index-html')
         self.assertEqual(id.validate(), id.OK)
 
         # Zope does not allow any id ending with '__' in a hard boiled manner
         # (see OFS.ObjectManager.checkValidId)
-        id = mangle.Id(self.folder, 'index__', allow_dup=1)
+        id = mangle.Id(self.root.folder, 'index__', allow_dup=1)
         self.assertEqual(id.validate(), id.RESERVED_POSTFIX)
 
-        id = mangle.Id(self.folder, 'index')
+        id = mangle.Id(self.root.folder, 'index')
         self.assertEqual(id.validate(), id.OK)
 
-        id = mangle.Id(self.folder, 'index', interface=IAsset)
+        id = mangle.Id(self.root.folder, 'index', interface=IAsset)
         self.assertEqual(id.validate(), id.RESERVED)
 
-        an_asset = self.folder.an_asset
-        id = mangle.Id(self.folder, 'index', instance=an_asset)
+        data = self.root.folder.data
+        id = mangle.Id(self.root.folder, 'index', instance=data)
         self.assertEqual(id.validate(), id.RESERVED)
 
-        #test IN_USE_ZOPE, by adding a non-reserved object to self.folder
-        id = mangle.Id(self.folder, 'pt_test')
+        #test IN_USE_ZOPE, by adding a non-reserved object to self.root.folder
+        id = mangle.Id(self.root.folder, 'pt_test')
         self.assertEqual(id.validate(), id.IN_USE_ZOPE)
 
     def test_cook_id(self):
-        id = mangle.Id(self.folder, u'Gro\N{LATIN SMALL LETTER SHARP S}e Datei').cook()
-        self.assert_(id.isValid())
-        self.assertEquals(str(id), 'Grose_Datei')
-        #self.assertEquals(str(id), 'Grosse_Datei') # this would be german replacement
+        id = mangle.Id(
+            self.root.folder,
+            u'Gro\N{LATIN SMALL LETTER SHARP S}e Datei').cook()
+        self.assertTrue(id.isValid())
+        self.assertEqual(str(id), 'Grose_Datei')
 
 
 
-class MangleTest(unittest.TestCase):
+class MangleTestCase(unittest.TestCase):
 
     def test_unquote(self):
         self.assertEquals(
@@ -147,10 +150,10 @@ class MangleTest(unittest.TestCase):
         s = mangle.Path.strip(['', 'foo', '.', 'bar'])
         self.assertEquals(s, ['', 'foo', 'bar'])
 
-import unittest
+
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(MangleIdTest))
-    suite.addTest(unittest.makeSuite(MangleTest))
+    suite.addTest(unittest.makeSuite(MangleIdTestCase))
+    suite.addTest(unittest.makeSuite(MangleTestCase))
     return suite
 
