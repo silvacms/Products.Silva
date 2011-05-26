8b76640ba1c7fb1725accb9880f934df4736a326
Sylvain Viollon <thefunny@gmail.com>
2011-05-26 13:01:08 +0200

    Fix Indexer edit form.

--- Products/Silva/Indexer.py
+++ Products/Silva/Indexer.py
@@ -155,9 +155,9 @@ class IndexerAddForm(silvaforms.SMIAddForm):
 class IndexerEditForm(silvaforms.SMIForm):
     """Edit form for an indexer. There is not that much to edit however.
     """
-    grok.name('silva.ui.content')
     grok.context(IIndexer)
 
+    label = _("Update Silva Indexer")
     description = _(
         u"An index is not editable. "
         u"However, you can update the index to include recent added content.")
