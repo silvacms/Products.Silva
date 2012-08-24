0c59071128a12c229160a57820b24bb0fda7dd3c
Sylvain Viollon <thefunny@gmail.com>
2012-08-24 12:02:49 +0200

    Get ride of browser directory.

--- Products/Silva/browser/__init__.py
+++
@@ -1,8 +0,0 @@
-# this is a package
-
-from five import grok
-
-class SilvaGlobals(grok.DirectoryResource):
-    # This export the globals directory using Zope 3 technology.
-    grok.path('scripts')
-    grok.name('Products.Silva.browser.scripts')
