a3dcf2b67e0c61024497ef155e1f2688f6b28be8
Sylvain Viollon <thefunny@gmail.com>
2012-08-24 12:06:11 +0200

    Fix deferred.

--- Products/Silva/UnicodeSplitter.py
+++ Products/Silva/UnicodeSplitter.py
@@ -1,9 +1,8 @@
 
 import zope.deferredimport
-import silva.core.services
 
 
 zope.deferredimport.deprecated(
     'Please run zodbupdate.',
-    Splitter=silva.core.services.splitter.Splitter)
+    Splitter='silva.core.services.splitter:Splitter')
 
