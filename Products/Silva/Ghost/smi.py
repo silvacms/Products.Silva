8346e4e9acf25282e9e2cb3cb1f68deb19e140ef
Sylvain Viollon <thefunny@gmail.com>
2013-11-12 17:19:36 +0100

    Update copyright.

--- Products/Silva/Ghost/smi.py
+++ Products/Silva/Ghost/smi.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013  Infrae. All rights reserved.
+# See also LICENSE.txt
 
 # Zope 3
 from five import grok
--- Products/Silva/GhostAsset/__init__.py
+++ Products/Silva/GhostAsset/__init__.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013  Infrae. All rights reserved.
+# See also LICENSE.txt
 # This is a package.
